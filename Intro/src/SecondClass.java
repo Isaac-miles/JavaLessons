e35453d41d046c1e34d73de8f19f50ce0ae65939
isaac-miles <isaacnice247@gmail.com>
2024-01-01 18:41:24 +0100

    updated repo

--- Intro/src/SecondClass.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class SecondClass {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int topScore = scanner.nextInt();
-
-    }
-}
