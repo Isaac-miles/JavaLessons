d44f54d365d6c805a6cd455e628498fc7e968348
isaac-miles <isaacnice247@gmail.com>
2024-01-02 21:58:56 +0100

    seconds and minutes challenge

---
+++ methods/src/SecondsAndMinutes.java
@@ -0,0 +1,10 @@
+public class SecondsAndMinutes {
+    public static void main(String[] args) {
+        System.out.println(getDurationString(3945)); //first test
+    }
+
+    public static String getDurationString(int seconds){
+        return "";
+    }
+
+}
