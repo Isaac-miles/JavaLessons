8d32d5a7c3e20e53caae9d1d8c844b50298fe098
isaac-miles <isaacnice247@gmail.com>
2024-01-02 22:26:44 +0100

    seconds and minutes challenge

--- methods/src/SecondsAndMinutes.java
+++ methods/src/SecondsAndMinutes.java
@@ -2,9 +2,13 @@ public class SecondsAndMinutes {
     public static void main(String[] args) {
         System.out.println(getDurationString(3945)); //first test case
         System.out.println(getDurationString(65,45)); //second test case
+        System.out.println(getDurationString(65, 145));
     }
 
     public static String getDurationString(int seconds){
+        if(seconds < 0){
+            return "Invalid data for seonds(" + seconds + "), must be a positive value";
+        }
         int minutes = seconds / 60;
         return  getDurationString(minutes,seconds);
 
