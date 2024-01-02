1f5225b92b659c402416699a69b1643ce91ef1ab
isaac-miles <isaacnice247@gmail.com>
2024-01-02 22:11:25 +0100

    seconds and minutes challenge

--- methods/src/SecondsAndMinutes.java
+++ methods/src/SecondsAndMinutes.java
@@ -7,14 +7,10 @@ public class SecondsAndMinutes {
         //two-step approach
         int minutes = seconds/60;
         int hours = minutes /60;
-        System.out.println("hours = "+ hours);
 
         int remainingMinutes = minutes % 60;
-        System.out.println("minutes =" + minutes);
-        System.out.println("remainingMinutes =" + remainingMinutes);
 
         int remainingSeconds = seconds%60;
-        System.out.println("remainingSeconds = " + remainingSeconds);
 
         return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s ";
     }
