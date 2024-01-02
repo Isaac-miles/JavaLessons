d085ee85d4d3aaa6d9cf75e2d6163ac724f56812
isaac-miles <isaacnice247@gmail.com>
2024-01-02 22:17:08 +0100

    seconds and minutes challenge

--- methods/src/SecondsAndMinutes.java
+++ methods/src/SecondsAndMinutes.java
@@ -5,8 +5,13 @@ public class SecondsAndMinutes {
     }
 
     public static String getDurationString(int seconds){
-        //two-step approach
-        int minutes = seconds/60;
+        int minutes = seconds / 60;
+        return  getDurationString(minutes,seconds);
+
+    }
+
+    public static String getDurationString(int minutes, int seconds){
+
         int hours = minutes /60;
 
         int remainingMinutes = minutes % 60;
@@ -14,9 +19,5 @@ public class SecondsAndMinutes {
         int remainingSeconds = seconds%60;
 
         return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s ";
-    }
-
-    public static String getDurationString(int minutes, int seconds){
-        return "";
     }
 }
