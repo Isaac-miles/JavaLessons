641655db11ddaa9428ae8745e658229d7c61bf66
isaac-miles <isaacnice247@gmail.com>
2024-01-03 17:47:10 +0100

    switch statement

--- controlflow/src/Main.java
+++ controlflow/src/Main.java
@@ -10,24 +10,12 @@ public class Main {
         System.out.println(month +" is in the "+getQuarter(month) + " quarter");
     }
     public static String getQuarter(String month){
-        switch (month){
-            case "January":
-            case "February":
-            case "March":
-                return "1st";
-            case "April":
-            case "May":
-            case "June":
-                return "2nd";
-            case "July":
-            case "August":
-            case "September":
-                return "3rd";
-            case "October":
-            case "November":
-            case "December":
-                return "4th";
-        }
-        return "Bad";
+        return switch (month) {
+            case "January", "February", "March" -> "1st";
+            case "April", "May", "June" -> "2nd";
+            case "July", "August", "September" -> "3rd";
+            case "October", "November", "December" -> "4th";
+            default -> "Bad";
+        };
     }
 }
