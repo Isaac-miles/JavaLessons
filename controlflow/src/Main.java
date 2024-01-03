53bfaaccddce4da8243e011aea808d70fffe113a
isaac-miles <isaacnice247@gmail.com>
2024-01-03 17:18:38 +0100

    switch statement

--- controlflow/src/Main.java
+++ controlflow/src/Main.java
@@ -8,7 +8,15 @@ public class Main {
         System.out.println("Enter val");
         int val = scanner.nextInt();
         switch (val){
-
+            case 1:
+                System.out.println("val was one");
+                break;
+            case 2:
+                System.out.println("val was two");
+                break;
+            default:
+                System.out.println("was not one or two");
+                break;
         }
     }
 }
