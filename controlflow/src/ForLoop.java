433a73ed14e95005464b63ae01414da4b0e6d31d
isaac-miles <isaacnice247@gmail.com>
2024-01-04 18:50:23 +0100

    refactor

--- controlflow/src/ForLoop.java
+++ controlflow/src/ForLoop.java
@@ -1,6 +1,7 @@
 public class ForLoop {
     public static void main(String[] args) {
         for(double rate=7.5; rate<=10.0; rate+=0.25){
+            if(rate >9.0) break;
             System.out.println(calculateInterest(100, rate));
         }
         for(double rate = 1.0; rate<=5.0; rate++){
