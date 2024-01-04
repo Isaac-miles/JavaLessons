3e028fd0abe0baf1a4c2e803a7ab99731f440bdd
isaac-miles <isaacnice247@gmail.com>
2024-01-05 00:18:28 +0100

    While loop

--- controlflow/src/WhileLoops.java
+++ controlflow/src/WhileLoops.java
@@ -3,6 +3,10 @@ public class WhileLoops {
 
         int j = 5;
         while (j<=20){
+            j++;
+           if(!isEven(j)){
+               continue;
+           }
             System.out.println(j +" is even number "+ isEven(j));
             j++;
         }
