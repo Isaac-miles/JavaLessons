c9f78569082e5fa41cbe8fa5e5456790712bb8b2
isaac-miles <isaacnice247@gmail.com>
2024-01-04 18:38:18 +0100

    refactor

--- controlflow/src/ForLoop.java
+++ controlflow/src/ForLoop.java
@@ -3,8 +3,8 @@ public class ForLoop {
         for(int i= 0; i<=3; i++){
             System.out.println(i);
         }
+        System.out.println(calculateInterest(1000, 3.5));
     }
-
     public static double calculateInterest(double amount, double interestRate){
         return (amount* (interestRate/100));
     }
