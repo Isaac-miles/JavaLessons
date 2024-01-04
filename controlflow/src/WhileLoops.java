91bc4aa0b28d342c40ac2483f55e40066b752f07
isaac-miles <isaacnice247@gmail.com>
2024-01-05 00:25:46 +0100

    While loop

--- controlflow/src/WhileLoops.java
+++ controlflow/src/WhileLoops.java
@@ -1,7 +1,7 @@
 public class WhileLoops {
     public static void main(String[] args) {
 
-        int j = 5;
+        int j = 4;
         int evenCount =0;
         int oddCount = 0;
         while (j<=20){
@@ -12,8 +12,10 @@ public class WhileLoops {
            }
             System.out.println(j +" is even number ");
            evenCount++;
+           if(evenCount >= 5) break;
         }
-
+        System.out.println("Total odd numbers "+ oddCount);
+        System.out.println("Total even numbers " + evenCount);
     }
     public static boolean isEven(int number){
         return number % 2 == 0;
