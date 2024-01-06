3b397d01cb338e17075cc75050f5770a4ff76f6c
isaac-miles <isaacnice247@gmail.com>
2024-01-06 20:15:08 +0100

    get input from user

--- controlflow/src/WhileLoops.java
+++ controlflow/src/WhileLoops.java
@@ -19,11 +19,6 @@ public class WhileLoops {
 
         System.out.println("The sum of the digits in number is "+ sumDigitChallenge(1234));
 
-        // reading inputs
-        int currentYear = 2024;
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
-
     }
 
 
@@ -44,10 +39,4 @@ public class WhileLoops {
 
     }
 
-    public static String getInputFromConsole(int currentYear){
-        return "";
-    }
-    public static String getInputFromScanner(int currentYear){
-        return "";
-    }
 }
