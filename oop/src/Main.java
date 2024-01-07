65b04abd2d6f236c7a2ef9ef9b780fbaa48b11ef
isaac-miles <isaacnice247@gmail.com>
2024-01-07 20:31:12 +0100

    OOP

--- oop/src/Main.java
+++ oop/src/Main.java
@@ -2,6 +2,8 @@
 public class Main {
     public static void main(String[] args) {
     Account account = new Account();
-
+    account.depositFunds(200.0);
+    account.withdrawFunds(67.0);
+    account.depositFunds(89);
     }
 }
