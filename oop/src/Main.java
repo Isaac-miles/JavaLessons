1c220f4326a70b07e276b61886b0c2054301d80a
isaac-miles <isaacnice247@gmail.com>
2024-01-07 20:48:21 +0100

    OOP

--- oop/src/Main.java
+++ oop/src/Main.java
@@ -1,10 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-    Account account = new Account();
-    account.setNumber("6560600229");
-    account.setBalance(1000.0);
-    account.setCustomerName("Isaac miles");
+    Account account = new Account("6560600", 1000.0,"miles", "miles@gmail.com","070500");
+        System.out.println(account.getBalance());
     account.depositFunds(200.0);
     account.withdrawFunds(67.0);
     account.depositFunds(89);
