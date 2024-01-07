b7c8377472c9d792b3b3e3bf4730e0d2019755f7
isaac-miles <isaacnice247@gmail.com>
2024-01-07 20:19:39 +0100

    OOP

---
+++ oop/src/Account.java
@@ -0,0 +1,19 @@
+public class Account {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+
+
+
+
+//    public void describeCar(){
+//        System.out.println(door + "-Door "+
+//                color+" "+
+//                make+" "+
+//                model+" "+
+//                (convertible? "Convertible":""));
+//    }
+
+}
