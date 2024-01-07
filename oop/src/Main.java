1f285b5901dab42826317785d9be9bf6af9f5191
isaac-miles <isaacnice247@gmail.com>
2024-01-07 20:20:42 +0100

    OOP

--- oop/src/Main.java
+++ oop/src/Main.java
@@ -2,11 +2,6 @@
 public class Main {
     public static void main(String[] args) {
     Account account = new Account();
-    account.setMake("Prado");
-    account.setColor("yellow");
-    account.setDoor(4);
-    account.setConvertible(true);
-    account.setModel("Model x");
-    account.describeCar();
+
     }
 }
