45bc7868496ef56bf16f43e9e7b4e7b6d36bd516
isaac-miles <isaacnice247@gmail.com>
2024-01-07 20:23:06 +0100

    OOP

--- oop/src/Account.java
+++ oop/src/Account.java
@@ -5,6 +5,48 @@ public class Account {
     private String customerEmail;
     private String customerPhone;
 
+    public void depositFunds(double depositAmount){
+        balance += depositAmount;
 
+    }
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
 
 }
