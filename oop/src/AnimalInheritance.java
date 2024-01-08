47eb92bd9e6f5b9f966a43e27f9fb137c6cce7e7
isaac-miles <isaacnice247@gmail.com>
2024-01-08 16:31:45 +0100

    Inheritance

--- oop/src/AnimalInheritance.java
+++ oop/src/AnimalInheritance.java
@@ -1,9 +1,9 @@
-public class Inheritance {
+public class AnimalInheritance {
     private String type;
     private String size;
     private double weight;
 
-    public Inheritance(String type, String size, double weight) {
+    public AnimalInheritance(String type, String size, double weight) {
         this.type = type;
         this.size = size;
         this.weight = weight;
