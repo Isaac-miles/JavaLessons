30410f76caff9954e469afc4dc880465ccf5abd1
isaac-miles <isaacnice247@gmail.com>
2024-01-08 11:45:58 +0100

    OOP

--- oop/src/PojoStudent.java
+++ oop/src/PojoStudent.java
@@ -1,4 +1,14 @@
 public class PojoStudent {
     //Plain old java object
+    private String id;
+    private  String name;
+    private String dateOfBirth;
+    private String classList;
 
+    public PojoStudent(String id, String name, String dateOfBirth, String classList) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.classList = classList;
+    }
 }
