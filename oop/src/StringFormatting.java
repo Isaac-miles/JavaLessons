b7a9f66adf52c5173e4ef9f4812faad0669425b0
isaac-miles <isaacnice247@gmail.com>
2024-01-09 23:34:13 +0100

    String formatting

--- oop/src/StringFormatting.java
+++ oop/src/StringFormatting.java
@@ -1,5 +1,11 @@
 public class StringFormatting {
     public static void main(String[] args) {
+        //insert a tab character \t
+        //insert a new line \n
+        //insert a double quote character \"
+        //insert a backslash character
+
         String bulletIt = "Print a bulleted list:"+"\u2022 first point"+"\u2022 Sub Point";
+        System.out.println(bulletIt);
     }
 }
