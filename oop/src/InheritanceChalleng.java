5c9b991829a94ff14df73faef90c2754b0344fc0
isaac-miles <isaacnice247@gmail.com>
2024-01-09 15:33:00 +0100

    Inheritance

--- oop/src/InheritanceChalleng.java
+++ oop/src/InheritanceChalleng.java
@@ -1,2 +1,12 @@
 public class InheritanceChalleng {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Worker{
+    private String name;
+    private String dob;
+    private String endDate;
+
 }
