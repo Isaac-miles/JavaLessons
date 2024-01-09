6632a0a8c90684c04bbe17b9677ac5b80adea911
isaac-miles <isaacnice247@gmail.com>
2024-01-09 15:36:48 +0100

    Inheritance

--- oop/src/InheritanceChalleng.java
+++ oop/src/InheritanceChalleng.java
@@ -1,6 +1,7 @@
 public class InheritanceChalleng {
     public static void main(String[] args) {
-
+    Worker work = new Worker("miles","12-02-2010","15-11-2019");
+        System.out.println(work);
     }
 }
 
@@ -9,4 +10,18 @@ class Worker{
     private String dob;
     private String endDate;
 
+    public Worker(String name, String dob, String endDate) {
+        this.name = name;
+        this.dob = dob;
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", dob='" + dob + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
 }
