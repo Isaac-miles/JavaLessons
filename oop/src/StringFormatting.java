90c407477fe3537996c371b8724fd59d2c15a288
isaac-miles <isaacnice247@gmail.com>
2024-01-10 15:28:11 +0100

    String formatting

--- oop/src/StringFormatting.java
+++ oop/src/StringFormatting.java
@@ -40,6 +40,11 @@ public class StringFormatting {
     public static void printInformation(String string){
         int length = string.length();
         System.out.printf("Length = %d %n", length);
+        if(string.isEmpty()){
+            System.out.println("string is empty ");
+            return;
+        }
         System.out.printf("First char = %c %n", string.charAt(0));
+        System.out.printf("Last char = %c %n", string.charAt(length-1));
     }
 }
