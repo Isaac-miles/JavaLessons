98d2ee27c9f1e74a3149ac92b739f29da421e761
isaac-miles <isaacnice247@gmail.com>
2024-01-10 22:19:26 +0100

    String manipulation

--- oop/src/StringManipulation.java
+++ oop/src/StringManipulation.java
@@ -2,6 +2,7 @@ public class StringManipulation {
     public static void main(String[] args) {
         String dob = "25-11-1996";
         int startIndex = dob.indexOf("1996");
-
+        System.out.println("stringIndex = " +startIndex);
+        System.out.println("Birth year = " + dob.substring(startIndex));
     }
 }
