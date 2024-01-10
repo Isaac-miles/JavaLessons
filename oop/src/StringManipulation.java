d414a043db27e047e110f54a17b1110ced0b63e0
isaac-miles <isaacnice247@gmail.com>
2024-01-10 22:39:21 +0100

    String manipulation

--- oop/src/StringManipulation.java
+++ oop/src/StringManipulation.java
@@ -8,5 +8,30 @@ public class StringManipulation {
         System.out.println("month = "+ dob.substring(3,5));
         String newDob = String.join("/","25","11", "1996");
         System.out.println("new date = " + newDob);
+
+        //never do this as this is inefficient
+        newDob = "25";
+        newDob = newDob.concat("/");
+        newDob = newDob.concat("15");
+        newDob = newDob.concat(("/"));
+        newDob = newDob.concat("1996");
+
+        newDob = "25".concat("/").concat("15").concat("/").concat("1997");
+        System.out.println(newDob);
+
+        System.out.println(newDob.replace('/','-'));
+        System.out.println(newDob.replace("5","00"));
+
+        System.out.println(newDob.replaceFirst("/","-"));
+        System.out.println(newDob.replaceAll("5","---"));
+
+        System.out.println("ABC\n".repeat(3));
+        System.out.println("-".repeat(20));
+
+        System.out.println("ABC\n".repeat(3).indent(8));
+        System.out.println("-".repeat(20));
+
+        System.out.println("    ABC\n".repeat(3).indent(-2));
+        System.out.println("-".repeat(20));
     }
 }
