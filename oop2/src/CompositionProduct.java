fd9cb3cce95c81674fbe05d1683339aa7a00a7fa
isaac-miles <isaacnice247@gmail.com>
2024-01-12 14:55:04 +0100

    composition

--- oop2/src/CompositionProduct.java
+++ oop2/src/CompositionProduct.java
@@ -1,3 +1,12 @@
 public class CompositionProduct {
+    private String model;
+    private String manufacturer;
+    private int width;
+    private int height;
+    private int dept;
 
+    public CompositionProduct(String model, String manufacturer) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+    }
 }
