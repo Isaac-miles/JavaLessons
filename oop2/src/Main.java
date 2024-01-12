e80253fe098974d9b94183e02fa9e38a001a729e
isaac-miles <isaacnice247@gmail.com>
2024-01-12 15:38:27 +0100

    composition

--- oop2/src/Main.java
+++ oop2/src/Main.java
@@ -3,5 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         ComputerCase theCase = new ComputerCase("200", "Dell", "240");
+        Monitor theMonitor = new Monitor("27 inches HD", "Dell",27, "2010 X 1140");
+        MotherBoard theMotherBoard = new MotherBoard("BJ-200", "Asus",4,6,"v6");
     }
 }
