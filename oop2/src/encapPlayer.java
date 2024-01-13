70f62b67c2b8171b9cbcca805d80828bb1d30e12
isaac-miles <isaacnice247@gmail.com>
2024-01-13 22:11:36 +0100

    Encapsulation

--- oop2/src/encapPlayer.java
+++ oop2/src/encapPlayer.java
@@ -2,4 +2,10 @@ public class encapPlayer {
     private String name;
     private int health;
     private String weapon;
+
+    public encapPlayer(String name, int health, String weapon) {
+        this.name = name;
+        this.health = health;
+        this.weapon = weapon;
+    }
 }
