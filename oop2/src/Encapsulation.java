90739957dcdd86bdf15d66580824e3615e447e3e
isaac-miles <isaacnice247@gmail.com>
2024-01-13 22:15:50 +0100

    Encapsulation

--- oop2/src/Encapsulation.java
+++ oop2/src/Encapsulation.java
@@ -1,14 +1,16 @@
 public class Encapsulation {
     public static void main(String[] args) {
         nonEncapPlayers player = new nonEncapPlayers();
+        //having direct access to properties of a class is not efficient
         player.name = "tim";
         player.health = 20;
         player.weapon ="Sword";
-
         player.loseHealth(10);
         System.out.printf("Remaining health = %d %n", player.healthRemaining());
-
         player.loseHealth(11);
         System.out.printf("Remaining health = %d", player.healthRemaining());
+
+        //encapsulated player class.
+        en
     }
 }
