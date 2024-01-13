a6b574925975358fc05b2f0d9da7839ef1e3c255
isaac-miles <isaacnice247@gmail.com>
2024-01-13 22:20:46 +0100

    Encapsulation

--- oop2/src/EncapPlayer.java
+++ oop2/src/EncapPlayer.java
@@ -1,38 +1,38 @@
 public class EncapPlayer {
-    private String name;
-    private int health;
+    private String fullName;
+    private int healthPercentage;
     private String weapon;
 
-    public EncapPlayer(String name) {
-        this(name, 100,"Axe");
+    public EncapPlayer(String fullName) {
+        this(fullName, 100,"Axe");
     }
 
-    public EncapPlayer(String name, int health, String weapon) {
-        this.name = name;
+    public EncapPlayer(String fullName, int health, String weapon) {
+        this.fullName = fullName;
         if(health <=0){
-            this.health = 1;
+            this.healthPercentage = 1;
         } else if (health > 100) {
-            this.health = 100;
+            this.healthPercentage = 100;
         }else {
-            this.health = health;
+            this.healthPercentage = health;
         }
         this.weapon = weapon;
     }
 
     public void loseHealth(int damage){
-        health = health - damage;
-        if(health <=0){
+        healthPercentage = healthPercentage - damage;
+        if(healthPercentage <=0){
             System.out.println("Player knocked out");
         }
     }
     public int healthRemaining(){
-        return health;
+        return healthPercentage;
     }
     public void restoreHealth(int extraHealth){
-        health = health +extraHealth;
-        if(health > 100){
+        healthPercentage = healthPercentage +extraHealth;
+        if(healthPercentage > 100){
             System.out.println("Player restored to 100%");
-            health = 100;
+            healthPercentage = 100;
         }
     }
 
