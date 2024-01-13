f8a5219bedd8a2949916f39ed4a7257a54afb282
isaac-miles <isaacnice247@gmail.com>
2024-01-13 22:10:23 +0100

    Encapsulation

--- oop2/src/Encapsulation.java
+++ oop2/src/Encapsulation.java
@@ -1,6 +1,6 @@
 public class Encapsulation {
     public static void main(String[] args) {
-        EncapPlayers player = new EncapPlayers();
+        nonEncapPlayers player = new nonEncapPlayers();
         player.name = "tim";
         player.health = 20;
         player.weapon ="Sword";
