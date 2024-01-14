77a5dce48822bc1216c03d418e72472196ba42d4
isaac-miles <isaacnice247@gmail.com>
2024-01-14 23:13:15 +0100

    Polymorphism

--- oop2/src/NextPolyMain.java
+++ oop2/src/NextPolyMain.java
@@ -5,7 +5,13 @@ public class NextPolyMain {
         //just like casting primitive types, you can cast objects also, without casting
         //the below code wouldn't have worked because the compiler cant figure out at compile time the object returned from
         //the getMovie method and since Jaws was expecting an adventure movie
-        Adventure jaws = (Adventure) PolyMovie.getMovie("S", "Jaws");
+        Adventure jaws = (Adventure) PolyMovie.getMovie("A", "Jaws");
         jaws.watchMovie();
+
+        Object comedy = PolyMovie.getMovie("c", "Snakes on the plane");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = PolyMovie.getMovie("c", "snakes in the plane");
     }
 }
