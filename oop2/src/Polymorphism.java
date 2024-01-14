a91c6f92a03d8485fcb1875976d6407395ddb402
isaac-miles <isaacnice247@gmail.com>
2024-01-14 20:56:12 +0100

    Polymorphism

--- oop2/src/Polymorphism.java
+++ oop2/src/Polymorphism.java
@@ -1,8 +1,9 @@
 public class Polymorphism {
     public static void main(String[] args) {
         PolyMovie theMovie = new Adventure("Star warz");
-        theMovie.watchMovie();
+//        theMovie.watchMovie();
 
         PolyMovie movie = PolyMovie.getMovie("Adventure","Shrek");
+        movie.watchMovie();
     }
 }
