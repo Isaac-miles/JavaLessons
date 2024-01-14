f8332057f42a1a7b124af361dc67f6958d742f13
isaac-miles <isaacnice247@gmail.com>
2024-01-14 20:19:47 +0100

    Polymorphism

--- oop2/src/PolyMovie.java
+++ oop2/src/PolyMovie.java
@@ -1,2 +1,10 @@
 public class PolyMovie {
+    private String title;
+
+    public PolyMovie(String title){
+        this.title = title;
+    }
+    public void watchMovie(){
+        String instanceType = this.getClass().getSimpleName();
+    }
 }
