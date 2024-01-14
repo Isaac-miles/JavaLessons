22e5aec259c709e79b9682b1a480387fccf8ca97
isaac-miles <isaacnice247@gmail.com>
2024-01-14 21:16:10 +0100

    Polymorphism

--- oop2/src/Polymorphism.java
+++ oop2/src/Polymorphism.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Polymorphism {
     public static void main(String[] args) {
         PolyMovie theMovie = new Adventure("Star warz");
 //        theMovie.watchMovie();
 
-        PolyMovie movie = PolyMovie.getMovie("Adventure","Shrek");
-        movie.watchMovie();
+//        PolyMovie movie = PolyMovie.getMovie("Adventure","Shrek");
+//        movie.watchMovie();
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Enter type (A for adventure, C for Comedy, S for science fiction, or Q for quit): ");
+            String type = scanner.nextLine();
+            if("Qq".contains((type))){
+                break;
+            }
+        }
     }
 }
