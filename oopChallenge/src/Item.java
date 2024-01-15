222bc0a901566b2ef4f91072ccebf697daba0e01
isaac-miles <isaacnice247@gmail.com>
2024-01-16 00:24:54 +0100

    oop mega Challenge

--- oopChallenge/src/Item.java
+++ oopChallenge/src/Item.java
@@ -27,4 +27,11 @@ public class Item {
             default -> getBasePrice();
         };
     }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public static void printItem(String name, double price){
+
+    }
 }
