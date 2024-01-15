e1bdcd434b7c6fab2cd94a37ac955654c103bec0
isaac-miles <isaacnice247@gmail.com>
2024-01-15 23:32:43 +0100

    oop mega Challenge

--- oopChallenge/src/Item.java
+++ oopChallenge/src/Item.java
@@ -3,4 +3,10 @@ public class Item {
     private String name;
     private double price;
     private String size = "MEDIUM";
+
+    public Item(String type, String name, double price) {
+        this.type = type.toUpperCase();
+        this.name = name.toUpperCase();
+        this.price = price;
+    }
 }
