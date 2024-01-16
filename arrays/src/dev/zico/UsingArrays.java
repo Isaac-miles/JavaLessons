a633a9c38e8f03905be51dc2b063b54c802fe34f
isaac-miles <isaacnice247@gmail.com>
2024-01-16 22:39:48 +0100

    Arrays

--- arrays/src/dev/zico/UsingArrays.java
+++ arrays/src/dev/zico/UsingArrays.java
@@ -1,9 +1,14 @@
 package dev.zico;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class UsingArrays {
     public static void main(String[] args) {
+        int[] firstArray = getRandomArrays(10);
+        System.out.println(Arrays.toString(firstArray));
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
 
     }
     private static int[] getRandomArrays(int len){
