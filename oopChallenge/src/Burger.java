80c6dfd87c591f20621bb5fd92885697fc23f0bb
isaac-miles <isaacnice247@gmail.com>
2024-01-16 01:03:48 +0100

    oop mega Challenge

--- oopChallenge/src/Burger.java
+++ oopChallenge/src/Burger.java
@@ -19,4 +19,18 @@ public class Burger extends Item{
                 ((extra2==null)? 0 : extra2.getAdjustedPrice()) +
                 ((extra3==null)? 0 : extra3.getAdjustedPrice());
     }
+
+    public double getExtraPrice(String toppingName){
+        return switch (toppingName.toUpperCase()){
+            case "AVOCADO","CHEESE"->1.0;
+            case "BACON", "HAM", "SALAD"->1.5;
+            default -> 0.0;
+        };
+    }
+    public void addToppings(String extra1, String extra2, String extra3){
+        this.extra1 = new Item("TOPPING", extra1,getExtraPrice(extra1));
+        this.extra2 = new Item("TOPPING", extra2,getExtraPrice(extra2));
+        this.extra3 = new Item("TOPPING", extra3,getExtraPrice(extra3));
+
+    }
 }
