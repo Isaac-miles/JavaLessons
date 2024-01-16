795b8ff83f5f7adbfab246c1ec5b9b0c6998cbcf
isaac-miles <isaacnice247@gmail.com>
2024-01-16 22:46:17 +0100

    Arrays

--- arrays/src/dev/zico/UsingArrays.java
+++ arrays/src/dev/zico/UsingArrays.java
@@ -10,6 +10,22 @@ public class UsingArrays {
         Arrays.sort(firstArray);
         System.out.println(Arrays.toString(firstArray));
 
+        int[] secondArray = new int[5];
+        System.out.println(Arrays.toString(secondArray));
+        Arrays.fill(secondArray, 2);
+        System.out.println(Arrays.toString(secondArray));
+
+        //returning a new array instead of manipulating your main array
+        int[] thirdArray = getRandomArrays(7);
+        System.out.println(Arrays.toString(thirdArray));
+
+        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length); //creates a new array
+        System.out.println(Arrays.toString(fourthArray));
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(fourthArray));
+
+
     }
     private static int[] getRandomArrays(int len){
         Random random = new Random();
