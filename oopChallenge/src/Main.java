1d1447f296a1f5451fe92dfbdf4e49c39416fb95
isaac-miles <isaacnice247@gmail.com>
2024-01-16 01:07:46 +0100

    oop mega Challenge

--- oopChallenge/src/Main.java
+++ oopChallenge/src/Main.java
@@ -2,13 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-     Item coke = new Item("drink", "coke",1.6);
-     coke.printItem();
-     coke.setSize("LARGE");
-     coke.printItem();
+//     Item coke = new Item("drink", "coke",1.6);
+//     coke.printItem();
+//     coke.setSize("LARGE");
+//     coke.printItem();
+//
+//     Item avocado = new Item("TOPPING", "avocado",1.4);
+//     avocado.printItem();
 
-     Item avocado = new Item("TOPPING", "avocado",1.4);
-     avocado.printItem();
-
+        Burger burger = new Burger("regular", 4.0);
+        burger.addToppings("BACON","CHEESE","MAYO");
+        burger.printItem();
     }
 }
