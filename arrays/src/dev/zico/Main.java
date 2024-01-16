7e2163951f39c3c51530542be8298323aea3604e
isaac-miles <isaacnice247@gmail.com>
2024-01-16 22:21:02 +0100

    Arrays

--- arrays/src/dev/zico/Main.java
+++ arrays/src/dev/zico/Main.java
@@ -1,11 +1,29 @@
 package dev.zico;
 
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int[] myIntArray = {1,2,3,4,5};
-        myIntArray[4] = 50;
-        System.out.println(myIntArray.length);
+//        int[] myIntArray = {1,2,3,4,5};
+//        myIntArray[4] = 50;
+//        System.out.println(myIntArray.length);
+//        for(int i=1; i<=myIntArray.length-1; i++){
+//            System.out.println(myIntArray[i]);
+//        }
+        int[] newArray;
+        newArray = new int[5];
+        for(int i=0; i< newArray.length; i++){
+            newArray[i] = newArray.length-i;
+            System.out.print(newArray.length-i);
+        }
+        System.out.println();
+        for(int element:newArray){
+            System.out.print(element + " ");
+        }
+        System.out.println(Arrays.toString(newArray));
+
+        Object objectVariable = newArray;
     }
 }
