7959bdc93eed44508c014e7dd52cc4117828414f
isaac-miles <isaacnice247@gmail.com>
2024-01-20 21:47:39 +0100

    Arrays challenge

--- arrays/src/dev/zico/VariableArgs.java
+++ arrays/src/dev/zico/VariableArgs.java
@@ -2,10 +2,15 @@ package dev.zico;
 
 public class VariableArgs {
     public static void main(String... args) {
-
+        System.out.println("Hello world");
+        String[] splitStrings = "Hello miles".split(" ");
+        printText(splitStrings);
+        System.out.println("_".repeat(20));
+        printText("hello");
+        printText();
     }
 
-    public static void printText(String[] texts){
+    public static void printText(String... texts){
         for(String text : texts){
             System.out.println(text);
         }
