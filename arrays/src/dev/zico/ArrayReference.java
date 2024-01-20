7577caa2b9b7e0191a0a11d3c8e657beb290398b
isaac-miles <isaacnice247@gmail.com>
2024-01-20 21:22:32 +0100

    Arrays challenge

--- arrays/src/dev/zico/ArrayReference.java
+++ arrays/src/dev/zico/ArrayReference.java
@@ -1,8 +1,16 @@
 package dev.zico;
 
+import java.util.Arrays;
+
 public class ArrayReference {
     public static void main(String[] args) {
         int[] myIntArray = new int[5];
         int[] anotherArray = myIntArray;
+
+        System.out.println("MyIntArray" + Arrays.toString(myIntArray));
+        System.out.println("MyIntArray" + Arrays.toString(anotherArray));
+        anotherArray[0] = 10;
+
+
     }
 }
