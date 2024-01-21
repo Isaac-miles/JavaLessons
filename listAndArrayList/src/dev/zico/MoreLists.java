2d370faf85cb4ab90d23ffcc257b5ae4b7eb17a5
isaac-miles <isaacnice247@gmail.com>
2024-01-21 21:27:21 +0100

    Arrays challenge

--- listAndArrayList/src/dev/zico/MoreLists.java
+++ listAndArrayList/src/dev/zico/MoreLists.java
@@ -21,5 +21,30 @@ public class MoreLists {
 
         groceries.addAll(nextList);
         System.out.println(groceries);
+
+        System.out.println("Third Item = " + groceries.get(2));
+        if(groceries.contains("yam")){
+            System.out.println("this contains the item searched for");
+        }
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("first = " + groceries.lastIndexOf("yogurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yogurt");
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("apples","eggs")); //removes the specified items
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("apple","yam","cheese")); //this method retains elements in the list
+        System.out.println(groceries);
+        groceries.clear();
+        System.out.println(groceries);
+        System.out.println("groceries empty = "+ groceries.isEmpty());
+
+        groceries.addAll(List.of("apples","milk","mustard"));
     }
 }
