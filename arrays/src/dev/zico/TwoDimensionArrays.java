af2a26cc3cfd04174b6da07f9b9a40b034e0403f
isaac-miles <isaacnice247@gmail.com>
2024-01-21 15:08:13 +0100

    Arrays challenge

--- arrays/src/dev/zico/TwoDimensionArrays.java
+++ arrays/src/dev/zico/TwoDimensionArrays.java
@@ -1,5 +1,13 @@
 package dev.zico;
 
+import java.util.Arrays;
+
 public class TwoDimensionArrays {
-    int[][] array2 = new int[4][4];
+    public static void main(String[] args) {
+        int[][] array2 = new int[4][4];
+
+        for(int[] outer:array2){
+            System.out.println(Arrays.toString(outer));
+        }
+    }
 }
