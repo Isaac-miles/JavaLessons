9a4482963e765ebb172ae3a1d4e1178ebc2124f4
isaac-miles <isaacnice247@gmail.com>
2024-01-21 20:45:35 +0100

    Arrays challenge

---
+++ listAndArrayList/src/dev/zico/MoreLists.java
@@ -0,0 +1,11 @@
+package dev.zico;
+
+import java.util.List;
+
+public class MoreLists {
+    public static void main(String[] args) {
+        String[] items = {"apples","bananas","milk", "eggs"};
+        List<String> list = List.of(items);
+        System.out.println(list);
+    }
+}
