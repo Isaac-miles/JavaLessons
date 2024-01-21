174eab0ceb8ca113588dfac6a2b6a20dd9651124
isaac-miles <isaacnice247@gmail.com>
2024-01-21 20:20:14 +0100

    Arrays challenge

--- listAndArrayList/src/dev/zico/Main.java
+++ listAndArrayList/src/dev/zico/Main.java
@@ -8,6 +8,9 @@ record GroceryItem(String name,String type, int count){
 
 public class Main {
     public static void main(String[] args) {
+        Object[] todoItems = new Object[3];
+        todoItems[0] = new GroceryItem("buy flakes");
+        todoItems[1] = new GroceryItem("cake", "food", 6);
 
     }
 }
