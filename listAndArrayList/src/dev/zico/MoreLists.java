e9b8cb1472bfb6a96a550967fc201383af78be50
isaac-miles <isaacnice247@gmail.com>
2024-01-21 20:54:05 +0100

    Arrays challenge

--- listAndArrayList/src/dev/zico/MoreLists.java
+++ listAndArrayList/src/dev/zico/MoreLists.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class MoreLists {
     public static void main(String[] args) {
         String[] items = {"apples","bananas","milk", "eggs"};
+        //this creates an immutable list items
         List<String> list = List.of(items);
         System.out.println(list);
+        System.out.println(list.getClass().getName());
+//        list.add("yam");
     }
 }
