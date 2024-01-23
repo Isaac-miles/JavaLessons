470ac0115612b8c2c4f242f6800140049820db1f
isaac-miles <isaacnice247@gmail.com>
2024-01-24 00:32:07 +0100

    Arrays challenge

--- listAndArrayList/src/dev/miles/MobilePhone.java
+++ listAndArrayList/src/dev/miles/MobilePhone.java
@@ -24,8 +24,34 @@ class Contact{
 
 public class MobilePhone {
     private String myNumber;
-    ArrayList<Contact> myContact;
+    private ArrayList<Contact> myContact;
+
+    public MobilePhone(String myNumber, ArrayList<Contact> myContact) {
+        this.myNumber = myNumber;
+        this.myContact = myContact;
+    }
+    public boolean addNewContact(Contact contact){
+        int value = findContact(contact);
+        return value >= 0;
+    }
+    public boolean updateContact(Contact oldContact,Contact newContact){
+        int contactIndex = findContact(oldContact);
+        if(contactIndex>=0){
+            System.out.println("contact exists and updated");
+            myContact.set(contactIndex,newContact);
+            return true;
+        }else return false;
+    }
 
 
+    private int findContact(Contact contact){
+       return myContact.indexOf(contact);
+    }
+    public void printContact(){
+
+        for (Contact contact :myContact){
+            System.out.printf("contact List%n %d.->%s%n",myContact.indexOf(contact)+1, contact.getName());
+        }
+    }
 }
 
