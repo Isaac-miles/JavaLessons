cd7f6fb9851f3a54215183883ccddda9b741299c
isaac-miles <isaacnice247@gmail.com>
2024-01-23 23:44:12 +0100

    Arrays challenge

---
+++ listAndArrayList/src/dev/miles/MobilePhone.java
@@ -0,0 +1,14 @@
+package dev.miles;
+
+class Contact{
+    private String name;
+    private String phoneNumber;
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
+public class MobilePhone {
+
+}
+
