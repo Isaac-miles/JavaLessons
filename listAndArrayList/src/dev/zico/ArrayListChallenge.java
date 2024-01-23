2eb310b522ee7e68f616d3afb62659763f75cfa7
isaac-miles <isaacnice247@gmail.com>
2024-01-23 10:54:11 +0100

    Arrays challenge

--- listAndArrayList/src/dev/zico/ArrayListChallenge.java
+++ listAndArrayList/src/dev/zico/ArrayListChallenge.java
@@ -1,10 +1,16 @@
 package dev.zico;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class ArrayListChallenge{
     public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        ArrayList<String> listItems = new ArrayList<>();
+
+    }
+    private static void printMessage(){
 
     }
 }
