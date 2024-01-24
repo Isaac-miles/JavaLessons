f4c76547dfa5e53c4383cfa5c901c51b0ab44222
isaac-miles <isaacnice247@gmail.com>
2024-01-24 23:02:56 +0100

    Linked list challenge

--- linkedList/src/dev/zico/LinkedListChallenge.java
+++ linkedList/src/dev/zico/LinkedListChallenge.java
@@ -6,9 +6,16 @@ record Place(String name, int distance){}
 public class LinkedListChallenge {
     public static void main(String[] args) {
         LinkedList<Place> placesToVisit = new LinkedList<>();
-
+        Place lagos = new Place("lagosBeach", 122);
+        addPlace(placesToVisit,lagos);
+        System.out.println(placesToVisit);
     }
     private static void addPlace(LinkedList<Place> list, Place place){
+        if(list.contains(place)){
+            System.out.println("Duplicate: Place already exists "+place);
+            return;
+        }
 
+        list.add(place);
     }
 }
