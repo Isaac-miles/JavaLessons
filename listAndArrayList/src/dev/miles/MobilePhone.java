5153d2f3573ec559b52d69ae61f9571f609f0d50
isaac-miles <isaacnice247@gmail.com>
2024-01-24 01:38:08 +0100

    Arrays challenge

--- listAndArrayList/src/dev/miles/MobilePhone.java
+++ listAndArrayList/src/dev/miles/MobilePhone.java
@@ -27,47 +27,58 @@ public class MobilePhone {
     private String myNumber;
     private ArrayList<Contact> myContacts;
 
-    public MobilePhone(String myNumber, ArrayList<Contact> myContacts) {
+    public MobilePhone(String myNumber) {
         this.myNumber = myNumber;
-        this.myContacts = myContacts;
+        this.myContacts = new ArrayList<Contact>();
     }
 
+
     public boolean addNewContact(Contact contact){
-        int value = findContact(contact);
-        return value >= 0;
+        int value = findContact(contact.getName());
+        if(value >=0){
+            System.out.println("contact is already on the file");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
     }
     public boolean updateContact(Contact oldContact,Contact newContact){
-        int contactIndex = findContact(oldContact);
-        if(contactIndex>=0){
-            System.out.println("contact exists and updated");
-            myContacts.set(contactIndex,newContact);
+        int contactIndex = findContact(oldContact.getName());
+        if(contactIndex >=0){
+            this.myContacts.set(contactIndex,newContact);
+            System.out.println("contact with the name "+oldContact.getName()+" was updated successfully with "+newContact.getName());
+
             return true;
-        }else return false;
+        }else {
+            System.out.println("contact was not found");
+            return false;
+        }
     }
     public boolean removeContact(Contact contact){
-        int findContact = findContact(contact);
-        if(findContact>=0){
-            myContacts.remove(contact);
-            System.out.println("contact removed successfully");
+        int foundContact = findContact(contact);
+        if(foundContact>=0){
+            myContacts.remove(foundContact);
+            System.out.println(contact.getName()+" was deleted successfully");
             return true;
-        }else return false;
+        }else {
+            System.out.println(contact.getName() + " was not found");
+            return false;
+        }
     }
-//    private int findContact(String contactName){
-//
-//        for(Contact contact:myContact){
-//
+    private int findContact(String contactName){
+//        for(Contact contact:myContacts){
+//            if(contact.getName().equals(contactName)){
+//                return myContacts.indexOf(contact);
+//            }
 //        }
-//        return myContact.indexOf(contactName);
-//    }
-//    public Contact queryContact(String name){
-//       ArrayList<String> contactNames = new ArrayList<>();
-//        for(Contact contact:myContact){
-//
-//           String[] contactName = contact.getName().split(",");
-//           contactNames.addAll(List.of(contactName));
-//        }
-//
-//    }
+        for(int i=0; i<myContacts.size();i++){
+            Contact contact = myContacts.get(i);
+            if(contact.getName().equals(contactName)){
+                return i;
+            }
+        }
+        return -1;
+    }
     private int findContact(Contact contact){
        return myContacts.indexOf(contact);
     }
