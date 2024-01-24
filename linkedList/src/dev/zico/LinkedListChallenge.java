e4547948d2f2002c712afaea0d6154a1a72c94c8
isaac-miles <isaacnice247@gmail.com>
2024-01-24 22:58:25 +0100

    Linked list challenge

--- linkedList/src/dev/zico/LinkedListChallenge.java
+++ linkedList/src/dev/zico/LinkedListChallenge.java
@@ -1,8 +1,14 @@
 package dev.zico;
 
+import java.util.LinkedList;
+
 record Place(String name, int distance){}
 public class LinkedListChallenge {
     public static void main(String[] args) {
+        LinkedList<Place> placesToVisit = new LinkedList<>();
+
+    }
+    private static void addPlace(LinkedList<Place> list, Place place){
 
     }
 }
