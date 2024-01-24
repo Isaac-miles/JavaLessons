cce6b15d530573797cdeebb38b946c473451b5de
isaac-miles <isaacnice247@gmail.com>
2024-01-24 16:42:41 +0100

    Arrays challenge

--- linkedList/src/dev/zico/Main.java
+++ linkedList/src/dev/zico/Main.java
@@ -11,6 +11,9 @@ public class Main {
         System.out.println(placesToVisit);
         addMore(placesToVisit);
         System.out.println(placesToVisit);
+        removeElement(placesToVisit);
+        System.out.println(placesToVisit);
+
     }
     public static void addMore(LinkedList<String> list){
         list.addFirst("abuja");
@@ -18,6 +21,28 @@ public class Main {
         list.offerFirst("yobe");
         list.offerLast("damaturu");
         list.offer("dogo");
+        //Stack methods
+        list.push("Uk");
+    }
+    public static void removeElement(LinkedList<String> list){
+        list.pop();
+        list.remove(2);
+        String pl = list.remove();
+        System.out.println(pl+" was removed");
+
+        String pl1 = list.removeFirst();
+        System.out.println(pl1+" was removed");
+
+        String pl2 = list.removeLast();
+        System.out.println(pl2+" was removed");
+
+        //queue methods
+        String p1 = list.poll();
+        System.out.println(p1+" was removed");
+
+        list.push("finland");
+        System.out.println(list);
 
     }
+
 }
