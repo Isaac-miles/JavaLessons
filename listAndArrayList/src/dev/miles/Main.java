ac0769b0bc285b91b306b60b338babef9c425b1d
isaac-miles <isaacnice247@gmail.com>
2024-01-24 10:31:29 +0100

    Arrays challenge

---
+++ listAndArrayList/src/dev/miles/Main.java
@@ -0,0 +1,9 @@
+package dev.miles;
+
+public class Main {
+    public static void main(String[] args) {
+        MobilePhone mp = new MobilePhone("07030991936");
+        Contact mp1 = Contact.createContact("miles","07030991936");
+
+    }
+}
