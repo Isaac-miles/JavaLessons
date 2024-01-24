e1a966e6d1a11e7b631d01008a008bc39ae2612a
isaac-miles <isaacnice247@gmail.com>
2024-01-24 23:10:42 +0100

    Linked list challenge

--- linkedList/src/dev/zico/LinkedListChallenge.java
+++ linkedList/src/dev/zico/LinkedListChallenge.java
@@ -14,12 +14,20 @@ public class LinkedListChallenge {
         LinkedList<Place> placesToVisit = new LinkedList<>();
         Place lagos = new Place("lagosBeach", 122);
         addPlace(placesToVisit,lagos);
+        addPlace(placesToVisit,lagos);
+
         System.out.println(placesToVisit);
     }
     private static void addPlace(LinkedList<Place> list, Place place){
         if(list.contains(place)){
             System.out.println("Duplicate: Place already exists "+place);
             return;
+        }
+        for(Place p :list){
+            if(p.name().equalsIgnoreCase(place.name())){
+                System.out.println("Duplicate found "+ place);
+                return;
+            }
         }
 
         list.add(place);
