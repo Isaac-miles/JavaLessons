8012522faffbe5c8f8542578f8cf3b8c4662cc79
isaac-miles <isaacnice247@gmail.com>
2024-01-25 16:37:21 +0100

    auto boxing and unboxing

---
+++ linkedList/src/dev/zico/AutoBoxingChallenge.java
@@ -0,0 +1,10 @@
+package dev.zico;
+
+import java.util.ArrayList;
+
+record Customer(String name, ArrayList<Double> transactions){}
+public class AutoBoxingChallenge {
+    public static void main(String[] args) {
+
+    }
+}
