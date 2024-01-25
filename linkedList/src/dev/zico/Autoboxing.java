de9b4fe9ebf88b2acd75724ebf567de5745145af
isaac-miles <isaacnice247@gmail.com>
2024-01-25 11:28:34 +0100

    Linked list challenge

--- linkedList/src/dev/zico/Autoboxing.java
+++ linkedList/src/dev/zico/Autoboxing.java
@@ -3,5 +3,8 @@ package dev.zico;
 public class Autoboxing {
     public static void main(String[] args) {
         Integer boxedInt = Integer.valueOf(15); //preferred but necessary
+//        Integer depreciatedBoxing = new Integer(13);
+        int unboxedInt = boxedInt.intValue(); //unnecessary
+
     }
 }
