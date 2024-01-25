ba8415c1e8b098dd33d6f5fbd825042aedd519d2
isaac-miles <isaacnice247@gmail.com>
2024-01-25 11:45:04 +0100

    Linked list challenge

--- linkedList/src/dev/zico/Autoboxing.java
+++ linkedList/src/dev/zico/Autoboxing.java
@@ -6,5 +6,18 @@ public class Autoboxing {
 //        Integer depreciatedBoxing = new Integer(13);
         int unboxedInt = boxedInt.intValue(); //unnecessary
 
+        //Automatic
+        Integer autoBox = 15;
+        int autoUnboxed = autoBox;
+        System.out.println(autoBox.getClass().getName());
+
+        Double resultBoxed = getDoubleObject(); //java compiler automatically boxing it because its being assign to a java.lan.Double class
+        double resultUnboxed = getDoubleLiteralPrimitive(); //same as this auto unboxing
+    }
+    private static Double getDoubleObject(){
+        return Double.valueOf(100.0);
+    }
+    private static Double getDoubleLiteralPrimitive(){
+        return 100.0;
     }
 }
