b6901ab5c9cc169d464f1798eaffe6149d9edbb0
isaac-miles <isaacnice247@gmail.com>
2024-01-27 02:29:26 +0100

    auto boxing and unboxing

--- linkedList/src/dev/zico/AutoBoxChallenge.java
+++ linkedList/src/dev/zico/AutoBoxChallenge.java
@@ -10,56 +10,58 @@ record CustomerA(String name, ArrayList<Double> transactions){
 }
 public class AutoBoxChallenge {
     public static void main(String[] args) {
-        Customer Isaac = new Customer("Isaac", 100);
+        CustomerA Isaac = new CustomerA("Isaac", 100);
 //        System.out.println(Isaac);
-
+        Bankk accessBank = new Bankk("Access Bank");
+        accessBank.addNewCustomer(Isaac);
+        accessBank.addTransactions("Isaac", -10);
+        accessBank.printTransactions("Isaac");
     }
+}
+class Bankk{
+    private String name;
+    private ArrayList<CustomerA> customers = new ArrayList<>(1000);
 
-class Bank{
-        private String name;
-        private ArrayList<Customer> customers = new ArrayList<>(1000);
-
-        public Bank(String name){
-            this.name = name.toUpperCase();
+    public Bankk(String name){
+        this.name = name.toUpperCase();
+    }
+    private CustomerA findCustomer(String customerName){
+        for(var customer:customers){
+            if(customer.name().equalsIgnoreCase(customerName)){
+                return customer;
+            }
         }
-    private Customer findCustomer(String customerName){
-          for(var customer:customers){
-              if(customer.name().equalsIgnoreCase(customerName)){
-                  return customer;
-              }
-          }
 
-            return null;
+        return null;
     }
-    private void addNewCustomer(Customer newCustomer){
-            Customer findCus = findCustomer(newCustomer.name());
-          for(var customer: customers){
-              if(findCus == null){
-                  customers.add(newCustomer);
-              }
-          }
+    public void addNewCustomer(CustomerA newCustomer){
+        CustomerA findCus = findCustomer(newCustomer.name());
+        for(var customer: customers){
+            if(findCus == null){
+                customers.add(newCustomer);
+            }
+        }
         System.out.println("Customer already exists " + newCustomer);
     }
 
-    private  void addTransactions(String customerName, double transaction){
-            Customer findCus = findCustomer(customerName);
-            if (findCus !=null){
-                findCus.transactions().add(transaction);
-            }
+    public   void addTransactions(String customerName, double transaction){
+        CustomerA findCus = findCustomer(customerName);
+        if (findCus !=null){
+            findCus.transactions().add(transaction);
+        }
         System.out.println("customer not found "+ findCus);
     }
 
     public void printTransactions(String customerName){
-            Customer findCus = findCustomer(customerName);
+        CustomerA findCus = findCustomer(customerName);
 
-            if(findCus == null){
-                System.out.println("customer with transactions not found "+ customerName);
-            }
-            for(var tran: findCus.transactions()){
-                System.out.println("__".repeat(30));
-                System.out.printf("%s%n %10.2f (%s)", findCus.name(),tran, tran<0?"debit":"credit");
+        if(findCus == null){
+            System.out.println("customer with transactions not found "+ customerName);
+        }
+        for(var tran: findCus.transactions()){
+            System.out.println("__".repeat(30));
+            System.out.printf("%s%n %10.2f (%s)", findCus.name(),tran, tran<0?"debit":"credit");
 
-            }
+        }
     }
-}
 }
