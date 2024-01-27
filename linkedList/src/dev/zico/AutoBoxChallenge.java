b77ccf065c53e9ab2f0c84dc57d56116a0a2ad22
isaac-miles <isaacnice247@gmail.com>
2024-01-27 01:47:53 +0100

    auto boxing and unboxing

--- linkedList/src/dev/zico/AutoBoxChallenge.java
+++ linkedList/src/dev/zico/AutoBoxChallenge.java
@@ -11,7 +11,8 @@ record CustomerA(String name, ArrayList<Double> transactions){
 public class AutoBoxChallenge {
     public static void main(String[] args) {
         Customer Isaac = new Customer("Isaac", 100);
-        System.out.println(Isaac);
+//        System.out.println(Isaac);
     }
+
 
 }
