5510dc261ea45c461b6d8e146c00e9cf325d9e92
isaac-miles <isaacnice247@gmail.com>
2024-01-27 01:36:32 +0100

    auto boxing and unboxing

--- linkedList/src/dev/zico/AutoBoxingChallenge.java
+++ linkedList/src/dev/zico/AutoBoxingChallenge.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 record Customer(String name, ArrayList<Double> transactions){
     public Customer(String name,double initialDeposit){
-        this(name.toUpperCase(),new ArrayList<Double>(500));
+        this(name.toUpperCase(),new ArrayList<>(500));
         transactions.add(initialDeposit);
     }
 }
