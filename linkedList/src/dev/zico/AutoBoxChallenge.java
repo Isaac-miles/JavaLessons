d07e3bd4202721f1760f919ce90464132bf5925e
isaac-miles <isaacnice247@gmail.com>
2024-01-27 17:10:22 +0100

    auto boxing and unboxing

--- linkedList/src/dev/zico/AutoBoxChallenge.java
+++ linkedList/src/dev/zico/AutoBoxChallenge.java
@@ -14,8 +14,8 @@ public class AutoBoxChallenge {
 //        System.out.println(Isaac);
         Bankk accessBank = new Bankk("Access Bank");
         Bankk bank = new Bankk("uba");
-        accessBank.addNewCustomer(Isaac);
-        accessBank.addNewCustomer(Isaac);
+        accessBank.addNewCustomer("miles", 200);
+        accessBank.addNewCustomer("miles", 100);
 //        accessBank.addTransactions("Isaac", -10);
 //        accessBank.printTransactions("isaac");
 //        System.out.println(accessBank);
