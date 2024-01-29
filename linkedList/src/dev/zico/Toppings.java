3e82c4a966d4604a8ae81a031bcda4df3cb4dc80
isaac-miles <isaacnice247@gmail.com>
2024-01-29 22:18:35 +0100

    The enum type

--- linkedList/src/dev/zico/Toppings.java
+++ linkedList/src/dev/zico/Toppings.java
@@ -1,4 +1,16 @@
 package dev.zico;
 
 public enum Toppings {
+    GREEN,
+    PEREWINKO,
+    BACON,
+    TOMATOES;
+
+    public double getPrice(){
+        return switch (this){
+            case BACON -> 1.5;
+            case GREEN -> 2.0;
+            default -> 0.0;
+        };
+    }
 }
