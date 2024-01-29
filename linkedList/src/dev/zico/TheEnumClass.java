c1b508611c8f1c56044b74135439c30a3641817b
isaac-miles <isaacnice247@gmail.com>
2024-01-29 21:51:11 +0100

    The enum type

--- linkedList/src/dev/zico/TheEnumClass.java
+++ linkedList/src/dev/zico/TheEnumClass.java
@@ -4,5 +4,8 @@ public class TheEnumClass {
     public static void main(String[] args) {
         EnumTypeExample weekdays = EnumTypeExample.WED;
         System.out.println(weekdays);
+
+        System.out.printf("Name is %s, Ordinal Value = %d%n",weekdays.name(), weekdays.ordinal());
+
     }
 }
