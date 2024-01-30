a2f373a4aa0272988b1853d24c1ad629494eda38
isaac-miles <isaacnice247@gmail.com>
2024-01-30 21:57:05 +0100

    Abstract classes

--- AbstractClasses/src/dev/zico/Main.java
+++ AbstractClasses/src/dev/zico/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
     Dog dog = new Dog("wolf","big",100);
     dog.makeNoise();
+
+    doAnimalStuffs(dog);
+    }
+    private static void doAnimalStuffs(Animal animal){
+        animal.makeNoise();
+        animal.move("fast");
     }
 }
