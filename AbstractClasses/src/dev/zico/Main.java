ca91e7e4e1b77e89a7979feea4dcf5266b7aa835
isaac-miles <isaacnice247@gmail.com>
2024-01-30 22:05:45 +0100

    Abstract classes

--- AbstractClasses/src/dev/zico/Main.java
+++ AbstractClasses/src/dev/zico/Main.java
@@ -1,11 +1,15 @@
 package dev.zico;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
     Dog dog = new Dog("wolf","big",100);
     dog.makeNoise();
 
     doAnimalStuffs(dog);
+        ArrayList<Animal> abimals = new ArrayList<>();
+
     }
     private static void doAnimalStuffs(Animal animal){
         animal.makeNoise();
