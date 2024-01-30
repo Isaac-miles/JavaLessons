8f432f1a4588e6a74a4c638a50a261ae427b011e
isaac-miles <isaacnice247@gmail.com>
2024-01-30 22:15:23 +0100

    Abstract classes

--- AbstractClasses/src/dev/zico/Animal.java
+++ AbstractClasses/src/dev/zico/Animal.java
@@ -13,7 +13,7 @@ public abstract class Animal {
     public abstract void move(String speed);
     public abstract void makeNoise();
 
-    public String getExplicitType(){
+    public final String getExplicitType(){
        return (getClass().getSimpleName() + " ("+ type+ " )");
     }
 
