53c4fcb281b8381d30e81e4a94448818765b5043
isaac-miles <isaacnice247@gmail.com>
2024-01-31 19:17:28 +0100

    Abstract classes challenge

---
+++ AbstractClasses/src/dev/abstractChallenge/ArtObject.java
@@ -0,0 +1,12 @@
+package dev.abstractChallenge;
+
+public class ArtObject extends ProductForSale{
+    public ArtObject(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+    }
+}
