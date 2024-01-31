5a32bb246d3ba1d75d191f580576a1c30079cd5e
isaac-miles <isaacnice247@gmail.com>
2024-02-01 00:11:08 +0100

    Interfaces

--- Interfaces/src/dev/zico/Animal.java
+++ Interfaces/src/dev/zico/Animal.java
@@ -1,5 +1,32 @@
 package dev.zico;
 
+
+class Satellite implements OrbitEarth{
+
+    @Override
+    public void achieveOrbit() {
+        System.out.println("orbit achieved");
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled{
+    void achieveOrbit();
+}
 interface FlightEnabled{
     double MILES_TO_KM = 1.60934;
     double KM_TO_MILES = 0.621371;
