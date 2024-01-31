922c8650c11092c6ff55d077ef46659b57026704
isaac-miles <isaacnice247@gmail.com>
2024-01-31 19:20:08 +0100

    Abstract classes challenge

--- AbstractClasses/src/dev/abstractChallenge/Store.java
+++ AbstractClasses/src/dev/abstractChallenge/Store.java
@@ -1,6 +1,9 @@
 package dev.abstractChallenge;
 
+import java.util.ArrayList;
+
 public class Store {
+    private static ArrayList<ProductForSale> storeProducts =new ArrayList<>();
     public static void main(String[] args) {
 
     }
