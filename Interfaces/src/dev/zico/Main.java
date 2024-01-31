f1b357be58a0010de8f8cf6a96b37669034aa72b
isaac-miles <isaacnice247@gmail.com>
2024-02-01 00:29:31 +0100

    Interfaces

--- Interfaces/src/dev/zico/Main.java
+++ Interfaces/src/dev/zico/Main.java
@@ -1,5 +1,8 @@
 package dev.zico;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Bird bird = new Bird();
@@ -23,6 +26,13 @@ public class Main {
        double kmsTraveled = 100;
        double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
         System.out.printf("the truck traveled %.2f km or %.2f miles%n",kmsTraveled,milesTraveled);
+
+        //using abstracted refrence type which is preferred
+        ArrayList<FlightEnabled> fliers = new ArrayList<>(); //remember an array list uses the list interface type!!
+        fliers.add(bird);
+        List<FlightEnabled> betterFliers = new ArrayList<>(); // this is preferred
+        betterFliers.add(bird);
+
     }
     private static void inFlightBound(FlightEnabled flier){
         flier.takeOff();
