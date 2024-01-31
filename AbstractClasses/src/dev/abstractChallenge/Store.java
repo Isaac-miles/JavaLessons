848c160818c5aefb54cb56817b878fc60783a517
isaac-miles <isaacnice247@gmail.com>
2024-01-31 19:22:51 +0100

    Abstract classes challenge

--- AbstractClasses/src/dev/abstractChallenge/Store.java
+++ AbstractClasses/src/dev/abstractChallenge/Store.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Store {
     private static ArrayList<ProductForSale> storeProducts =new ArrayList<>();
     public static void main(String[] args) {
+        storeProducts.add(new ArtObject("NFT", 150,"An epic art of wonders"));
+        storeProducts.add(new ArtObject("Nature painting", 170,"Nature at it's best"));
 
     }
 }
