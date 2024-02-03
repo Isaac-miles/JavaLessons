90d68add98167af83c879b1c8ab8312135dc44b8
isaac-miles <isaacnice247@gmail.com>
2024-02-03 23:27:00 +0100

    Interfaces challenge

--- Interfaces/src/dev/miles/Mappable.java
+++ Interfaces/src/dev/miles/Mappable.java
@@ -2,7 +2,7 @@ package dev.miles;
 
 public interface Mappable {
     String JSON_PROPERTY = """
-            "properties": {%s}
+            "properties":{%s}
             """;
     String getLabel();
     Geometry getShape();
@@ -13,11 +13,11 @@ public interface Mappable {
                 "type": "%s", "label": "%s", "marker": "%s"
                 """.formatted(getShape(),getLabel(),getMarker());
     }
+//    static void mapIt(Mappable mappable){
+//        System.out.println(JSON_PROPERTY.formatted(mappable.toJSON()));
+//    }
     static void mapIt(Mappable mappable){
-        System.out.println(JSON_PROPERTY.formatted(mappable.toJSON()));
-    }
-    static void mapIt1(Mappable mappable){
-        System.out.printf((JSON_PROPERTY) , mappable.toJSON());
+        System.out.printf((JSON_PROPERTY) + "%n", mappable.toJSON());
     }
 }
 
