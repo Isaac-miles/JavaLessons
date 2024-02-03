384df65a1fd527cc88fb278692be8fcdd0d36a7f
isaac-miles <isaacnice247@gmail.com>
2024-02-03 23:14:57 +0100

    Interfaces challenge

--- Interfaces/src/dev/miles/Main.java
+++ Interfaces/src/dev/miles/Main.java
@@ -1,7 +1,11 @@
 package dev.miles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Mappable> mappables = new ArrayList<>();
 
     }
 }
