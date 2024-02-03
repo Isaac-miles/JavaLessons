8e5c3185bbc338b5a1e5f89bf679b600eb784e7b
isaac-miles <isaacnice247@gmail.com>
2024-02-03 23:07:02 +0100

    Interfaces challenge

--- Interfaces/src/dev/miles/BUILDING.java
+++ Interfaces/src/dev/miles/BUILDING.java
@@ -1,5 +1,28 @@
 package dev.miles;
 
-public class BUILDING {
+public class BUILDING implements Mappable{
+    private String name;
+    private  UsageType usage;
 
+    public BUILDING(String name, UsageType usage) {
+        this.name = name;
+        this.usage = usage;
+    }
+
+    @Override
+    public String getLabel() {
+        return null;
+    }
+
+    @Override
+    public Geometry getShape() {
+        return null;
+    }
+
+    @Override
+    public String getMarker() {
+        return null;
+    }
 }
+
+enum UsageType {ENTERTAINMENT, GOVERNMENT, RESIDENTIAL, SPORTS}
