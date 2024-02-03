70bb63886838104296641088bf08a36bdbaa00eb
isaac-miles <isaacnice247@gmail.com>
2024-02-03 23:18:21 +0100

    Interfaces challenge

--- Interfaces/src/dev/miles/Main.java
+++ Interfaces/src/dev/miles/Main.java
@@ -7,5 +7,8 @@ public class Main {
     public static void main(String[] args) {
         List<Mappable> mappables = new ArrayList<>();
 
+        mappables.add(new Building("Lagos Ikeja Mall",UsageType.ENTERTAINMENT));
+        mappables.add(new Building("Aso rock",UsageType.GOVERNMENT));
+        mappables.add(new Building("Tesling Balogun Stadium",UsageType.SPORTS));
     }
 }
