1d741b17dadf14f019755e089177d0ccb5383ab4
isaac-miles <isaacnice247@gmail.com>
2024-02-03 23:00:04 +0100

    Interfaces challenge

--- Interfaces/src/dev/miles/Mappable.java
+++ Interfaces/src/dev/miles/Mappable.java
@@ -10,6 +10,15 @@ public interface Mappable {
 
     default String toJSON(){
         return """
-                """;
+                "type": "%s", "label": "%s", "marker": "%s"
+                """.formatted(getShape(),getLabel(),getMarker());
+    }
+    static void mapIt(Mappable mappable){
+        System.out.println(JSON_PROPERTY.formatted(mappable.toJSON()));
+    }
+    static void mapIt1(Mappable mappable){
+        System.out.printf((JSON_PROPERTY) + "%n", mappable.toJSON());
     }
 }
+
+enum Geometry{LINE,POINT,POLYGON}
