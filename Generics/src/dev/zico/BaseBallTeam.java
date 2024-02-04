2ead122b0985827bb7bf4ad0fd7775e2ed11bd8a
isaac-miles <isaacnice247@gmail.com>
2024-02-04 21:30:03 +0100

    Interfaces challenge

--- Generics/src/dev/zico/BaseBallTeam.java
+++ Generics/src/dev/zico/BaseBallTeam.java
@@ -1,5 +1,13 @@
 package dev.zico;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseBallTeam {
+    private String teamName;
+    private List<BaseBallPlayer> teamMembers = new ArrayList<>();
+    private int totalWins = 0;
+    private int totalLosses = 0;
+    private int totalTies = 0;
 
 }
