d913c0c31e611324477ffe58f3f5aad57eab244d
isaac-miles <isaacnice247@gmail.com>
2024-02-04 21:44:53 +0100

    Interfaces challenge

--- Generics/src/dev/zico/Main.java
+++ Generics/src/dev/zico/Main.java
@@ -2,7 +2,8 @@ package dev.zico;
 
 public class Main {
     public static void main(String[] args) {
-
+        BaseBallTeam lakers = new BaseBallTeam("Lakers");
+        BaseBallTeam olandos = new BaseBallTeam("Olandos");
     }
 }
 
