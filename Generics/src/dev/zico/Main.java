a1352ffe43924c7078c442dfa3b6f88f05bfc71d
isaac-miles <isaacnice247@gmail.com>
2024-02-04 21:47:40 +0100

    Interfaces challenge

--- Generics/src/dev/zico/Main.java
+++ Generics/src/dev/zico/Main.java
@@ -5,6 +5,12 @@ public class Main {
         BaseBallTeam lakers = new BaseBallTeam("Lakers");
         BaseBallTeam olandos = new BaseBallTeam("Olandos");
     }
+    public static void scoreResult(BaseBallTeam team1, int t1_score,
+                                   BaseBallTeam team2, int t2_score){
+        String message = team1.setScore(t1_score,t2_score);
+        team2.setScore(t2_score,t1_score);
+        System.out.printf("%s %s %s %n", team1,message,team2);
+    }
 }
 
 record BaseBallPlayer(String name, String position){}
