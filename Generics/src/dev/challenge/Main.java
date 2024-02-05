52c7eec546677cc8b64156ae3d150d1d4fdf92ac
isaac-miles <isaacnice247@gmail.com>
2024-02-06 00:20:35 +0100

    Generics challenge

--- Generics/src/dev/challenge/Main.java
+++ Generics/src/dev/challenge/Main.java
@@ -2,12 +2,12 @@ package dev.challenge;
 
 public class Main {
     public static void main(String[] args) {
-        var nationalNGParks = new Part[]{
-                new Part("Yankari","22.1442, -204.45213"),
-                new Part("Abia reserves", "332.4762, -101.9848"),
-                new Part("Elegushi","28.4245, -023.23424")
+        var nationalNGParks = new Park[]{
+                new Park("Yankari","22.1442, -204.45213"),
+                new Park("Abia reserves", "332.4762, -101.9848"),
+                new Park("Elegushi","28.4245, -023.23424")
         };
-        Layer<Part> parkLayer = new Layer<>(nationalNGParks);
+        Layer<Park> parkLayer = new Layer<>(nationalNGParks);
         parkLayer.renderLayer();
     }
 }
