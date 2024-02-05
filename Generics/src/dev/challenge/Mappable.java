6a14ff35764b16fc6cae8cea16862e1cb926a80b
isaac-miles <isaacnice247@gmail.com>
2024-02-05 21:35:21 +0100

    Generics

--- Generics/src/dev/challenge/Mappable.java
+++ Generics/src/dev/challenge/Mappable.java
@@ -1,4 +1,11 @@
 package dev.challenge;
 
 public interface Mappable {
+    void render();
+    static double[] stringToLocation(String location){
+        var splits = location.split(",");
+        double lat = Double.parseDouble(splits[0]);
+        double lng = Double.parseDouble(splits[1]);
+        return new double[]{lat,lng};
+    }
 }
