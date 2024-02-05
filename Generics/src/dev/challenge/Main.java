bde5ee47e5e7078d8122896b23630cbedb7590ae
isaac-miles <isaacnice247@gmail.com>
2024-02-06 00:14:43 +0100

    Generics challenge

--- Generics/src/dev/challenge/Main.java
+++ Generics/src/dev/challenge/Main.java
@@ -2,6 +2,10 @@ package dev.challenge;
 
 public class Main {
     public static void main(String[] args) {
-
+        var nationalNGParks = new Part[]{
+                new Part("Yankari","22.1442, -204.45213"),
+                new Part("Abia reserves", "332.4762, -101.9848"),
+                new Part("Elegushi","28.4245, -023.23424")
+        };
     }
 }
