88b27e9cb158abb91b31db792e69ed5db1e9e59e
isaac-miles <isaacnice247@gmail.com>
2024-02-05 12:29:19 +0100

    Generics

--- Generics/src/dev/zico/Main.java
+++ Generics/src/dev/zico/Main.java
@@ -2,13 +2,22 @@ package dev.zico;
 
 public class Main {
     public static void main(String[] args) {
-        BaseBallTeam lakers = new BaseBallTeam("Lakers");
-        BaseBallTeam olandos = new BaseBallTeam("Olandos");
-        scoreResult(lakers,5,olandos,4);
+        BaseBallTeam lakers1 = new BaseBallTeam("Lakers");
+        BaseBallTeam olandos1 = new BaseBallTeam("Olandos");
+        scoreResult(lakers1,5,olandos1,4);
+
+        SportTeam lakers = new SportTeam("Lakers");
+        SportTeam olandos = new SportTeam("Olandos");
+//        scoreResult(lakers,5,olandos,4);
 
         lakers.addTeamMember(new BaseBallPlayer("Kobi","Attacker"));
         olandos.addTeamMember(new BaseBallPlayer("Mosh", "defender"));
         olandos.listTeamMembers();
+
+        SportTeam afa = new SportTeam("Ajaokuata");
+        var baron = new FootballPlayer("Baron","CDM");
+        afa.addTeamMember(baron);
+        afa.listTeamMembers();
     }
     public static void scoreResult(BaseBallTeam team1, int t1_score,
                                    BaseBallTeam team2, int t2_score){
@@ -16,6 +25,14 @@ public class Main {
         team2.setScore(t2_score,t1_score);
         System.out.printf("%s %s %s %n", team1,message,team2);
     }
+    public static void scoreResult(SportTeam team1, int t1_score,
+                                   SportTeam team2, int t2_score){
+        String message = team1.setScore(t1_score,t2_score);
+        team2.setScore(t2_score,t1_score);
+        System.out.printf("%s %s %s %n", team1,message,team2);
+
+    }
 }
 
-record BaseBallPlayer(String name, String position){}
+record BaseBallPlayer(String name, String position) implements Player{}
+record FootballPlayer(String name, String position) implements Player{}
