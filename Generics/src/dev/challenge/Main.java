b82c77dc2cfe200b49d0912dc6dbc9f7c3829a95
isaac-miles <isaacnice247@gmail.com>
2024-02-06 00:16:39 +0100

    Generics challenge

--- Generics/src/dev/challenge/Main.java
+++ Generics/src/dev/challenge/Main.java
@@ -7,5 +7,7 @@ public class Main {
                 new Part("Abia reserves", "332.4762, -101.9848"),
                 new Part("Elegushi","28.4245, -023.23424")
         };
+        Layer<Part> parkLayer = new Layer<>(nationalNGParks);
+        parkLayer.renderLayer();
     }
 }
