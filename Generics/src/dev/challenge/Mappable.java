0f77b60866ae959ee57e47735924da3f7de0fa66
isaac-miles <isaacnice247@gmail.com>
2024-02-05 21:40:14 +0100

    Generics

--- Generics/src/dev/challenge/Mappable.java
+++ Generics/src/dev/challenge/Mappable.java
@@ -1,5 +1,7 @@
 package dev.challenge;
 
+import java.util.Arrays;
+
 public interface Mappable {
     void render();
     static double[] stringToLocation(String location){
@@ -7,5 +9,17 @@ public interface Mappable {
         double lat = Double.parseDouble(splits[0]);
         double lng = Double.parseDouble(splits[1]);
         return new double[]{lat,lng};
+    }
+}
+
+abstract class Point implements Mappable{
+    private double[] location = new double[2];
+
+    @Override
+    public void render() {
+
+    }
+    private String location(){
+        return Arrays.toString(location);
     }
 }
