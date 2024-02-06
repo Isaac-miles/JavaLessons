242f2b7f9948a1bc421188204aa76758de1c3449
isaac-miles <isaacnice247@gmail.com>
2024-02-06 02:05:34 +0100

    Comparator interface

--- Generics/src/dev/zico/Comparing.java
+++ Generics/src/dev/zico/Comparing.java
@@ -77,6 +77,6 @@ class Student implements Comparable<Student>{
 class StudentGPAComparator implements Comparator<Student>{
     @Override
     public int compare(Student o1, Student o2) {
-        return  (o1.gpa + o1.name).compareTo(o2.gpa+o2.name);
+        return  (o2.gpa + o2.name).compareTo(o1.gpa + o1.name);
     }
 }
