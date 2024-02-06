df42ed3c9b973e470638f5673d99d70eafbe85db
isaac-miles <isaacnice247@gmail.com>
2024-02-06 14:44:58 +0100

    Generics extras

--- Generics/src/dev/util/QueryList.java
+++ Generics/src/dev/util/QueryList.java
@@ -8,8 +8,11 @@ public class QueryList <T extends QueryItem>{
     public QueryList(List<T> items){
         this.items = items;
     }
-    public static <T extends QueryItem> List<T> getMatches(List<T> items, String field, String value){
-        List<T> matches = new ArrayList<>();
+
+    //a generic method type is unrelated to the generic type declared on the generic class
+
+    public static <S extends QueryItem> List<S> getMatches(List<S> items, String field, String value){
+        List<S> matches = new ArrayList<>();
         for(var item: items){
             if(item.matchFieldValues(field,value)){
                 matches.add(item);
