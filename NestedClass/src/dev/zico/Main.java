341b03f8ed385d4e721bcc5f42d81a9e8e302818
isaac-miles <isaacnice247@gmail.com>
2024-02-06 23:54:00 +0100

    Nested classes

--- NestedClass/src/dev/zico/Main.java
+++ NestedClass/src/dev/zico/Main.java
@@ -1,7 +1,25 @@
 package dev.zico;
 
+import dev.miles.Employee;
+import dev.miles.EmployeeComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>(List.of(
+                new Employee(101,"Samuel",2024),
+                new Employee(102,"Ema",2024),
+                new Employee(103,"Joy",2027),
+                new Employee(104,"Tomi",2025),
+                new Employee(105,"Tete",2026) ));
+        var comaprator = new EmployeeComparator<>();
+        employees.sort(comaprator);
 
+        for(Employee e: employees){
+            System.out.println(e);
+        }
     }
+
 }
