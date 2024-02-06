22d6f8afb7d3af14de13ae64551c5a36375c3235
isaac-miles <isaacnice247@gmail.com>
2024-02-07 00:17:43 +0100

    Nested classes

--- NestedClass/src/dev/miles/StoreEmployee.java
+++ NestedClass/src/dev/miles/StoreEmployee.java
@@ -1,6 +1,19 @@
 package dev.miles;
 
+import java.util.Comparator;
+
 public class StoreEmployee extends Employee {
     private String store;
 
+    public  class StoreComparator<T extends StoreEmployee> implements Comparator<StoreEmployee>{
+
+        @Override
+        public int compare(StoreEmployee o1, StoreEmployee o2) {
+            int result = o1.store.compareTo(o2.store);
+            if(result==0){
+                return new Employee.EmployeeComparator<>("yearStarted").compare(o1,o2);
+            }
+            return result;
+        }
+    }
 }
