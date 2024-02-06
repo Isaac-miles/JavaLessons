c7049fdc4fbd6a74a02795e6220cc1d967639003
isaac-miles <isaacnice247@gmail.com>
2024-02-06 10:59:32 +0100

    Generics extras

--- Generics/src/dev/model/ZCOStudent.java
+++ Generics/src/dev/model/ZCOStudent.java
@@ -7,4 +7,13 @@ public class ZCOStudent extends Student{
     public ZCOStudent(){
         percentComplete = random.nextDouble(0,100.001);
     }
+
+    @Override
+    public String toString() {
+        return "%s %8.1f%%".formatted(super.toString(), percentComplete);
+    }
+
+    public double getPercentComplete() {
+        return percentComplete;
+    }
 }
