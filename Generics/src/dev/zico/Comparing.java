d35d4fe37538bd5c5bc520573abda3af4add92b1
isaac-miles <isaacnice247@gmail.com>
2024-02-06 02:12:26 +0100

    Comparator interface

--- Generics/src/dev/zico/Comparing.java
+++ Generics/src/dev/zico/Comparing.java
@@ -58,7 +58,7 @@ class Student implements Comparable<Student>{
     }
     @Override
     public int compareTo(Student o) {
-        return Integer.valueOf(id).compareTo(Integer.valueOf(o.id));
+        return Integer.compare(id, o.id);
     }
 
     //used for comparable example
