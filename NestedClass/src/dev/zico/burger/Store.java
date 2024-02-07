fa5302b25572980711acb7d4337c6cbc12fc6895
isaac-miles <isaacnice247@gmail.com>
2024-02-07 02:34:58 +0100

    Nested classes

--- NestedClass/src/dev/zico/burger/Store.java
+++ NestedClass/src/dev/zico/burger/Store.java
@@ -3,7 +3,7 @@ package dev.zico.burger;
 public class Store {
     public static void main(String[] args) {
         Meal meal = new Meal();
-        meal.addToppings("Cheese","Mayo","ketchup");
+        meal.addToppings("Cheese","Mayo","ketchup","Smoove");
         System.out.println(meal);
 
         Meal NGRegularMeal = new Meal(1_450);
