35389aded81de044d9e2f5c39785b33b27bd928a
isaac-miles <isaacnice247@gmail.com>
2024-02-12 17:03:03 +0100

    lambda expression

--- LambdaEpression/src/Main.java
+++ LambdaEpression/src/Main.java
@@ -1,5 +1,11 @@
 
 public class Main {
+    record Person(String firstName, String lastName){
+        @Override
+        public String toString() {
+            return firstName+" "+lastName;
+        }
+    }
     public static void main(String[] args) {
 
     }
