fdb4649878a8d563682261a8197530c5cc259c02
isaac-miles <isaacnice247@gmail.com>
2024-02-12 21:59:22 +0100

    lambda expression consumer interface

--- LambdaEpression/src/dev/zico/ConsumerInterface.java
+++ LambdaEpression/src/dev/zico/ConsumerInterface.java
@@ -1,7 +1,10 @@
 package dev.zico;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
 
 public class ConsumerInterface {
     public static void main(String[] args) {
@@ -20,9 +23,28 @@ public class ConsumerInterface {
         var result2 = calculator((a, b)-> a / b, 5.5,4.5);
         var result3 = calculator((a,b)->a.toUpperCase()+" "+ b.toUpperCase(),"Josiah"," Isaac");
 
+        //using the consumer and the biConsumer
+        var coords = Arrays.asList(
+                new double[]{47.2160, -75.2946},
+                new double[]{29.2160, -89.2946},
+                new double[]{35.2160, -95.2946});
+        coords.forEach(s-> System.out.println(Arrays.toString(s)));
 
+        BiConsumer<Double,Double> p1 = (lat,lon)-> System.out.printf("[lat:%.3f lon:%.3f]%n",lat,lon);
     }
-    public static  <T> T calculator(Operations<T> function, T value1, T value2){
+    //using java util
+    public static  <T> T calculator(BinaryOperator<T> function, T value1, T value2){
+        T result = function.apply(value1, value2);
+        System.out.println("Result of operation: " + result);
+        return result;
+    }
+
+    public static <T> void processPoint(T t1, T t2, BiConsumer<T,T> consumer){
+        consumer.accept(t1,t2);
+    }
+
+    //created
+    public static  <T> T calculator1(Operations<T> function, T value1, T value2){
         T result = function.operate(value1, value2);
         System.out.println("Result of operation: " + result);
         return result;
