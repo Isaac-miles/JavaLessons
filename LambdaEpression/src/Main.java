aeb4d4d9800bbb7650a194802c22c226bdde898a
isaac-miles <isaacnice247@gmail.com>
2024-02-12 17:07:18 +0100

    lambda expression

--- LambdaEpression/src/Main.java
+++ LambdaEpression/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     record Person(String firstName, String lastName){
@@ -7,6 +10,13 @@ public class Main {
         }
     }
     public static void main(String[] args) {
+        List<Person> people = new ArrayList<>(Arrays.asList(
+                new Person("Miles","Davies"),
+                new Main.Person("Davies","Isaac"),
+                new Person("Helen", "Bright"),
+                new Person("Charlie", "Brown")
+                ));
+        //using anonymous class
 
     }
 }
