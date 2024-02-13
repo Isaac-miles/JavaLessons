1204b1089cd2c820de2bd5d4b9134805df522045
isaac-miles <isaacnice247@gmail.com>
2024-02-14 00:16:29 +0100

    supplierInterface

--- LambdaEpression/src/dev/challenge/SupplierLambdaExpression.java
+++ LambdaEpression/src/dev/challenge/SupplierLambdaExpression.java
@@ -1,7 +1,13 @@
 package dev.challenge;
 
+import java.util.function.Supplier;
+
 public class SupplierLambdaExpression {
     public static void main(String[] args) {
-
+        Supplier<String> message = ()->{
+            return "I Love Java";
+        };
+       String iLovejava  = message.get();
+        System.out.println(iLovejava);
     }
 }
