96cadfa6663aee9acf36ab31707ad8843834e4b8
isaac-miles <isaacnice247@gmail.com>
2024-02-14 00:11:57 +0100

    function Interface

---
+++ LambdaEpression/src/dev/challenge/StringBuilderLambda.java
@@ -0,0 +1,40 @@
+package dev.challenge;
+
+import java.util.function.Function;
+
+public class StringBuilderLambda {
+    public static void main(String[] args) {
+        //or you can replace Function with UnaryOperator<String>
+        Function<String, String> everyScondChar = s -> {
+            StringBuilder returnVal = new StringBuilder();
+            for(int i=0; i<s.length(); i++){
+                if(i % 2 == 1){
+                    returnVal.append(s.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+        System.out.println(everyScondChar.apply("1234567890"));
+
+        System.out.println(everySecondCharacter(everyScondChar,"1234567890"));
+
+    }
+
+
+
+    //change the following method to a lambda expression
+    public static String everySecondCharQuestion(String source){
+        StringBuilder returnVal = new StringBuilder();
+        for(int i=0; i<source.length(); i++){
+            if(i % 2 == 1){
+                returnVal.append(source.charAt(1));
+            }
+        }
+        return returnVal.toString();
+    }
+
+    public static String everySecondCharacter(Function<String,String> character, String s) {
+        return character.apply(s);
+    }
+}
