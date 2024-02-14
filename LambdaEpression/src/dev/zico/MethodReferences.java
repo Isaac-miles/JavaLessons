4f68cf6b856dc092359d5611c187756f1d31fc8d
isaac-miles <isaacnice247@gmail.com>
2024-02-14 13:09:21 +0100

    lambda expression

--- LambdaEpression/src/dev/zico/MethodReferences.java
+++ LambdaEpression/src/dev/zico/MethodReferences.java
@@ -1,7 +1,12 @@
 package dev.zico;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodReferences {
     public static void main(String[] args) {
-
+        List<String> list = new ArrayList<>(List.of(
+                "Ann","Bob","Dave"));
+        list.forEach(s-> System.out.println(s));
     }
 }
