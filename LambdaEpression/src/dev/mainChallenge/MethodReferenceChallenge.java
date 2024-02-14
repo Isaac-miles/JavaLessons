f4c5c693f17e621fe5d2ac4ae8e443698ffc7833
isaac-miles <isaacnice247@gmail.com>
2024-02-14 18:08:13 +0100

    lambda expression

--- LambdaEpression/src/dev/mainChallenge/MethodReferenceChallenge.java
+++ LambdaEpression/src/dev/mainChallenge/MethodReferenceChallenge.java
@@ -1,5 +1,6 @@
 package dev.mainChallenge;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -8,11 +9,32 @@ import java.util.function.UnaryOperator;
 public class MethodReferenceChallenge {
     private static Random random = new Random();
     public static void main(String[] args) {
+        String[] names = {"Anna","Bob","Cameron","Donald","Eva","Francis"};
+        List<UnaryOperator<String>> list = new ArrayList<>(List.of(
+                String::toUpperCase,
+                s->s+" "+ getRandomChar('A','Z')+"."
 
+
+        ));
+        applyChanges(names,list);
     }
 
     private static void applyChanges(String[] names, List<UnaryOperator<String>> stringFuntions){
         List<String> backedByArray = Arrays.asList(names);
+        for(var function:stringFuntions){
+            backedByArray.replaceAll((s)->s.transform(function));
+            System.out.println(Arrays.toString(names));
+        }
+    }
 
+    private static char getRandomChar(char startChar, char endChar){
+        return (char) random.nextInt(startChar,endChar+1);
+    }
+
+    private static String reverse(String s){
+        return reverse(s, 0, s.length());
+    }
+    private static String reverse(String s,int start,int end){
+        return new StringBuilder(s.substring(start,end)).reverse().toString();
     }
 }
