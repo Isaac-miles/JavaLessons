57c04a05a66f02ef7ef03840e64e15178f8bf53f
isaac-miles <isaacnice247@gmail.com>
2024-02-14 17:50:24 +0100

    lambda expression

--- LambdaEpression/src/dev/mainChallenge/MethodReferenceChallenge.java
+++ LambdaEpression/src/dev/mainChallenge/MethodReferenceChallenge.java
@@ -1,5 +1,18 @@
 package dev.mainChallenge;
 
-public class MethodReferenceChallenge {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.UnaryOperator;
 
+public class MethodReferenceChallenge {
+    private static Random random = new Random();
+    public static void main(String[] args) {
+
+    }
+
+    private static void applyChanges(String[] names, List<UnaryOperator<String>> stringFuntions){
+        List<String> backedByArray = Arrays.asList(names);
+
+    }
 }
