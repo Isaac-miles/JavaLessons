6b1111648f9e036634bbb38ecf5bed602a3461b8
isaac-miles <isaacnice247@gmail.com>
2024-02-14 18:23:44 +0100

    lambda expression

--- LambdaEpression/src/dev/mainChallenge/MethodReferenceChallenge.java
+++ LambdaEpression/src/dev/mainChallenge/MethodReferenceChallenge.java
@@ -7,34 +7,35 @@ import java.util.Random;
 import java.util.function.UnaryOperator;
 
 public class MethodReferenceChallenge {
-    private static Random random = new Random();
+    private static final Random random = new Random();
     public static void main(String[] args) {
         String[] names = {"Anna","Bob","Cameron","Donald","Eva","Francis"};
         List<UnaryOperator<String>> list = new ArrayList<>(List.of(
                 String::toUpperCase,
-                s->s+" "+ getRandomChar('A','Z')+"."
-
+                s-> s +" "+ getRandomChar()+".",
+                s-> s + (" " + reverse(s, s.indexOf(" "))),
+                MethodReferenceChallenge::reverse
 
         ));
         applyChanges(names,list);
     }
 
-    private static void applyChanges(String[] names, List<UnaryOperator<String>> stringFuntions){
+    private static void applyChanges(String[] names, List<UnaryOperator<String>> stringFunctions){
         List<String> backedByArray = Arrays.asList(names);
-        for(var function:stringFuntions){
+        for(var function:stringFunctions){
             backedByArray.replaceAll((s)->s.transform(function));
             System.out.println(Arrays.toString(names));
         }
     }
 
-    private static char getRandomChar(char startChar, char endChar){
-        return (char) random.nextInt(startChar,endChar+1);
+    private static char getRandomChar(){
+        return (char) random.nextInt('A', 'Z' +1);
     }
 
     private static String reverse(String s){
-        return reverse(s, 0, s.length());
+        return reverse(s, s.length());
     }
-    private static String reverse(String s,int start,int end){
-        return new StringBuilder(s.substring(start,end)).reverse().toString();
+    private static String reverse(String s, int end){
+        return new StringBuilder(s.substring(0,end)).reverse().toString();
     }
 }
