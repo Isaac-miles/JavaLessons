96ffdd0b6bcd10f20e35f6512c274aa6177776ca
isaac-miles <isaacnice247@gmail.com>
2024-02-14 13:18:51 +0100

    lambda expression

--- LambdaEpression/src/dev/zico/MethodReferences.java
+++ LambdaEpression/src/dev/zico/MethodReferences.java
@@ -2,11 +2,17 @@ package dev.zico;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class MethodReferences {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(List.of(
                 "Ann","Bob","Dave"));
         list.forEach(System.out::println);
+    }
+
+    private static <T> void calculator(T v1, T v2, BinaryOperator<T> function){
+        T result = function.apply(v1,v2);
+        System.out.println("Result of operation: "+result);
     }
 }
