2f1544b14642794a2e82a82b8e3aefc2682d9757
isaac-miles <isaacnice247@gmail.com>
2024-02-14 16:28:31 +0100

    lambda expression

--- LambdaEpression/src/dev/zico/MethodReferences.java
+++ LambdaEpression/src/dev/zico/MethodReferences.java
@@ -1,6 +1,7 @@
 package dev.zico;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.function.Supplier;
@@ -25,11 +26,21 @@ public class MethodReferences {
 
         Supplier<PlainOld> reference1 = PlainOld::new;
         reference1.get();
+        System.out.println("Getting Array");
+        PlainOld[] pojo1 = seedArray(PlainOld::new,5);
+
+        calculator("Hello ","world",(s1,s2)->s1.concat(s2));
+        calculator("Hello ","world", String::concat);
 
     }
 
     private static <T> void calculator(T v1, T v2, BinaryOperator<T> function){
         T result = function.apply(v1,v2);
         System.out.println("Result of operation: "+result);
+    }
+    private static PlainOld[] seedArray(Supplier<PlainOld> reference,int count){
+        PlainOld[] array = new PlainOld[count];
+        Arrays.setAll(array,i->reference.get());
+        return array;
     }
 }
