ad37803d244dcfdad183432226aa96cc0f1bacef
isaac-miles <isaacnice247@gmail.com>
2024-02-14 12:34:18 +0100

    lambda expression

--- LambdaEpression/src/dev/mainChallenge/LambdaExpressionChallenge.java
+++ LambdaEpression/src/dev/mainChallenge/LambdaExpressionChallenge.java
@@ -1,6 +1,8 @@
 package dev.mainChallenge;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class LambdaExpressionChallenge {
@@ -11,5 +13,24 @@ public class LambdaExpressionChallenge {
         Arrays.setAll(names, i->names[i].toUpperCase());
         System.out.println("---->Transformed to uppercase");
         System.out.println(Arrays.toString(names));
+
+        List<String> backedByArray = Arrays.asList(names);
+
+        backedByArray.replaceAll(s->s+" "+getRandomChar('A','B')+".");
+        System.out.println("---->Add random middle initial");
+        System.out.println(Arrays.toString(names));
+
+        backedByArray.replaceAll(s->s+" "+getReversedName(s.split(" ")[0]));
+        Arrays.asList(names).forEach(s-> System.out.println(s));
+
+        List<String> newList = new ArrayList<>(List.of(names));
+        newList.removeIf(s->s.substring(0,s.indexOf(" ")).equals(s.substring(s.lastIndexOf(" ")+1)));
+
+    }
+    public static char getRandomChar(char startChar, char endChar){
+        return (char) random.nextInt(startChar, endChar+1);
+    }
+    private static String getReversedName(String firstName){
+        return new StringBuilder(firstName).reverse().toString();
     }
 }
