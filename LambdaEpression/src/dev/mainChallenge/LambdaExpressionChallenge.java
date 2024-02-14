a0b425338448363ad1a93d3e0cd50fe7a909a3f9
isaac-miles <isaacnice247@gmail.com>
2024-02-14 11:28:40 +0100

    lambda expression

--- LambdaEpression/src/dev/mainChallenge/LambdaExpressionChallenge.java
+++ LambdaEpression/src/dev/mainChallenge/LambdaExpressionChallenge.java
@@ -1,5 +1,6 @@
 package dev.mainChallenge;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class LambdaExpressionChallenge {
@@ -7,6 +8,8 @@ public class LambdaExpressionChallenge {
 
     public static void main(String[] args) {
         String[] names = {"Anna","Bob","Carol","David","Ed","Fred","Gary"};
-        
+        Arrays.setAll(names, i->names[i].toUpperCase());
+        System.out.println("---->Transformed to uppercase");
+        System.out.println(Arrays.toString(names));
     }
 }
