68e7d0caae2fb7df94b397d051a4116fe4550a18
isaac-miles <isaacnice247@gmail.com>
2024-02-16 09:30:46 +0100

    Java collections

---
+++ CollectionOverview/src/dev/zico/Main.java
@@ -0,0 +1,14 @@
+package dev.zico;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        String[] names = {"Ann","Bob","Carol","David","Edna"};
+        list.addAll(Arrays.asList(names));
+        System.out.println(list);
+    }
+}
