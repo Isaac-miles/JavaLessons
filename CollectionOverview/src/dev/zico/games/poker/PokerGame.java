77c85eba111fa62d0d171cad84838382223653af
isaac-miles <isaacnice247@gmail.com>
2024-02-17 00:31:28 +0100

    Java collections

--- CollectionOverview/src/dev/zico/games/poker/PokerGame.java
+++ CollectionOverview/src/dev/zico/games/poker/PokerGame.java
@@ -1,4 +1,12 @@
 package dev.zico.games.poker;
 
+import dev.collectionMethods.Card;
+
+import java.util.List;
+
 public class PokerGame {
+
+    private final List<Card> deck = Card.getStandardDeck();
+    private int playerCount;
+    private int cardsInHand;
 }
