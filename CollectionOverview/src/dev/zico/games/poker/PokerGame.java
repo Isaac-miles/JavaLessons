2e757f4818cccf21e1e9484ca4c90934bb635641
isaac-miles <isaacnice247@gmail.com>
2024-02-17 00:50:01 +0100

    Java collections

--- CollectionOverview/src/dev/zico/games/poker/PokerGame.java
+++ CollectionOverview/src/dev/zico/games/poker/PokerGame.java
@@ -2,10 +2,7 @@ package dev.zico.games.poker;
 
 import dev.collectionMethods.Card;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class PokerGame {
 
@@ -27,5 +24,18 @@ public class PokerGame {
         int randomMiddle = new Random().nextInt(15,35);
         Collections.rotate(deck,randomMiddle);
         Card.printDeck(deck);
+    }
+
+    private void deal(){
+        Card[][] hands = new Card[playerCount][cardsInHand];
+        for(int deckIndex=0, i= 0; i<cardsInHand;i++){
+            for(int j=0; j<playerCount;j++){
+                hands[j][i] =deck.get(deckIndex++);
+            }
+        }
+        int playerNo = 1;
+        for(Card[] hand: hands){
+            pokerHands.add(new PokerHand(playerNo++, Arrays.asList(hand)));
+        }
     }
 }
