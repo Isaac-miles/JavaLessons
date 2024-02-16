19513afb9e8e64842b81bdf322bd184c7eb7255d
isaac-miles <isaacnice247@gmail.com>
2024-02-16 09:35:34 +0100

    Java collections

--- CollectionOverview/src/dev/zico/Main.java
+++ CollectionOverview/src/dev/zico/Main.java
@@ -2,17 +2,21 @@ package dev.zico;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
         String[] names = {"Ann","Bob","Carol","David","Edna"};
-        List<String> list = new ArrayList<>(Arrays.asList(names));
+        Collection<String> list = new ArrayList<>(Arrays.asList(names));
         System.out.println(list);
 
         list.add("Fred");
         list.addAll(Arrays.asList("Goerge","Gary","Grace"));
+        System.out.println(list);
         System.out.println(list.contains("miles"));
         list.removeIf(s->s.endsWith("ce"));
+        System.out.println("_".repeat(25));
+        System.out.println(list);
+
     }
 }
