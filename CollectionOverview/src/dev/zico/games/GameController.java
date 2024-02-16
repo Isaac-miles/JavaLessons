729944634502ed61d99938aa85c7416ccbd654ee
isaac-miles <isaacnice247@gmail.com>
2024-02-17 00:39:20 +0100

    Java collections

--- CollectionOverview/src/dev/zico/games/GameController.java
+++ CollectionOverview/src/dev/zico/games/GameController.java
@@ -5,6 +5,6 @@ import dev.zico.games.poker.PokerGame;
 public class GameController {
     public static void main(String[] args) {
         PokerGame fiveCardDraw = new PokerGame(4,5);
-
+        fiveCardDraw.startPlay();
     }
 }
