0abd8df53c1afd6cc9441c137ab7a4322a77587c
isaac-miles <isaacnice247@gmail.com>
2024-02-16 13:43:47 +0100

    Java collections

--- CollectionOverview/src/dev/zico/Main.java
+++ CollectionOverview/src/dev/zico/Main.java
@@ -2,12 +2,12 @@ package dev.zico;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.TreeSet;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
         String[] names = {"Ann","Bob","Carol","David","Edna"};
-        Collection<String> list = new TreeSet<>(Arrays.asList(names));
+        Collection<String> list = new HashSet<>(Arrays.asList(names));
         System.out.println(list);
 
         list.add("Fred");
@@ -17,6 +17,8 @@ public class Main {
         list.removeIf(s->s.endsWith("ce"));
         System.out.println("_".repeat(25));
         System.out.println(list);
+//        list.sort() doesn't compile because sort doesn't exist on collections
+
 
     }
 }
