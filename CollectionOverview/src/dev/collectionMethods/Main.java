0eaaee86e90a32b16c17f7163ac7db01d918db58
isaac-miles <isaacnice247@gmail.com>
2024-02-16 20:16:38 +0100

    Java collections

--- CollectionOverview/src/dev/collectionMethods/Main.java
+++ CollectionOverview/src/dev/collectionMethods/Main.java
@@ -1,10 +1,19 @@
 package dev.collectionMethods;
 
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+        Card[] cardArray = new Card[13];
+        Card aceOfhearts = Card.getFaceCard(Card.Suit.HEART,'A');
+        Arrays.fill(cardArray,aceOfhearts);
+        Card.printDeck(Arrays.asList(cardArray),"Ace of Hearts",1);
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards,aceOfhearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
     }
 }
