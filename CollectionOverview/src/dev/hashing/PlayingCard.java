61510c49418eb83b92edb3b343678dc10e903008
isaac-miles <isaacnice247@gmail.com>
2024-02-18 22:52:57 +0100

    Java collections (Hashing)

--- CollectionOverview/src/dev/hashing/PlayingCard.java
+++ CollectionOverview/src/dev/hashing/PlayingCard.java
@@ -16,4 +16,21 @@ public class PlayingCard {
         return face + " of " + suit;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlayingCard that = (PlayingCard) o;
+
+        if (!suit.equals(that.suit)) return false;
+        return face.equals(that.face);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + face.hashCode();
+        return result;
+    }
 }
