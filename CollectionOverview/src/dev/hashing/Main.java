fc777874c3320297fbae8b203d1a62c5d1275c61
isaac-miles <isaacnice247@gmail.com>
2024-02-18 22:17:07 +0100

    Java collections (Hashing)

--- CollectionOverview/src/dev/hashing/Main.java
+++ CollectionOverview/src/dev/hashing/Main.java
@@ -1,5 +1,8 @@
 package dev.hashing;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         String aText = "Hello";
@@ -7,5 +10,8 @@ public class Main {
         String ctest = String.join("l", "He","lo");
         String dTesxt = "He".concat("llo");
         String eText = "hello";
+
+        List<String> hellos = Arrays.asList(aText,bTest,ctest,dTesxt,eText);
+        hellos.forEach(s-> System.out.println(s+": "+ s.hashCode()));
     }
 }
