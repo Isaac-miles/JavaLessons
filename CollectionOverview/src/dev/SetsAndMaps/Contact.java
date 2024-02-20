5cebaa0b68223acaeacb5ad645cb957a8e7083e3
isaac-miles <isaacnice247@gmail.com>
2024-02-20 17:38:13 +0100

    Java collections (HashingSets)

--- CollectionOverview/src/dev/SetsAndMaps/Contact.java
+++ CollectionOverview/src/dev/SetsAndMaps/Contact.java
@@ -1,5 +1,9 @@
 package dev.SetsAndMaps;
 
-public class Contact {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Contact {
+    private Set<String> emails = new HashSet<>();
+    private Set<String> phones = new HashSet<>();
 }
