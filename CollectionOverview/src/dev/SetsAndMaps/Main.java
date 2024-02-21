1c738d458254882b33e106e4e2c9bda0de19db67
isaac-miles <isaacnice247@gmail.com>
2024-02-21 12:41:00 +0100

    Java collections (HashingSets)

--- CollectionOverview/src/dev/SetsAndMaps/Main.java
+++ CollectionOverview/src/dev/SetsAndMaps/Main.java
@@ -1,7 +1,15 @@
 package dev.SetsAndMaps;
 
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+    public static void printData(String header, Collection<Contact> contacts){
+        System.out.println("-".repeat(30));
+        System.out.println(header);
+        System.out.println("-".repeat(30));
+        contacts.forEach(System.out::println);
     }
 }
