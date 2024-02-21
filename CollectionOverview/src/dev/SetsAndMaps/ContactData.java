9338ab5ddfb8b4f4866226eacb45c83bfa78a879
isaac-miles <isaacnice247@gmail.com>
2024-02-21 12:11:23 +0100

    Java collections (HashingSets)

--- CollectionOverview/src/dev/SetsAndMaps/ContactData.java
+++ CollectionOverview/src/dev/SetsAndMaps/ContactData.java
@@ -1,4 +1,16 @@
 package dev.SetsAndMaps;
 
 public class ContactData {
+    private static final String phoneData = """
+            Charles Okocha, 08023448294
+            Imade David, 02093448723
+            Imole Brown, 09428337382
+            Faith Yakubu, 02092883847
+            Isaac All, 07030992937
+            Holding Clerk, 09022394876
+            Robbin Hood, 01211339487
+            Lucky Dube, 07020205510
+            Prince Dim, 05020993847
+            Johnson Drew, 09055903610
+            """;
 }
