f12f18b18cbe99b7970f360a61ac45c56a50da30
isaac-miles <isaacnice247@gmail.com>
2024-02-21 12:51:33 +0100

    Java collections (HashingSets)

--- CollectionOverview/src/dev/SetsAndMaps/Main.java
+++ CollectionOverview/src/dev/SetsAndMaps/Main.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        List<Contact> emails =  ContactData.getData("email");
+        List<Contact> emails =  ContactData.getData("email");
         List<Contact> phones =  ContactData.getData("phone");
-//        printData("Phone List", phones);
-//        printData("Email list", emails);
+        printData("Phone List", phones);
+        printData("Email list", emails);
     }
     public static void printData(String header, Collection<Contact> contacts){
-        System.out.println("-".repeat(30));
+        System.out.println("-".repeat(35));
         System.out.println(header);
-        System.out.println("-".repeat(30));
+        System.out.println("-".repeat(35));
         contacts.forEach(System.out::println);
     }
 }
