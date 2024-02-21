decf85080cbe6f20651378c5216e5d9c867dc69a
isaac-miles <isaacnice247@gmail.com>
2024-02-21 12:44:27 +0100

    Java collections (HashingSets)

--- CollectionOverview/src/dev/SetsAndMaps/ContactData.java
+++ CollectionOverview/src/dev/SetsAndMaps/ContactData.java
@@ -9,7 +9,7 @@ public class ContactData {
     private static final String phoneData = """
             Charles Okocha, 08023448294
             Imade David, 02093448723
-            Imole Brown, 09428337382
+            Imade David, 09428337382
             Faith Yakubu, 02092883847
             Isaac All, 07030992937
             Holding Clerk, 09022394876
