25e8398938655169f8fadec3d253afdf119cd5d8
isaac-miles <isaacnice247@gmail.com>
2024-02-22 23:21:22 +0100

    Java collections (HashingSets)

---
+++ CollectionOverview/src/dev/challenge/TaskData.java
@@ -0,0 +1,39 @@
+package dev.challenge;
+
+public class TaskData {
+    private static String tasks = """
+            Infrastructure; Logging, High 
+            Infrastructure, DB Access, Medium
+            Infrastructure, Security, High 
+            Infrastructure, Password Policy, Medium
+            Data Design, Task Table, Medium 
+            Data Design, Employee Table, Medium
+            Data Design, Cross Reference Tables, High 
+            Data Design, Encryption Policy, High
+            Data Access, Write Views, Low 
+            Data Access, Set Up Users, Low
+             Data Access, Set Up Access Policy, Low
+            """;
+    private static String annsTask = """
+           Infrastructure, Security, High, In Progress
+           Infrastructure, Password Policy, Medium, In Progress
+           Research, Cloud solutions, Medium, In Progress
+           Data Design, Encryption Policy, High
+           Data Design, Project Table, Medium
+           Data Access, Write Views, Low, In Progress
+            """;
+    private static String bobsTask = """
+            Infrastructure, Security, High, In Progress 
+            Infrastructure, Password Policy, Medium
+            Data Design, Encryption Policy, High
+            Data Access, Write Views, Low, In Progress
+            """;
+    private static String carolsTask = """
+            Infrastructure, Logging, High, In Progress 
+            Infrastructure, DB Access, Medium 
+            Infrastructure, Password Policy, Medium
+            Data Design, Task Table, High
+            Data Access, Write Views, Low
+            """;
+
+}
