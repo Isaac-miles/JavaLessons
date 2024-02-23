dfc1bd6082354b3e39d92a95606db6070b29e269
isaac-miles <isaacnice247@gmail.com>
2024-02-23 01:40:01 +0100

    Java collections (HashingSets)

--- CollectionOverview/src/dev/challenge/Main.java
+++ CollectionOverview/src/dev/challenge/Main.java
@@ -1,13 +1,11 @@
 package dev.challenge;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        Set<Task> tasks = TaskData.gettasks("all");
+        sortAndPrint("All Tasks", tasks);
     }
 
     private static void sortAndPrint(String header, Collection<Task> collection){
