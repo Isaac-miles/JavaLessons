b21973e55cd2ebc8fefa422941188b76cb5bdbab
isaac-miles <isaacnice247@gmail.com>
2024-02-23 20:03:29 +0100

    Java collections (TreeSet)

--- CollectionOverview/src/dev/treeSetChallenge/BookingAgent.java
+++ CollectionOverview/src/dev/treeSetChallenge/BookingAgent.java
@@ -2,6 +2,9 @@ package dev.treeSetChallenge;
 
 public class BookingAgent {
     public static void main(String[] args) {
-
+        int rows = 10;
+        int totalSeats = 100;
+        Theatre lagosQuilox = new Theatre("Lagos Quilox",rows,totalSeats);
+        lagosQuilox.printSeatMap();
     }
 }
