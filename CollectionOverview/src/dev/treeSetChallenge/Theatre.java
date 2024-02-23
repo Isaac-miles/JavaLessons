92c1f7c3eced2962884630a1007f3d2249ad1984
isaac-miles <isaacnice247@gmail.com>
2024-02-23 19:47:40 +0100

    Java collections (TreeSet)

--- CollectionOverview/src/dev/treeSetChallenge/Theatre.java
+++ CollectionOverview/src/dev/treeSetChallenge/Theatre.java
@@ -1,12 +1,28 @@
 package dev.treeSetChallenge;
 
+import java.util.NavigableSet;
+
 public class Theatre {
-    class Seat{
+    class Seat implements Comparable<Seat>{
         private String seatNum;
         private boolean reserved;
 
         public Seat(char rowChar,int seatNo) {
             this.seatNum = "%C%03d".formatted(rowChar,seatNo);
         }
+
+        @Override
+        public String toString() {
+            return seatNum;
+        }
+
+        @Override
+        public int compareTo(Seat o) {
+           return seatNum.compareTo(o.seatNum);
+        }
     }
+    private String theatreName;
+    private int seatsPerRow;
+    private NavigableSet<Seat> seats;
+
 }
