2aa6e51677946e6f7e6a2f3a31e72d333e5f432b
isaac-miles <isaacnice247@gmail.com>
2024-02-24 22:17:00 +0100

    Java collections (TreeSet)

--- CollectionOverview/src/dev/treeSetChallenge/BookingAgent.java
+++ CollectionOverview/src/dev/treeSetChallenge/BookingAgent.java
@@ -15,6 +15,14 @@ public class BookingAgent {
         bookSeat(lagosQuilox,'M',3);
 
         bookSeats(lagosQuilox,4,'B',3,10);
+        bookSeats(lagosQuilox,6,'B','C',3,10);
+        bookSeats(lagosQuilox,4,'B',1,10);
+        bookSeats(lagosQuilox,4,'B','C',1,10);
+        bookSeats(lagosQuilox,1,'B','C',1,1);
+        bookSeats(lagosQuilox,4,'M','Z',1,10);
+        bookSeats(lagosQuilox,10,'A','E',1,10);
+
+
     }
 
     private static void bookSeat(Theatre theatre, char row, int seatNo){
