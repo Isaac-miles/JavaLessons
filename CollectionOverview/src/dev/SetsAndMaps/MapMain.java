b467df22ca1250d4fb7595336c6add73a6773a5e
isaac-miles <isaacnice247@gmail.com>
2024-02-25 23:09:07 +0100

    Java collections (Maps)

--- CollectionOverview/src/dev/SetsAndMaps/MapMain.java
+++ CollectionOverview/src/dev/SetsAndMaps/MapMain.java
@@ -1,12 +1,25 @@
 package dev.SetsAndMaps;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapMain {
     public static void main(String[] args) {
         List<Contact> emails =  ContactData.getData("email");
         List<Contact> phones =  ContactData.getData("phone");
 
+        List<Contact> fullList = new ArrayList<>(phones);
+        fullList.addAll(emails);
+        fullList.forEach(System.out::println);
+        System.out.println("--------------------------------------");
 
+        Map<String, Contact> contacts = new HashMap<>();
+
+        for(Contact contact:fullList){
+            contacts.put(contact.getName(),contact);
+        }
+        contacts.forEach((k,v)-> System.out.println("key= "+k+", value= "+v));
     }
 }
