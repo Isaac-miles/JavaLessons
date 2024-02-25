18896483e3b9e7f5f02ddbfff92952b75ce9c667
isaac-miles <isaacnice247@gmail.com>
2024-02-25 23:04:52 +0100

    Java collections (Maps)

--- CollectionOverview/src/dev/SetsAndMaps/MapMain.java
+++ CollectionOverview/src/dev/SetsAndMaps/MapMain.java
@@ -1,7 +1,12 @@
 package dev.SetsAndMaps;
 
+import java.util.List;
+
 public class MapMain {
     public static void main(String[] args) {
+        List<Contact> emails =  ContactData.getData("email");
+        List<Contact> phones =  ContactData.getData("phone");
+
 
     }
 }
