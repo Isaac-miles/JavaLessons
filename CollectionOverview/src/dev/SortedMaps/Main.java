51f696cec2704249ad909192b003739bf378a4e1
isaac-miles <isaacnice247@gmail.com>
2024-02-26 22:47:46 +0100

    Java collections (sorted maps)

--- CollectionOverview/src/dev/SortedMaps/Main.java
+++ CollectionOverview/src/dev/SortedMaps/Main.java
@@ -1,5 +1,6 @@
 package dev.SortedMaps;
 
+import java.time.LocalDate;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.NavigableMap;
@@ -9,6 +10,8 @@ public class Main {
     private static Map<String,Purchase> purchases = new LinkedHashMap<>();
     private static NavigableMap<String, Student> students = new TreeMap<>();
     public static void main(String[] args) {
+        Course jbc = new Course("jbc101","Java Bi Functions","Java");
+        Course python = new Course("pyt102","Python Master Class","Python");
 
     }
     private static void addPurchase(String name, Course course,double price){
@@ -19,5 +22,11 @@ public class Main {
         }else {
             existingStudent.addCourse(course);
         }
+
+        int day = purchases.size()+1;
+        String key = course.courseId()+"_"+existingStudent.getId();
+        int year = LocalDate.now().getYear();
+        Purchase purchase = new Purchase(course.courseId(),existingStudent.getId(),price,year,day);
+        purchases.put(key,purchase);
     }
 }
