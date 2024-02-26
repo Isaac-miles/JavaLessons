d729ceaf7a35afc0b59d562f444fe690eab12bf5
isaac-miles <isaacnice247@gmail.com>
2024-02-26 22:41:27 +0100

    Java collections (sorted maps)

--- CollectionOverview/src/dev/SortedMaps/Main.java
+++ CollectionOverview/src/dev/SortedMaps/Main.java
@@ -1,7 +1,23 @@
 package dev.SortedMaps;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
 public class Main {
+    private static Map<String,Purchase> purchases = new LinkedHashMap<>();
+    private static NavigableMap<String, Student> students = new TreeMap<>();
     public static void main(String[] args) {
 
+    }
+    private static void addPurchase(String name, Course course,double price){
+        Student existingStudent = students.get(name);
+        if(existingStudent == null){
+            existingStudent = new Student(name,course);
+            students.put(name,existingStudent);
+        }else {
+            existingStudent.addCourse(course);
+        }
     }
 }
