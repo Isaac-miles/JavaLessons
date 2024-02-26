ed75e61ba82f5a8a1a81066c65a076d8ca92a306
isaac-miles <isaacnice247@gmail.com>
2024-02-26 20:24:18 +0100

    Java collections (Map View)

---
+++ CollectionOverview/src/dev/adventureMapChallenge/AdventureGame.java
@@ -0,0 +1,25 @@
+package dev.adventureMapChallenge;
+
+import java.util.Map;
+
+public class AdventureGame {
+    private static final String GAME_LOCATIONS = """
+            road, at the end of the road, w: hill, E:well house, S:valley,N: forest
+            hill, on top of hill with a view in all directions,N: forest, E: road
+            well house, inside a well house for a small spring,W: road, N: lake, S: stream
+            valley, in a forest valley beside a tumbling stream,N: road, W:hill,E:strea
+            forest, at the edge of a thick dark forest,S: road,E: lake
+            lake, by an alpine lake surrounded by wildflowers, W: forest, S:well house
+            stream, near a stream with a rocky bed,W:valley, N:well house
+            """;
+    private enum Compass{
+        E,N,S,W;
+        private static final String[] directions = {"East","North","South","West"};
+        public String getString(){
+            return directions[this.ordinal()];
+        }
+    }
+    private record Location(String description, Map<Compass, String> nextPlaces){
+
+    }
+}
