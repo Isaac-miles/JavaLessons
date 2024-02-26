5ba65c868376a60a56072f1a839f36c43218b36e
isaac-miles <isaacnice247@gmail.com>
2024-02-26 21:53:36 +0100

    Java collections (AdventureGame challenge)

--- CollectionOverview/src/dev/adventureMapChallenge/Main.java
+++ CollectionOverview/src/dev/adventureMapChallenge/Main.java
@@ -1,8 +1,14 @@
 package dev.adventureMapChallenge;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         AdventureGame game = new AdventureGame();
         game.play("lake");
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String direction = scanner.nextLine().trim().toUpperCase().substring(0,1);
+        }
     }
 }
