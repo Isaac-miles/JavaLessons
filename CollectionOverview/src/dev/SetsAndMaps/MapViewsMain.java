fea1220f347b6f91c6583d8669182c3b1e360dca
isaac-miles <isaacnice247@gmail.com>
2024-02-26 16:24:21 +0100

    Java collections (Map View)

--- CollectionOverview/src/dev/SetsAndMaps/MapViewsMain.java
+++ CollectionOverview/src/dev/SetsAndMaps/MapViewsMain.java
@@ -1,7 +1,11 @@
 package dev.SetsAndMaps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapViewsMain {
     public static void main(String[] args) {
+        Map<String, Contact> contacts = new HashMap<>();
 
     }
 }
