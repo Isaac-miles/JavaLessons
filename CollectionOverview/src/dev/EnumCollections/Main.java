33efe88a796a5927c93448eb74160fd81e4957e1
isaac-miles <isaacnice247@gmail.com>
2024-02-27 13:25:04 +0100

    Java collections (sorted maps)

--- CollectionOverview/src/dev/EnumCollections/Main.java
+++ CollectionOverview/src/dev/EnumCollections/Main.java
@@ -3,15 +3,30 @@ package dev.EnumCollections;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
-    enum WeekDay{MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY}
+    enum WeekDay{SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY}
+
     public static void main(String[] args) {
         List<WeekDay> annsWorkDays = new ArrayList<>(List.of(
                 WeekDay.MONDAY,WeekDay.TUESDAY,WeekDay.WEDNESDAY,WeekDay.THURSDAY,WeekDay.FRIDAY));
 
         var annsDaysSet = EnumSet.copyOf(annsWorkDays);
         System.out.println(annsDaysSet.getClass().getSimpleName());
+        annsDaysSet.forEach(System.out::println);
 
+        var allDaysSet = EnumSet.allOf(WeekDay.class);
+        System.out.println("----------------------------------------");
+        allDaysSet.forEach(System.out::println);
+
+        Set<WeekDay> newPersonDays = EnumSet.complementOf(annsDaysSet);
+        System.out.println("----------------------------------------");
+        newPersonDays.forEach(System.out::println);
+
+        Set<WeekDay> anotherWay = EnumSet.copyOf(allDaysSet);
+        anotherWay.removeAll(annsDaysSet);
+        System.out.println("----------------------------------------");
+        anotherWay.forEach(System.out::println);
     }
 }
