b1cc51d82e9003670bfa511bd02c741ddad334c1
isaac-miles <isaacnice247@gmail.com>
2024-02-27 14:17:46 +0100

    Java collections (StoreIventoryChallenge)

--- CollectionOverview/src/dev/StoreInventoryChalleng/InventoryItem.java
+++ CollectionOverview/src/dev/StoreInventoryChalleng/InventoryItem.java
@@ -15,4 +15,41 @@ public class InventoryItem {
         this.qtyReorder = qtyTotal;
         this.qtyLow = qtyLow;
     }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public boolean reservedItem(int qty){
+        if((qtyTotal - qtyReserve) >= qty){
+            qtyReserve +=qty;
+            return true;
+        }
+        return false;
+    }
+    public void releaseItem(int qty){
+        qtyReserve -=qty;
+    }
+    public boolean sellItem(int qty){
+        if(qtyTotal >=qty){
+            qtyTotal -=qty;
+            qtyReserve -=qty;
+            if(qtyTotal <= qtyLow){
+                placeInventoryOrder();
+            }
+            return true;
+        }
+        return false;
+    }
+    private void placeInventoryOrder(){
+        System.out.printf("Ordering qty %d : %s%n", qtyReorder,product);
+    }
+
+    @Override
+    public String toString() {
+        return "%s, $%.2f : [%04d,%2d]".formatted(product,price,qtyTotal,qtyReserve);
+    }
 }
