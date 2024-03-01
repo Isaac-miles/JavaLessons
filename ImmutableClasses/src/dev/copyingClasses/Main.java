9790468f1396307a353e4e58d5047eba17eee563
isaac-miles <isaacnice247@gmail.com>
2024-03-01 21:26:08 +0100

    Immutable classes

--- ImmutableClasses/src/dev/copyingClasses/Main.java
+++ ImmutableClasses/src/dev/copyingClasses/Main.java
@@ -2,15 +2,23 @@ package dev.copyingClasses;
 
 import java.util.Arrays;
 
-record Person(String name, String dob){}
+record Person(String name, String dob,Person[] kids){
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", kids=" + Arrays.toString(kids) +
+                '}';
+    }
+}
 public class Main {
     public static void main(String[] args) {
 
-        Person joe = new Person("Joe","01/01/1961");
-        Person jim = new Person("Jim","01/02/1962");
-        Person jack = new Person("Jack","01/03/1963");
-        Person jane = new Person("Jane","01/04/1964");
-        Person jill = new Person("Jill","01/05/1965");
+        Person joe = new Person("Joe","01/01/1961",null);
+        Person jim = new Person("Jim","01/02/1962",null);
+        Person jack = new Person("Jack","01/03/1963",new Person[]{joe,jim});
+        Person jane = new Person("Jane","01/04/1964",null);
+        Person jill = new Person("Jill","01/05/1965",new Person[]{joe,jim});
 
         Person[] persons = {jane,jack,joe,jill,jim};
         Person[] personCopy = Arrays.copyOf(persons,persons.length); //shallow copy
