5d5fafb7eb87e9444d3056e524d66d448d1f347d
isaac-miles <isaacnice247@gmail.com>
2024-03-01 21:01:26 +0100

    Immutable classes

---
+++ ImmutableClasses/src/dev/bank/BankCustomer.java
@@ -0,0 +1,19 @@
+package dev.bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankCustomer {
+
+    private static int lastCustomerId = 10_000_000;
+    private final String name;
+    private final int customerId;
+    private final List<BankAccount> accounts = new ArrayList<>();
+
+    public BankCustomer(String name,double checkingAmount, double savingsAmount) {
+        this.name = name;
+        this.customerId = lastCustomerId++;
+        accounts.add(new BankAccount(BankAccount.AccountType.CHECKING,checkingAmount));
+        accounts.add(new BankAccount(BankAccount.AccountType.SAVING,savingsAmount));
+    }
+}
