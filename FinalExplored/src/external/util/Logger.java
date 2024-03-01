08950e983a52a5707c8433c6e4881950c1e9d35c
isaac-miles <isaacnice247@gmail.com>
2024-03-01 15:48:42 +0100

    Immutable classes(Final explored)

--- FinalExplored/src/external/util/Logger.java
+++ FinalExplored/src/external/util/Logger.java
@@ -7,5 +7,6 @@ public class Logger {
     public static void logToConsole(CharSequence message){
 
         LocalDateTime dt = LocalDateTime.now();
+        System.out.printf("%1$tD %1$tT : %2$s%n",dt,message);
     }
 }
