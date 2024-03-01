4756bed3bbcde17ac2a9344886b5515cf836c038
isaac-miles <isaacnice247@gmail.com>
2024-03-01 15:44:27 +0100

    Immutable classes(Final explored)

---
+++ FinalExplored/src/external/util/Logger.java
@@ -0,0 +1,11 @@
+package external.util;
+
+import java.time.LocalDateTime;
+
+public class Logger {
+
+    public static void logToConsole(CharSequence message){
+
+        LocalDateTime dt = LocalDateTime.now();
+    }
+}
