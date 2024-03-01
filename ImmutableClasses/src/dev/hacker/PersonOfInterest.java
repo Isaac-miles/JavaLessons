d9c996d2a1e8bcfd55b094682909aa501f87b62b
isaac-miles <isaacnice247@gmail.com>
2024-03-01 20:48:40 +0100

    Immutable classes

---
+++ ImmutableClasses/src/dev/hacker/PersonOfInterest.java
@@ -0,0 +1,10 @@
+package dev.hacker;
+
+import dev.zico.PersonImmutable;
+
+public class PersonOfInterest extends PersonImmutable {
+    public PersonOfInterest(PersonImmutable person) {
+        super(person);
+    }
+
+}
