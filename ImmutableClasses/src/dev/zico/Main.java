9da36466ed203263feb0415473e1114211ce6b39
isaac-miles <isaacnice247@gmail.com>
2024-03-01 17:56:36 +0100

    Immutable classes

--- ImmutableClasses/src/dev/zico/Main.java
+++ ImmutableClasses/src/dev/zico/Main.java
@@ -2,4 +2,8 @@ package dev.zico;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Person jane = new Person();
+        jane.setName("Jane");
+    }
 }
--- ImmutableClasses/src/dev/zico/Person.java
+++ ImmutableClasses/src/dev/zico/Person.java
@@ -38,7 +38,7 @@ public class Person {
         if(kids !=null){
             String[] names = new String[kids.length];
             Arrays.setAll(names,i->names[i]=kids[i]==null? "":kids[i].name);
-            kidstring = String.join(", "+name);
+            kidstring = String.join(", "+ Arrays.toString(names));
         }
         return name+", dob =  "+ dob+ ", kids = "+kidstring;
     }
