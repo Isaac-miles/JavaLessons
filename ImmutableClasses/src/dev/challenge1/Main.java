0ef21f3084e5adb7d2351d1e3a0274d8a2631af8
isaac-miles <isaacnice247@gmail.com>
2024-03-01 21:06:25 +0100

    Immutable classes

--- ImmutableClasses/src/dev/challenge1/Main.java
+++ ImmutableClasses/src/dev/challenge1/Main.java
@@ -1,11 +1,12 @@
 package dev.challenge1;
 
-import dev.bank.BankAccount;
+import dev.bank.BankCustomer;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING,500);
-        System.out.println(account);
-
+//        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING,500);
+//        System.out.println(account);
+        BankCustomer joe = new BankCustomer("Joe",500.0,1000.0);
+        System.out.println(joe);
     }
 }
