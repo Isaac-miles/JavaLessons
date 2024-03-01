964985bb336ee2d167f465d6e93b6d180c7cac59
isaac-miles <isaacnice247@gmail.com>
2024-03-01 17:46:47 +0100

    Immutable classes(Final explored)

---
+++ ImmutableClasses/src/dev/zico/Person.java
@@ -0,0 +1,32 @@
+package dev.zico;
+
+public class Person {
+
+    private String name;
+    private String dob;
+    private Person[] kids;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public Person[] getKids() {
+        return kids;
+    }
+
+    public void setKids(Person[] kids) {
+        this.kids = kids;
+    }
+}
