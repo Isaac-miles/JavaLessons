7486c8eb05f7b6d442610f9f209753bb846428fc
isaac-miles <isaacnice247@gmail.com>
2024-03-02 22:45:26 +0100

    Immutable classes

--- ImmutableClasses/src/dev/challenge1/Main.java
+++ ImmutableClasses/src/dev/challenge1/Main.java
@@ -9,9 +9,10 @@ public class Main {
     public static void main(String[] args) {
 //        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING,500);
 //        System.out.println(account);
-        BankCustomer joe = new BankCustomer("Joe",500.0,1000.0);
-        System.out.println(joe);
+//        BankCustomer joe = new BankCustomer("Joe",500.0,1000.0);
+//        System.out.println(joe);
 
+         BankCustomer joe = null;
         List<BankAccount> accounts = joe.getAccounts();
         accounts.clear();
         System.out.println(joe);
