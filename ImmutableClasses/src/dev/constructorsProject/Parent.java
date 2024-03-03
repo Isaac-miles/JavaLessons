b4d792a87a6a87a80a10024eb702c6e66fc357e8
isaac-miles <isaacnice247@gmail.com>
2024-03-03 20:07:02 +0100

    Constructors advance

--- ImmutableClasses/src/dev/constructorsProject/Parent.java
+++ ImmutableClasses/src/dev/constructorsProject/Parent.java
@@ -4,6 +4,7 @@ public class Parent {
     private final String name;
     private final String dob;
 
+    //this is an instance initializer
     {
         name = "John Doe";
         dob = "01/01/1900";
