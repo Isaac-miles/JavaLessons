8ee7e0f2b64a64fa8d42983d2671378e88a50afd
isaac-miles <isaacnice247@gmail.com>
2024-03-03 19:58:01 +0100

    Constructors advance

--- ImmutableClasses/src/dev/constructorsProject/Parent.java
+++ ImmutableClasses/src/dev/constructorsProject/Parent.java
@@ -1,5 +1,22 @@
 package dev.constructorsProject;
 
 public class Parent {
+    private String name;
+    private String dob;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
 }
