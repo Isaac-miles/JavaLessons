00fee755792ebdb8330edca7475b27975ea59b8b
isaac-miles <isaacnice247@gmail.com>
2024-03-03 20:02:21 +0100

    Constructors advance

--- ImmutableClasses/src/dev/constructorsProject/Main.java
+++ ImmutableClasses/src/dev/constructorsProject/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Parent parent = new Parent();
         Child child = new Child();
+        System.out.println(parent);
+        System.out.println(child);
 
     }
 }
