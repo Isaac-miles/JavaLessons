fafe54d5fc3f956ee0428902fd65043f11479c56
isaac-miles <isaacnice247@gmail.com>
2024-03-04 17:26:26 +0100

    Constructors advance

--- ImmutableClasses/src/dev/constructorsProject/Person.java
+++ ImmutableClasses/src/dev/constructorsProject/Person.java
@@ -1,5 +1,8 @@
 package dev.constructorsProject;
 
 public record Person(String name, String dob) {
-
+    public Person(String name, String dob) {
+        this.name = name;
+        this.dob = dob.replace("-","/");
+    }
 }
