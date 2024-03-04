edaaa1641b47f81d9d3cda26ca8b2af933ed835b
isaac-miles <isaacnice247@gmail.com>
2024-03-04 21:10:01 +0100

    Constructors advance

--- ImmutableClasses/src/dev/constructorsProject/Generation.java
+++ ImmutableClasses/src/dev/constructorsProject/Generation.java
@@ -1,14 +1,24 @@
 package dev.constructorsProject;
 
 public enum Generation {
-    GEN_Z,
-    MILLENNIAL,
-    GEN_X,
-    BABY_BOOMER,
-    SILENT_GENERATION,
-    GREATEST_GENERATION;
+    GEN_Z(2001,2025),
+    MILLENNIAL(1981,200),
+    GEN_X(1965,1980),
+    BABY_BOOMER(1946,1964),
+    SILENT_GENERATION(1927,1945),
+    GREATEST_GENERATION(1901,1926);
 
-    Generation() {
+    private final int startYear;
+    private final  int endYear;
+
+    Generation(int startYear, int endYear) {
+        this.startYear = startYear;
+        this.endYear = endYear;
         System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return this.name()+" "+startYear+" - "+endYear;
     }
 }
