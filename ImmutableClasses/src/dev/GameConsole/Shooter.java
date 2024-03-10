af921df7d55e89a065f06448a5ddf77498e0f5f9
isaac-miles <isaacnice247@gmail.com>
2024-03-10 19:30:06 +0100

    Constructors advance(Game Console)

--- ImmutableClasses/src/dev/GameConsole/Shooter.java
+++ ImmutableClasses/src/dev/GameConsole/Shooter.java
@@ -2,7 +2,7 @@ package dev.GameConsole;
 
 public record Shooter(String name)implements Player {
     boolean findPrize(){
-        System.out.println("Prize found, score should be adjusted. ");
+//        System.out.println("Prize found, score should be adjusted. ");
         return false;
     }
     boolean useWeapon(String weapon){
