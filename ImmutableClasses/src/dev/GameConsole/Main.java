5596b31e31859d7f0a82d868bc37037fc17f01c0
isaac-miles <isaacnice247@gmail.com>
2024-04-10 16:04:36 +0100

    Constructors advance(Game Console)

--- ImmutableClasses/src/dev/GameConsole/Main.java
+++ ImmutableClasses/src/dev/GameConsole/Main.java
@@ -3,8 +3,8 @@ package dev.GameConsole;
 public class Main {
     public static void main(String[] args) {
         var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
         GameConsole con = new GameConsole<>(new ShooterGame("the wounded game"));
     }
 }
