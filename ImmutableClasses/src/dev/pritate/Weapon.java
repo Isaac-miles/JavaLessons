03bbdaee6d468a385536e38be10e55a9349b8ff4
isaac-miles <isaacnice247@gmail.com>
2024-04-15 17:56:19 +0100

    Constructors advance(Game Console)

--- ImmutableClasses/src/dev/pritate/Weapon.java
+++ ImmutableClasses/src/dev/pritate/Weapon.java
@@ -1,4 +1,17 @@
 package dev.pritate;
 
 public enum Weapon {
+
+    KNIFE(0,10),
+    AXE(0,30),
+    MACHETE(1,40),
+    PISTOL(1,50);
+
+    private final int minLevel;
+    private final int hitPoints;
+
+    Weapon(int minLevel, int hitPoints) {
+        this.minLevel = minLevel;
+        this.hitPoints = hitPoints;
+    }
 }
