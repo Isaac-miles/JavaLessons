e24521c898da97fa83fa79c253d59e2027ab4336
isaac-miles <isaacnice247@gmail.com>
2024-04-15 18:04:54 +0100

    Constructors advance(Game Console)

--- ImmutableClasses/src/dev/pritate/Weapon.java
+++ ImmutableClasses/src/dev/pritate/Weapon.java
@@ -1,5 +1,9 @@
 package dev.pritate;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
 public enum Weapon {
 
     KNIFE(0,10),
@@ -30,5 +34,10 @@ public enum Weapon {
             }
         }
         return values()[0];
+    }
+    public static List<Weapon> getWeaponsByLevel(int levelOfPlay){
+        List<Weapon> weapons = new ArrayList<>(EnumSet.allOf(Weapon.class));
+        weapons.removeIf(weapon -> (weapon.minLevel > levelOfPlay));
+        return weapons;
     }
 }
