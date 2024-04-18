5f71f8b97618810ec117929500730bd1fece6610
isaac-miles <isaacnice247@gmail.com>
2024-04-18 11:25:54 +0100

    Constructors advance(Game Console)

--- ImmutableClasses/src/dev/pirate/Pirate.java
+++ ImmutableClasses/src/dev/pirate/Pirate.java
@@ -2,10 +2,28 @@ package dev.pirate;
 
 import dev.GameConsole.Player;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class Pirate implements Player {
+    private final String name;
+    private final Map<String,Integer> gameData;
+    private final List<String> townsVisited = new LinkedList<>();
+    private Weapon currentWeapon;
+
+    public Pirate(String name){
+        this.name = name;
+    }
+    //---------------instance initializer
+    {
+        gameData = new HashMap<>(Map.of());
+    }
+
 
     @Override
     public String name() {
-        return null;
+        return name;
     }
 }
