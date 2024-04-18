ff6a2fc8c8106f3e0ec0af306bdd3ca6630fc9fd
isaac-miles <isaacnice247@gmail.com>
2024-04-18 11:15:54 +0100

    Constructors advance(Game Console)

--- ImmutableClasses/src/dev/GameConsole/Main.java
+++ ImmutableClasses/src/dev/GameConsole/Main.java
@@ -1,10 +1,15 @@
 package dev.GameConsole;
 
+import dev.pritate.Weapon;
+
 public class Main {
     public static void main(String[] args) {
-        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+//        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
+
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.println("Weapon = "+ weapon + ", hitPoints=" + weapon.getHitPoints()+ ", minLevel="+weapon.getMinLevel());
 
     }
 }
