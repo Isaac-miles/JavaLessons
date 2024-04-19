97ee57753c2580da8ff631fc30b9aee15b810546
isaac-miles <isaacnice247@gmail.com>
2024-04-19 19:46:43 +0100

    Constructors advance(Game Console)

--- ImmutableClasses/src/dev/pirate/PirateGame.java
+++ ImmutableClasses/src/dev/pirate/PirateGame.java
@@ -3,9 +3,17 @@ package dev.pirate;
 import dev.GameConsole.Game;
 import dev.GameConsole.GameAction;
 
+import java.util.List;
 import java.util.Map;
 
 public class PirateGame extends Game<Pirate> {
+
+    private static final List<List<String>> levelMap;
+
+    //------------------------static initializer------
+    static {
+
+    }
     public PirateGame(String gameName) {
         super(gameName);
     }
