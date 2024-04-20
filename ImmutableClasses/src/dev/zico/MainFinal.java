b8c96b973b0b93a4a1e7d5f1dca8609ea74fbd23
isaac-miles <isaacnice247@gmail.com>
2024-04-20 21:54:25 +0100

    Constructors advance(Game Console)

--- ImmutableClasses/src/dev/zico/MainFinal.java
+++ ImmutableClasses/src/dev/zico/MainFinal.java
@@ -1,4 +1,18 @@
 package dev.zico;
 
+import dev.GameConsole.Game;
+import dev.GameConsole.GameConsole;
+import dev.GameConsole.Player;
+
+class SpecialGameConsole<T extends Game<? extends Player>> extends GameConsole<Game<? extends Player>> {
+
+    public SpecialGameConsole(Game<? extends Player> game) {
+        super(game);
+    }
+}
+
 public class MainFinal {
+    public static void main(String[] args) {
+
+    }
 }
