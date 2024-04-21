17b9b3590c79d127498069015c57aa6e4cf0e5dc
isaac-miles <isaacnice247@gmail.com>
2024-04-21 20:24:29 +0100

    (Game Console) sealed classes

--- ImmutableClasses/src/dev/zico/sealed/SpecialAbstractClass.java
+++ ImmutableClasses/src/dev/zico/sealed/SpecialAbstractClass.java
@@ -4,7 +4,7 @@ public sealed abstract class SpecialAbstractClass permits FinalKid, NonSealedKid
         SealedKid, SpecialAbstractClass.Kid {
 
     //if this nested class Kid is the only class dependent on SpecialAbstractClass, then you can ommit it from
-    // the permit list, but if its not the only class you cant
+    // the permit list, but if it's not the only class you cant
     final class Kid extends SpecialAbstractClass{
 
     }
