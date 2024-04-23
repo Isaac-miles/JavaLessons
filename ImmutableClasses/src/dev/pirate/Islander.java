e0205903ed3d2387af32aa52ed7a77212b1c8f05
isaac-miles <isaacnice247@gmail.com>
2024-04-23 22:12:22 +0100

    (Game Console) sealed classes

---
+++ ImmutableClasses/src/dev/pirate/Islander.java
@@ -0,0 +1,8 @@
+package dev.pirate;
+
+public final class Islander extends Combatant{
+    public Islander(String name,Weapon weapon) {
+        super(name);
+        setCurrentWeapon(weapon);
+    }
+}
