48b1c0696bbaf4c9be4027e2f1eae387316b3a73
isaac-miles <isaacnice247@gmail.com>
2024-04-23 22:48:51 +0100

    (Game Console) sealed classes

--- ImmutableClasses/src/dev/pirate/Town.java
+++ ImmutableClasses/src/dev/pirate/Town.java
@@ -1,10 +1,16 @@
 package dev.pirate;
 
+import java.util.Collections;
 import java.util.List;
 
 public record Town(String name, String island, int level,
                    List<Loot> loot,
                    List<Feature> features,
                    List<Combatant> opponents) {
+    private <T> List<T> randomReduced(List<T> list, int size){
+
+        Collections.shuffle(list);
+        return list.subList(0,size);
+    }
 
 }
