11125b3060615311131417508fb209932de8e367
isaac-miles <isaacnice247@gmail.com>
2024-04-23 14:52:35 +0100

    (Game Console) sealed classes

--- ImmutableClasses/src/dev/zico/sealed/StringChecker.java
+++ ImmutableClasses/src/dev/zico/sealed/StringChecker.java
@@ -6,6 +6,6 @@ public final class StringChecker implements SealedInterface{
 
     @Override
     public boolean testData(Predicate<String> p, String... strings) {
-        return false;
+//        return false;
     }
 }
