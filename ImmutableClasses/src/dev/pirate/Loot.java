f3ab8f50c16a4a9e8cae7d7c78bf8a280301f1cf
isaac-miles <isaacnice247@gmail.com>
2024-04-23 20:20:49 +0100

    (Game Console) sealed classes

---
+++ ImmutableClasses/src/dev/pirate/Loot.java
@@ -0,0 +1,16 @@
+package dev.pirate;
+
+public enum Loot {
+    SILVER_COIN(5),
+    GOLD_COIN(10),
+    GOLD_RING(125),
+    PEAL_NECKLACE(250),
+    GOLD_BAR(500);
+
+    private final int worth;
+
+    Loot(int worth){
+        this.worth = worth;
+    }
+
+}
