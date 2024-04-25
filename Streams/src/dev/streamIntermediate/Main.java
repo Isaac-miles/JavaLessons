0f576cb8e51615fe444611f3f105c74fb3747349
isaac-miles <isaacnice247@gmail.com>
2024-04-25 16:31:42 +0100

    streams

---
+++ Streams/src/dev/streamIntermediate/Main.java
@@ -0,0 +1,11 @@
+package dev.streamIntermediate;
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        IntStream.iterate((int) 'A', i-> i <= (int)'z',i-> i+1)
+                .forEach(d-> System.out.printf("%C ",d));
+    }
+}
