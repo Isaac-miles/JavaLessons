eecabc12bb42f50fb20ab3b2f49e5ad2ac77bb82
isaac-miles <isaacnice247@gmail.com>
2024-04-25 12:52:58 +0100

    streams

--- Streams/src/dev/zico/Main.java
+++ Streams/src/dev/zico/Main.java
@@ -7,12 +7,17 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         String[] strings = {"One","Two","Three"};
-        Arrays.stream(strings)
-                .sorted(Comparator.reverseOrder())
+        var firstStream = Arrays.stream(strings)
+                .sorted(Comparator.reverseOrder());
+//                .forEach(System.out::println);
+
+       var secondStream = Stream.of("Six","Five","Four")
+                .map(String::toUpperCase)  ;
+//                .forEach(System.out::println);
+
+        Stream.concat(secondStream,firstStream)
                 .forEach(System.out::println);
 
-        Stream.of("Six","Five","Four")
-                .map(String::toUpperCase)
-                .forEach(System.out::println);
     }
+
 }
