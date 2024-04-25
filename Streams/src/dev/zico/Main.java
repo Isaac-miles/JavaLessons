fe564b34f5c79674f2f80c003296b692453ee111
isaac-miles <isaacnice247@gmail.com>
2024-04-25 12:48:52 +0100

    streams

--- Streams/src/dev/zico/Main.java
+++ Streams/src/dev/zico/Main.java
@@ -2,12 +2,17 @@ package dev.zico;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         String[] strings = {"One","Two","Three"};
         Arrays.stream(strings)
                 .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
+
+        Stream.of("Six","Five","Four")
+                .map(String::toUpperCase)
                 .forEach(System.out::println);
     }
 }
