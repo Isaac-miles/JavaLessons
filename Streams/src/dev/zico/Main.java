ef6e7f40676376c9a776cb30911718516012c265
isaac-miles <isaacnice247@gmail.com>
2024-04-25 13:04:54 +0100

    streams

--- Streams/src/dev/zico/Main.java
+++ Streams/src/dev/zico/Main.java
@@ -30,6 +30,11 @@ public class Main {
             myMap.put(c,numbers);
             bingoIndex += 15;
         }
+
+        myMap.entrySet()
+                .stream()
+                .map(e->e.getKey()+" has range: "+ e.getValue()[0]+ " _ "+e.getValue()[e.getValue().length-1])
+                .forEach(System.out::println);
     }
 
 }
