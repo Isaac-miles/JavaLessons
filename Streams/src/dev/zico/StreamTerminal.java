bb2148311c5eaee59b02c3aab587a55e66ba316e
isaac-miles <isaacnice247@gmail.com>
2024-04-29 22:24:06 +0100

    streams

--- Streams/src/dev/zico/StreamTerminal.java
+++ Streams/src/dev/zico/StreamTerminal.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public class StreamTerminal {
     public static void main(String[] args) {
-        var result = IntStream.iterate(0, i -> i <=1000, i->i+10)
+        var result = IntStream.iterate(0, i -> i <=1000, i->i+3)
                 .summaryStatistics();
         System.out.println("Result = "+result);
     }
