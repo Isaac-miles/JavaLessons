31c8bcb1184bc579d54d103a099eede8247c6431
isaac-miles <isaacnice247@gmail.com>
2024-04-29 16:16:53 +0100

    streams

--- Streams/src/dev/zico/Seat.java
+++ Streams/src/dev/zico/Seat.java
@@ -1,4 +1,7 @@
 package dev.zico;
 
 public record Seat(char rowMarker,int seatNumber,double price) {
+    public Seat(char rowMarker, int seatNumber) {
+        this(rowMarker, seatNumber, 0);
+    }
 }
