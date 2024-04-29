756ecee6b96652c8b1a7b9f964574a04e25e19e6
isaac-miles <isaacnice247@gmail.com>
2024-04-29 16:36:10 +0100

    streams

--- Streams/src/dev/streamIntermediate/Main.java
+++ Streams/src/dev/streamIntermediate/Main.java
@@ -33,7 +33,9 @@ public class Main {
         var stream= Stream.iterate(0,i-> i < maxSeats, i->i+1)
                 .map(i->new Seat((char)('A'+i/seatsInRow),
                         i%seatsInRow + 1))
-                .mapToDouble(Seat::price);
+                .mapToDouble(Seat::price)
+                .boxed()
+                .map("%2f"::formatted);
         stream.forEach(System.out::println);
     }
 }
