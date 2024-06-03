4c2fe54e8f053b337d4c35c5d247f76c41f3f511
isaac-miles <isaacnice247@gmail.com>
2024-06-03 21:21:09 +0100

    streams

--- Streams/src/dev/zico/StreamTerminal.java
+++ Streams/src/dev/zico/StreamTerminal.java
@@ -6,7 +6,7 @@ public class StreamTerminal {
     public static void main(String[] args) {
         var result = IntStream.iterate(0, i -> i <=1000, i->i+3)
                 .summaryStatistics();
-        System.out.println("Result = "+result);
+//        System.out.println("Result = "+result);
 
         var leapYearData = IntStream.iterate(2000,i-> i <=2025, i->i = i+1)
                 .filter(i->i % 4 == 0)
