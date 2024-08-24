356077c1d21b4a8974dbfa6dfe64745df151d458
isaac-miles <isaacnice247@gmail.com>
2024-08-24 23:12:53 +0100

    streams

--- ImmutableClasses/src/dev/constructorsProject/Person.java
+++ ImmutableClasses/src/dev/constructorsProject/Person.java
@@ -3,7 +3,7 @@ package dev.constructorsProject;
 public record Person(String name, String dob) {
 //    public Person(String name, String dob) {
 //        this.name = name;
-        this.dob = dob.replace("-","/");
+//        this.dob = dob.replace("-","/");
 //    }
 
     public Person(Person p) {
