6bc1cf825ebfccead40c71d3e2b4d554ed6d4add
isaac-miles <isaacnice247@gmail.com>
2025-01-03 21:50:36 +0100

    Student streams

--- StreamingStudents/src/dev/zico/Main.java
+++ StreamingStudents/src/dev/zico/Main.java
@@ -2,5 +2,8 @@ package dev.zico;
 
 public class Main {
     public static void main(String[] args){
+        Course jsm = new Course("JSM","Javascript Masterclass");
+        Course csharp = new Course("CSHARP","C# Masterclass");
+        Student miles = new Student ("NG",2022,"M",28,true,jsm,csharp);
     }
 }
