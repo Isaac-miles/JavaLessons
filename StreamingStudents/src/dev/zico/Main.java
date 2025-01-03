c429f4d6cfe880851f1fc63e3f8483891acd8c1d
isaac-miles <isaacnice247@gmail.com>
2025-01-03 22:18:05 +0100

    Student streams

--- StreamingStudents/src/dev/zico/Main.java
+++ StreamingStudents/src/dev/zico/Main.java
@@ -1,14 +1,20 @@
 package dev.zico;
 
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args){
         Course jsm = new Course("JSM","Javascript Masterclass");
         Course csharp = new Course("CSHARP","C# Masterclass");
-        Student miles = new Student ("NG",2022,28,"M",true,jsm,csharp);
-        System.out.println(miles);
+//        Student miles = new Student ("NG",2022,28,"M",true,jsm,csharp);
+//        System.out.println(miles);
+//
+//        miles.watchLecture("JSM",5,2,2022);
+//        miles.watchLecture("CSHARP",10,7,2023);
+//        System.out.println(miles);
 
-        miles.watchLecture("JSM",5,2,2022);
-        miles.watchLecture("CSHARP",10,7,2023);
-        System.out.println(miles);
+        Stream.generate(()->Student.getRandomStudent(jsm,csharp))
+                .limit(10)
+                .forEach(System.out::println);
     }
 }
