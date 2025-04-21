b8d072ec366ec46795c95249763ff9db0c9ca83c
isaac-miles <isaacnice247@gmail.com>
2025-04-21 15:13:23 +0100

    Student streams

--- StreamingStudents/src/dev/zico/Main.java
+++ StreamingStudents/src/dev/zico/Main.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
-    public static void main(String[] args){
-        Course jsm = new Course("JSM","Javascript Masterclass");
-        Course csharp = new Course("CSHARP","C# Masterclass");
+    public static void main(String[] args) {
+        Course jsm = new Course("JSM", "Javascript Masterclass");
+        Course csharp = new Course("CSHARP", "C# Masterclass");
 //        Student miles = new Student ("NG",2022,28,"M",true,jsm,csharp);
 //        System.out.println(miles);
 //
@@ -20,62 +20,63 @@ public class Main {
 //                .forEach(System.out::println);
 
         Student[] students = new Student[1000];
-        Arrays.setAll(students,(i)->Student.getRandomStudent(csharp,jsm));
+        Arrays.setAll(students, (i) -> Student.getRandomStudent(csharp, jsm));
 
         var maleStudents = Arrays.stream(students)
                 .filter(student -> student.getGender().equals("M"));
         System.out.println("# of male female students " + maleStudents.count());
 
-        for(String gender: List.of("M","F","O")){
+        for (String gender : List.of("M", "F", "O")) {
             var mysStudents = Arrays.stream(students)
-                    .filter(s->s.getGender().equals(gender));
-            System.out.println("# of "+ gender + " students "+ mysStudents.count());
+                    .filter(s -> s.getGender().equals(gender));
+            System.out.println("# of " + gender + " students " + mysStudents.count());
         }
         List<Predicate<Student>> list = List.of(
-                (s)->s.getAge() < 30,
-                (Student s)-> s.getAge() >= 30 && s.getAge() < 60
+                (s) -> s.getAge() < 30,
+                (Student s) -> s.getAge() >= 30 && s.getAge() < 60
         );
 
         long total = 0;
-        for(int i=0; i< list.size(); i++){
+        for (int i = 0; i < list.size(); i++) {
             var myStudent = Arrays.stream(students)
                     .filter(list.get(i));
             long cnt = myStudent.count();
-            total +=cnt;
+            total += cnt;
             System.out.printf("# of students (%s) = %d%n", i == 0 ? " < 30" : " >= 30 & < 60", cnt);
         }
-        System.out.println("# of students >= 60 = "+(students.length - total));
+        System.out.println("# of students >= 60 = " + (students.length - total));
 
         var ageStream = Arrays.stream(students)
                 .mapToInt(Student::getAgeEnrolled);
-        System.out.println("Stats for Enrollment Age = "+ ageStream.summaryStatistics());
+        System.out.println("Stats for Enrollment Age = " + ageStream.summaryStatistics());
 
         var currentAgeStream = Arrays.stream(students)
                 .mapToInt(Student::getAge);
-        System.out.println("Stats for Current Age = "+ currentAgeStream.summaryStatistics());
+        System.out.println("Stats for Current Age = " + currentAgeStream.summaryStatistics());
 
         //what country are my students from?
         Arrays.stream(students)
                 .map(Student::getCountryCode)
                 .distinct()
                 .sorted()
-                .forEach(s-> System.out.print(s +" "));
+                .forEach(s -> System.out.print(s + " "));
 
         //Are there any students that are still active and that have been enrolled  for the last 7years
         System.out.println();
         boolean longTerm = Arrays.stream(students)
-                .anyMatch(s-> (s.getAge() - s.getAgeEnrolled() >= 7) && (s.getMonthsSinceActive() < 12));
-        System.out.println("longTerm students? "+ longTerm);
+                .anyMatch(s -> (s.getAge() - s.getAgeEnrolled() >= 7) && (s.getMonthsSinceActive() < 12));
+        System.out.println("longTerm students? " + longTerm);
 
         long longTermCount = Arrays.stream(students)
-                .filter(s->(s.getAge() - s.getAgeEnrolled() >= 7) && (s.getMonthsSinceActive() < 12))
+                .filter(s -> (s.getAge() - s.getAgeEnrolled() >= 7) && (s.getMonthsSinceActive() < 12))
                 .count();
-        System.out.println("longTerm students "+ longTermCount);
+        System.out.println("longTerm students " + longTermCount);
 
-        Arrays.stream(students)
-                .filter(s->(s.getAge() - s.getAgeEnrolled() >=7) && (s.getMonthsSinceActive() < 12))
+        List<Student> longTimeLearners = Arrays.stream(students)
+                .filter(s -> (s.getAge() - s.getAgeEnrolled() >= 7) && (s.getMonthsSinceActive() < 12))
                 .filter(Student::hasProgrammingExperience)
                 .limit(5)
-                .forEach(System.out::println);
+                .toList();
+
     }
 }
