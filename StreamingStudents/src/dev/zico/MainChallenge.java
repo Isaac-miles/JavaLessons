c10a040e47e197a802d425c0b637976a53f3d9ec
isaac-miles <isaacnice247@gmail.com>
2025-04-22 22:30:45 +0100

    Student streams

--- StreamingStudents/src/dev/zico/MainChallenge.java
+++ StreamingStudents/src/dev/zico/MainChallenge.java
@@ -1,10 +1,19 @@
 package dev.zico;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class MainChallenge {
     public static void main(String[] args) {
         Course jsm = new Course("JSM", "Javascript Masterclass",100);
         Course pym = new Course("PYM", "Python Masterclass",50);
         Course cgm = new Course("CGIJ", "Creating Games in Java");
+
+        List<Student> students =
+                Stream.generate(()->Student.getRandomStudent(jsm,pym,cgm))
+                .limit(5000)
+                .toList();
+
 
 
 
