a9b49be5ec0f81672695756981dd2e75d9860c2d
isaac-miles <isaacnice247@gmail.com>
2025-04-22 21:17:39 +0100

    Student streams

--- StreamingStudents/src/dev/zico/MainCollect.java
+++ StreamingStudents/src/dev/zico/MainCollect.java
@@ -1,4 +1,21 @@
 package dev.zico;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MainCollect {
+    public static void main(String[] args) {
+        Course jsm = new Course("JSM", "Javascript Masterclass");
+        Course pym = new Course("PYM", "Python Masterclass");
+
+        List<Student> students =
+                Stream.generate(()->Student.getRandomStudent(jsm,pym))
+                        .limit(500)
+                        .toList();
+        Set<Student> nigerianStudents = students.stream()
+                .filter(s->s.getCountryCode().equals("NG"))
+                .collect(Collectors.toSet());
+    }
 }
