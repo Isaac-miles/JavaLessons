2909de51788844e93177ea44193e7e5660675228
isaac-miles <isaacnice247@gmail.com>
2025-04-22 22:13:30 +0100

    Student streams

--- StreamingStudents/src/dev/zico/MainChallenge.java
+++ StreamingStudents/src/dev/zico/MainChallenge.java
@@ -2,6 +2,7 @@ package dev.zico;
 
 public class MainChallenge {
     public static void main(String[] args) {
-
+        Course jsm = new Course("JSM", "Javascript Masterclass",100);
+        Course pym = new Course("PYM", "Python Masterclass",50);
     }
 }
