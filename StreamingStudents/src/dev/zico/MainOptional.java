962f28a5bd9cbeddd327e901fcc73060dc6c6bbf
isaac-miles <isaacnice247@gmail.com>
2025-05-04 22:23:00 +0100

    Student streams

--- StreamingStudents/src/dev/zico/MainOptional.java
+++ StreamingStudents/src/dev/zico/MainOptional.java
@@ -1,5 +1,6 @@
 package dev.zico;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -16,19 +17,23 @@ public class MainOptional {
                         .limit(1000)
                         .collect(Collectors.toList());
 
-        Optional<Student> o1 = getStudent(null,"first");
+        Optional<Student> o1 = getStudent(new ArrayList<>(),"first");
         System.out.println("Empty = "+o1.isEmpty()+ ", Present = "+o1.isPresent());
-        System.out.println();
+        System.out.println(o1);
+
+        Optional<Student> o2 = getStudent(students,"first");
+        System.out.println("Empty = "+o2.isEmpty()+ ", Present = "+o2.isPresent());
+        System.out.println(o2);
     }
 
     private static Optional<Student> getStudent(List<Student> list, String type){
         if(list==null||list.size()==0){
             return Optional.empty();
         } else if (type.equals("first")) {
-            return  Optional.of(list.get(0));
+            return  Optional.ofNullable(list.get(0));
         } else if (type.equals("last")) {
-            return Optional.of(list.get(list.size()-1));
+            return Optional.ofNullable(list.get(list.size()-1));
         }
-        return Optional.of(list.get(new Random().nextInt(list.size())));
+        return Optional.ofNullable(list.get(new Random().nextInt(list.size())));
     }
 }
