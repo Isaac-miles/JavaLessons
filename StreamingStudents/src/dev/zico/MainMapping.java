a6a1195734dad6a593250ceb7847f6008d49a06d
isaac-miles <isaacnice247@gmail.com>
2025-05-28 20:41:07 +0100

    Student streams

--- StreamingStudents/src/dev/zico/MainMapping.java
+++ StreamingStudents/src/dev/zico/MainMapping.java
@@ -1,7 +1,8 @@
 package dev.zico;
 
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MainMapping {
     public static void main(String[] args) {
@@ -9,9 +10,16 @@ public class MainMapping {
         Course pym = new Course("PYM", "Python Masterclass",50);
         Course jgame = new Course("J-GAME", "Creating Games in Java");
 
-        List<Student> students =
-                Stream.generate(()->Student.getRandomStudent(jsm,pym))
-                        .limit(5000)
-                        .toList();
+        List<Student> students = IntStream
+                .rangeClosed(1,1000)
+                .mapToObj(s->Student.getRandomStudent(jsm,pym))
+                .toList();
+
+//the collectors method returns a collector that collects data into a map, this map gets keyed by the result of the
+// function lambda we provide
+
+        var mappedStudents = students.stream()
+                .collect(Collectors.groupingBy(Student::getCountryCode));
+        mappedStudents.forEach((k,s)-> System.out.println(k +" "+ s.size()));
     }
 }
