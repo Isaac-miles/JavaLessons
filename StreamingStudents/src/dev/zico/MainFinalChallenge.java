000357299bfda82217c2da2bc14d8b1e9372c9c9
isaac-miles <isaacnice247@gmail.com>
2025-05-28 22:27:06 +0100

    Student streams

--- StreamingStudents/src/dev/zico/MainFinalChallenge.java
+++ StreamingStudents/src/dev/zico/MainFinalChallenge.java
@@ -1,6 +1,8 @@
 package dev.zico;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class MainFinalChallenge {
     public static void main(String[] args) {
@@ -9,6 +11,12 @@ public class MainFinalChallenge {
         Course jgame = new Course("J-GAME", "Creating Games in Java");
 
         int currentYear = LocalDate.now().getYear();
+        List<Student> students = Stream
+                .generate(()->Student.getRandomStudent((jgame,pym,jsm))
+                .filter(s->s.getYearsSinceEnrolled() >= (currentYear- 4))
+                .limit(10000)
+                .toList();
+
 
 
     }
