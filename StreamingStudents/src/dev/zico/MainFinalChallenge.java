8a8e054cda0736f07e773919ddf41fc67ae248d2
isaac-miles <isaacnice247@gmail.com>
2025-05-28 22:42:57 +0100

    Student streams

--- StreamingStudents/src/dev/zico/MainFinalChallenge.java
+++ StreamingStudents/src/dev/zico/MainFinalChallenge.java
@@ -2,6 +2,7 @@ package dev.zico;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainFinalChallenge {
@@ -12,12 +13,39 @@ public class MainFinalChallenge {
 
         int currentYear = LocalDate.now().getYear();
         List<Student> students = Stream
-                .generate(()->Student.getRandomStudent((jgame,pym,jsm))
-                .filter(s->s.getYearsSinceEnrolled() >= (currentYear- 4))
+                .generate(()->Student.getRandomStudent(jsm,pym,jgame))
+                .filter(s->s.getYearEnrolled() >= (currentYear- 4))
                 .limit(10000)
                 .toList();
 
+        System.out.println(students
+                .stream()
+                .mapToInt(Student::getYearEnrolled)
+                .summaryStatistics());
 
+        students.subList(0,10).forEach(System.out::println);
 
+        System.out.println(students
+                .stream()
+                .mapToInt(s->s.getEngagementMap().size())
+                .summaryStatistics());
+
+        var mappedActivity = students.stream()
+                .flatMap(s ->s.getEngagementMap().values().stream())
+                .collect(Collectors.groupingBy(CourseEngagement::getCourseCode,Collectors.counting()));
+
+        mappedActivity.forEach((k,v)-> System.out.println(k+" "+v));
+
+        var classCount = students.stream()
+                .collect(Collectors.groupingBy(s->s.getEngagementMap().size(),
+                Collectors.counting()));
+
+        classCount.forEach((k,v)-> System.out.println(k+" "+v));
+
+        var percentages = students.stream()
+                .flatMap(s ->s.getEngagementMap().values().stream())
+                .collect(Collectors.groupingBy(CourseEngagement::getCourseCode,Collectors.counting()));
+
+        percentages.forEach((k,v)-> System.out.println(k+" "+v));
     }
 }
