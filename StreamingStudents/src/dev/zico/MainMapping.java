f294649510e9f6f717655d3ace7a1d8232218c1e
isaac-miles <isaacnice247@gmail.com>
2025-05-28 21:10:57 +0100

    Student streams

--- StreamingStudents/src/dev/zico/MainMapping.java
+++ StreamingStudents/src/dev/zico/MainMapping.java
@@ -3,6 +3,7 @@ package dev.zico;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import static java.util.stream.Collectors.*;
 
 public class MainMapping {
     public static void main(String[] args) {
@@ -21,5 +22,30 @@ public class MainMapping {
         var mappedStudents = students.stream()
                 .collect(Collectors.groupingBy(Student::getCountryCode));
         mappedStudents.forEach((k,s)-> System.out.println(k +" "+ s.size()));
+
+        System.out.println("-".repeat(30));
+//  The static import statement lets you import one or more static members of a class, I cam use the static import statement,
+//  specifying the collectors class, with a wild card to import all static members.
+//        import static java.util.stream.Collectors.*;
+
+        int minAge = 25;
+        var youngerSet = students.stream()
+                .collect(groupingBy(Student::getCountryCode,
+                 filtering(s->s.getAge() <=minAge, toList())));
+        youngerSet.forEach((k,s)-> System.out.println(k +" "+ s.size()));
+
+        var experienced = students.stream()
+                .collect(partitioningBy(Student::hasProgrammingExperience));
+        System.out.println("Experience Student = "+ experienced.get(true).size());
+
+        var expCount = students.stream()
+                .collect(partitioningBy(Student::hasProgrammingExperience,counting()));
+        System.out.println("Experience Student = "+ expCount.get(true));
+
+        var experienceAndActive = students.stream()
+                .collect(partitioningBy(
+                        s->s.hasProgrammingExperience() && s.getMonthsSinceActive() == 0
+                        ,counting()));
+        System.out.println("Experience and Active Student = "+ experienceAndActive.get(true));
     }
 }
