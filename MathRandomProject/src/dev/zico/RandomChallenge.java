ae2a8d92376caeac49cf7ab5e1d18b3d4aa53f7c
isaac-miles <isaacnice247@gmail.com>
2025-05-30 22:18:53 +0100

    Java Core fundamentals, Math,Randomization,BigDecimal

---
+++ MathRandomProject/src/dev/zico/RandomChallenge.java
@@ -0,0 +1,12 @@
+package dev.zico;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomChallenge {
+    private static final Random random = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
