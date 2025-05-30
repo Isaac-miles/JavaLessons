abed34016d4159d1ba0d21a84391376088f8d27e
isaac-miles <isaacnice247@gmail.com>
2025-05-30 23:38:32 +0100

    Java Core fundamentals, Math,Randomization,BigDecimal

--- MathRandomProject/src/dev/zico/MainGame.java
+++ MathRandomProject/src/dev/zico/MainGame.java
@@ -1,16 +1,16 @@
 package dev.zico;
 
-import dev.zico.dice.ScoredItem;
-
-import java.util.ArrayList;
-import java.util.List;
+import dev.zico.dice.DiceGame;
+import dev.zico.game.GameConsole;
 
 public class MainGame {
     public static void main(String[] args) {
-        List<Integer> currentDice = new ArrayList<>(
-                List.of(2,2,4,4,4));
-        for (ScoredItem s: ScoredItem.values()){
-            System.out.printf("Score for %s is %d %n",s,s.score(currentDice));
-        }
+//        List<Integer> currentDice = new ArrayList<>(
+//                List.of(2,2,4,4,4));
+//        for (ScoredItem s: ScoredItem.values()){
+//            System.out.printf("Score for %s is %d %n",s,s.score(currentDice));
+//        }
+
+        var console = new GameConsole<>(new DiceGame("Dice Game"));
     }
 }
