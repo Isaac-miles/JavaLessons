4337749a2aa4b845f2c2240c6a904b9677feb880
isaac-miles <isaacnice247@gmail.com>
2025-05-30 22:33:26 +0100

    Java Core fundamentals, Math,Randomization,BigDecimal

--- MathRandomProject/src/dev/zico/RandomChallenge.java
+++ MathRandomProject/src/dev/zico/RandomChallenge.java
@@ -1,5 +1,7 @@
 package dev.zico;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,6 +9,23 @@ public class RandomChallenge {
     private static final Random random = new Random();
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        List<Integer> currentDice = new ArrayList<>();
+        int rolls = 0;
+        do{
+            rollDice(currentDice);
+            currentDice.clear();
+        }while (++rolls < 5);
+    }
+    private static void rollDice(List<Integer> currentDice){
+        int randomCount = 5 - currentDice.size();
 
+        var newDice = random
+                .ints(randomCount,1,7)
+                .sorted()
+                .boxed()
+                .toList();
+
+        currentDice.addAll(newDice);
+        System.out.println("Your dice are: "+ currentDice);
     }
 }
