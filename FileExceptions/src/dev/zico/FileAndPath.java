ca8d65e127dac5976cac04fc5ac8c36f66398d06
isaac-miles <isaacnice247@gmail.com>
2025-06-06 21:36:10 +0100

    Java Core fundamentals, Path, Files, FileReader

---
+++ FileExceptions/src/dev/zico/FileAndPath.java
@@ -0,0 +1,62 @@
+package dev.zico;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileAndPath {
+    public static void main(String[] args) {
+        useFile("testFile.txt");
+        usePath("pathFile.tst");
+    }
+
+    private static void useFile(String fileName){
+        File file = new File(fileName);
+        boolean fileExists = file.exists();
+        System.out.printf("File '%s' %s%n", fileName, fileExists ? "exists." : "does not exist.");
+
+        if(fileExists){
+            System.out.println("Deleting File: "+ fileName);
+            fileExists = !file.delete();
+        }
+        if(!fileExists){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Something went wrong!!! ");;
+            }
+            System.out.println("Created File: "+ fileName);
+            if(file.canWrite()){
+                System.out.println("Would write to file here");
+            }
+        }
+    }
+    private static void usePath(String fileName){
+        Path path = new Path.of(fileName);
+
+        boolean fileExists = Files.exists(path);
+        System.out.printf("File '%s' %s%n", fileName, fileExists ? "exists." : "does not exist.");
+
+        if(fileExists){
+            System.out.println("Deleting File: "+ fileName);
+            try {
+                Files.delete(path);
+                fileExists = false;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(!fileExists){
+            try {
+                Files.createFile(path);
+            } catch (IOException e) {
+                System.out.println("Something went wrong!!! ");;
+            }
+            System.out.println("Created File: "+ fileName);
+            if(Files.isWritable(path)){
+                System.out.println("Would write to file here");
+            }
+        }
+    }
+}
