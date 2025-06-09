efbca81934a79033666c74c588aee61f632c5c8b
isaac-miles <isaacnice247@gmail.com>
2025-06-09 21:48:05 +0100

    Java Core fundamentals, Path, Files, FileReader

--- FileExceptions/src/dev/zico/FileWalker.java
+++ FileExceptions/src/dev/zico/FileWalker.java
@@ -1,11 +1,20 @@
 package dev.zico;
 
+import java.io.IOException;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 
 public class FileWalker {
     public static void main(String[] args) {
-
+        Path startingPath  = Path.of(".");
+        FileVisitor<Path> statsVisitor = new StatsVisitor();
+        try {
+            Files.walkFileTree(startingPath,statsVisitor);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static class StatsVisitor extends SimpleFileVisitor<Path>{
