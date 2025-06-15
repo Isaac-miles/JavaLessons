6dd822e9682967e11545a7d5de5b03aeecb7d37f
isaac-miles <isaacnice247@gmail.com>
2025-06-15 21:43:36 +0100

    Java Core fundamentals, Path, Files, FileReader

--- FileExceptions/src/dev/writingFiles/CourseEngagement.java
+++ FileExceptions/src/dev/writingFiles/CourseEngagement.java
@@ -1,59 +1,85 @@
 package dev.writingFiles;
 
 import java.time.LocalDate;
-import java.time.Period;
+import java.time.Month;
 
 public class CourseEngagement {
-    private final Course course;
-    private final LocalDate enrollmentDate;
-    private  String engagementType;
-    private LocalDate lastActivityDate;
-    int lastLecture;
-    public CourseEngagement(Course course, LocalDate enrollmentDate, String engagementType) {
-        this.course = course;
-        this.enrollmentDate = this.lastActivityDate = enrollmentDate;
+
+    private final String courseCode;
+    private String engagementType;
+
+    private final int enrollmentMonth;
+    private final int enrollmentYear;
+
+    private int lastLecture;
+    private int lastActiveMonth;
+    private int lastActiveYear;
+
+
+    public CourseEngagement(String courseCode, int month, int year, String engagementType) {
+        this.courseCode = courseCode;
+        enrollmentMonth = lastActiveMonth = month;
+        enrollmentYear = lastActiveYear = year;
         this.engagementType = engagementType;
-    }
-
-    public String getCourseCode() {
-        return course.courseCode();
-    }
-
-    public int getEnrollmentYear() {
-        return enrollmentDate.getYear();
     }
 
     public String getEngagementType() {
         return engagementType;
     }
 
-    public int getLastActivityYear() {
-        return lastActivityDate.getYear();
+    public int getEnrollmentMonth() {
+        return enrollmentMonth;
     }
 
-    public  String getLastActivityMonth(){
-        return  "%tb".formatted(lastActivityDate);
+    public int getEnrollmentYear() {
+        return enrollmentYear;
     }
 
-    public double getPercentComplete(){
-        return lastLecture * 100.0/ course.lectureCount();
+    public int getLastLecture() {
+        return lastLecture;
     }
 
-    public int getMonthsSinceActive(){
+    public int getLastActiveMonth() {
+        return lastActiveMonth;
+    }
+
+    public int getLastActiveYear() {
+        return lastActiveYear;
+    }
+
+
+    public double getPercentComplete(int lectureCount) {
+        return lastLecture*100.0/lectureCount;
+    }
+
+    public int getInactiveMonths() {
+
         LocalDate now = LocalDate.now();
-        var months = Period.between(lastActivityDate, now).toTotalMonths();
-        return (int) months;
+        int currentMonth = now.getMonthValue();
+
+        int months = (now.getYear() - lastActiveYear) * 12;
+        if (currentMonth > lastActiveMonth) {
+            months += (currentMonth - lastActiveMonth);
+        } else {
+            months -= (lastActiveMonth - currentMonth);
+        }
+        return months;
     }
 
-    void watchLecture(int lectureNumber, LocalDate currentDate){
-        lastLecture = Math.max(lectureNumber, lastLecture);
-        lastActivityDate = currentDate;
-        engagementType = "Lecture "+ lastLecture;
-    }
 
     @Override
     public String toString() {
-        return "%s: %s %d %s [%d]".formatted(course.courseCode(),getLastActivityMonth(),getLastActivityYear(),
-                engagementType, getMonthsSinceActive());
+        return "%s,%s,%d,%s".formatted(courseCode,
+                Month.of(lastActiveMonth), lastActiveYear, engagementType);
+    }
+
+    void recordLastActivity(int lectureNumber, int month, int year) {
+
+        if (lectureNumber > lastLecture) {
+            lastLecture = lectureNumber;
+        }
+        lastActiveMonth = month;
+        lastActiveYear = year;
+        engagementType = "Lecture " + lastLecture;
     }
 }
