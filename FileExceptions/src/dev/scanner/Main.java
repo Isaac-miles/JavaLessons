901b0310ae1bf24d11ed07457af7a8e58593e02a
isaac-miles <isaacnice247@gmail.com>
2025-06-15 20:33:51 +0100

    Java Core fundamentals, Path, Files, FileReader

---
+++ FileExceptions/src/dev/scanner/Main.java
@@ -0,0 +1,34 @@
+package dev.scanner;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(new FileReader("fixedWidth.txt"))) {
+//            while(scanner.hasNext()){
+//                System.out.println(scanner.nextLine());
+//            }
+//            System.out.println(scanner.delimiter());
+//            scanner.useDelimiter("$");
+//            scanner.tokens().forEach(System.out::println);
+
+//            scanner.findAll("[A-Za-z]{10,}")
+//                    .map(MatchResult::group)
+//                    .distinct()
+//                    .sorted().forEach(System.out::println);
+
+            var result = scanner.findAll("(.{15})(.{3})(.{12})(.{8})(.{2}).*")
+                    .skip(1)
+                    .map(m->m.group(3).trim())
+                    .distinct()
+                    .sorted()
+                    .toArray(String[]::new);
+            System.out.println(Arrays.toString(result));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
