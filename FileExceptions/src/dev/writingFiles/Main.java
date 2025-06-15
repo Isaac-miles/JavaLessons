998f8ce288f83b44330da96b1f2a8ff90c55d008
isaac-miles <isaacnice247@gmail.com>
2025-06-15 23:11:10 +0100

    Java Core fundamentals, Path, Files, FileReader

--- FileExceptions/src/dev/writingFiles/Main.java
+++ FileExceptions/src/dev/writingFiles/Main.java
@@ -1,9 +1,8 @@
 package dev.writingFiles;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -19,20 +18,95 @@ public class Main {
 
         List<Student> students = Stream
                 .generate(()->Student.getRandomStudent(jsm,pym))
-                .limit(5)
+                .limit(25)
                 .toList();
 
 //        System.out.println(header);
 //        students.forEach(s->s.getEngagementRecords().forEach(System.out::println));
 
         Path path = Path.of("student.csv");
-        try{
-            Files.writeString(path,header);
-            for(Student s : students){
-                Files.write(path,s.getEngagementRecords(), StandardOpenOption.APPEND);
+//        try{
+//            Files.writeString(path,header);
+//            for(Student s : students){
+//                Files.write(path,s.getEngagementRecords(), StandardOpenOption.APPEND);
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        try{
+//            List<String> data = new ArrayList<>();
+//            data.add(header);
+//            for (Student student : students) {
+//                data.addAll(student.getEngagementRecords());
+//            }
+//            Files.write(path,data);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+
+        try(BufferedWriter bufferedWriter =
+                Files.newBufferedWriter(Path.of("take2.csv"))){
+            bufferedWriter.write(header);
+            bufferedWriter.newLine();
+            int count = 0;
+            for (Student student :students){
+                for(var record:student.getEngagementRecords()){
+                    bufferedWriter.write(record);
+                    bufferedWriter.newLine();
+                    count++;
+                    if(count % 5 == 0){
+                        Thread.sleep(2000);
+                        System.out.print(".");
+                    }
+                    if(count % 10 ==0){
+                        bufferedWriter.flush();
+                    }
+                }
             }
-        } catch (IOException e) {
+        }catch (IOException e){
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+
+        try(FileWriter fileWriter = new FileWriter("take3.csv")){
+            fileWriter.write(header);
+            fileWriter.write(System.lineSeparator());
+            for (Student student :students){
+                for(var record:student.getEngagementRecords()){
+                    fileWriter.write(record);
+                    fileWriter.write(System.lineSeparator());
+
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try(PrintWriter printWriter = new PrintWriter("take4.txt")){
+            printWriter.println(header);
+            for (Student student :students){
+                for(var record:student.getEngagementRecords()){
+                    String[] recordData = record.split(",");
+                    printWriter.printf("%-12d%-5s%2d%4d%3d%-1s".formatted(
+                            student.getStudentId(),
+                            student.getCountry(),
+                            student.getEnrollmentYear(),
+                            student.getEnrollmentMonth(),
+                            student.getEnrollmentAge(),
+                            student.getGender()));
+                    printWriter.printf("%-1s",(student.hasExperience()?'Y':'N')); //Experienced?
+                    printWriter.format("%-3s%10.2f%-10s%-4s%-30s",
+                            recordData[7],
+                            student.getPercentComplete(recordData[7]),
+                            recordData[8],
+                            recordData[9],
+                            recordData[10]);
+                    printWriter.println();
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
