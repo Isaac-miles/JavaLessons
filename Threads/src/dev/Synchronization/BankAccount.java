bf3505b36c1da63ae7b7dc7aeb33910df7276030
isaac-miles <isaacnice247@gmail.com>
2025-06-20 16:51:07 +0100

    Threads

--- Threads/src/dev/Synchronization/BankAccount.java
+++ Threads/src/dev/Synchronization/BankAccount.java
@@ -23,4 +23,19 @@ public class BankAccount {
         System.out.printf("STARTING BALANCE: %.0f, DEPOSIT(%.0f)"+
                 " : NEW BALANCE = %.0f%n", originalBalance,amount,balance);
     }
+    public void withdraw(double amount){
+        try{
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        double originalBalance = balance;
+        if(balance >= amount){
+            balance -=amount;
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAW(%.0f)"+
+                    " : NEW BALANCE = %.0f%n", originalBalance,amount,balance);
+        }
+        System.out.printf("STARTING BALANCE: %.0f, WITHDRAW(%.0f)"+
+                " : INSUFFICIENT FUNDS = %.0f%n", originalBalance,amount,balance);
+    }
 }
