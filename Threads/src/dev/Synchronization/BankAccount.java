1c6b1e18f5c8455042cf25f8555c0601bd59568b
isaac-miles <isaacnice247@gmail.com>
2025-06-20 16:42:53 +0100

    Threads

---
+++ Threads/src/dev/Synchronization/BankAccount.java
@@ -0,0 +1,13 @@
+package dev.Synchronization;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+}
