69262c16e3f8f4f30b4b1c451cd8ceba0c274875
isaac-miles <isaacnice247@gmail.com>
2025-06-20 15:39:47 +0100

    Threads

--- Threads/src/dev/MultipleThread/Main.java
+++ Threads/src/dev/MultipleThread/Main.java
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-
         StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
         Thread green = new Thread(stopWatch::countDown, ThreadColor.ANSI_GREEN.name());
         Thread purple = new Thread(() -> stopWatch.countDown(7),
