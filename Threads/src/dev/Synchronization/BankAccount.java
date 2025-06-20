d40f7e0d9f42c9a3e14ff289bc9df1b4ef7ee315
isaac-miles <isaacnice247@gmail.com>
2025-06-21 00:08:50 +0100

    Threads

--- Threads/src/dev/Synchronization/BankAccount.java
+++ Threads/src/dev/Synchronization/BankAccount.java
@@ -6,6 +6,8 @@ public class BankAccount {
     private double balance;
     private String name;
 
+    private final Object lockName = new Object();
+    private final Object lockBalance = new Object();
     public BankAccount(String name, double balance) {
         this.balance = balance;
         this.name = name;
@@ -16,7 +18,7 @@ public class BankAccount {
     }
 
     public  void setName(String name) {
-        synchronized(this.name){
+        synchronized(lockName){
             this.name = name;
             System.out.println("Updated name = "+ this.name);
         }
@@ -28,17 +30,27 @@ public class BankAccount {
     public  void deposit(double amount){
         try{
             System.out.println("Deposit - Talking to the teller at the bank...");
-            Thread.sleep(7000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 //A thread acquires a lock by executing a synchronized method on the instance, or by using the instance as the parameter to synchronized statement
 //A thread releases a lock when it exits from a synchronized block or method, even if it throws an exception, an intrisic lock is only available on an obj
-        synchronized (this){
+        synchronized (lockBalance){
             double originalBalance = balance;
             balance+=amount;
             System.out.printf("STARTING BALANCE: %.0f, DEPOSIT(%.0f)"+
                     " : NEW BALANCE = %.0f%n", originalBalance,amount,balance);
+            addPromoDollars(amount);
+        }
+    }
+
+    private void addPromoDollars(double amount){
+        if(amount >=5000){
+            synchronized (lockBalance){
+                System.out.println("Congratulations, you earned a loyalty bonus.");
+                balance +=25;
+            }
         }
     }
     public synchronized void withdraw(double amount){
