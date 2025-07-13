563abb3c4acb17345311241ff00eed7b8abb72a9
isaac-miles <isaacnice247@gmail.com>
2025-07-13 16:12:19 +0100

    Threads

--- Threads/src/dev/Executors/ShoeWareHouse.java
+++ Threads/src/dev/Executors/ShoeWareHouse.java
@@ -2,13 +2,22 @@ package dev.Executors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ShoeWareHouse {
     private int MAX_BUFFER_SEIZE = 20;
     private List<Order> shippingItems;
+    private final ExecutorService fuflillmentService;
+
     public static final String[] PRODUCT_LIST = {"Running Shoes", "Sandals","Boots","Slippers","High Tops"};
     public ShoeWareHouse(){
         this.shippingItems = new ArrayList<>();
+        fuflillmentService = Executors.newFixedThreadPool(3);
+    }
+
+    public void shutDown(){
+        fuflillmentService.shutdown();
     }
     public synchronized void receiveOrder(Order item){
         while (shippingItems.size() > MAX_BUFFER_SEIZE){
