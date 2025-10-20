0a0b675221a7260ff72344f06f199551e6669d78
isaac-miles <isaacnice247@gmail.com>
2025-10-20 20:11:59 +0100

    Database JDBC

--- MySQLMusic/src/dev/zico/Main.java
+++ MySQLMusic/src/dev/zico/Main.java
@@ -1,6 +1,10 @@
 package dev.zico;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 public class Main {
     private final static String CON_STRING = "jdbc:mysql://localhost:3306/music";
@@ -10,5 +14,11 @@ public class Main {
         int okCxl = JOptionPane.showConfirmDialog(null, passField,"Enter DB Password",JOptionPane.OK_CANCEL_OPTION);
         final char[] password = (okCxl==JOptionPane.OK_OPTION)? passField.getPassword():null;
 
+        try(Connection connection = DriverManager.getConnection(CON_STRING,username,String.valueOf(password))){
+            System.out.println("Successfully connected to the music database");
+            Arrays.fill(password,' ');
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
