50c5d8645bcece1aa70a71e41358a469f336fd6d
isaac-miles <isaacnice247@gmail.com>
2025-10-20 20:06:26 +0100

    Database JDBC

--- MySQLMusic/src/dev/zico/Main.java
+++ MySQLMusic/src/dev/zico/Main.java
@@ -3,10 +3,12 @@ package dev.zico;
 import javax.swing.*;
 
 public class Main {
+    private final static String CON_STRING = "jdbc:mysql://localhost:3306/music";
     public static void main(String[] args) {
         String username = JOptionPane.showInputDialog(null,"Enter DB Username");
         JPasswordField passField = new JPasswordField();
         int okCxl = JOptionPane.showConfirmDialog(null, passField,"Enter DB Password",JOptionPane.OK_CANCEL_OPTION);
         final char[] password = (okCxl==JOptionPane.OK_OPTION)? passField.getPassword():null;
+
     }
 }
