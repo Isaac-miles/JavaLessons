c752701081b4377f5dcea47148377c0713087b01
isaac-miles <isaacnice247@gmail.com>
2025-10-20 19:56:08 +0100

    Database JDBC

--- MySQLMusic/src/dev/zico/Main.java
+++ MySQLMusic/src/dev/zico/Main.java
@@ -1,4 +1,10 @@
 package dev.zico;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        String username = JOptionPane.showInputDialog(null,"Enter DB Username");
+
+    }
 }
