120f0c2412931e91a0f20f5b93422bec51b32bda
isaac-miles <isaacnice247@gmail.com>
2025-10-22 22:13:23 +0100

    Database JDBC

--- MySQLMusic/src/dev/zico/QueryMusic.java
+++ MySQLMusic/src/dev/zico/QueryMusic.java
@@ -1,7 +1,24 @@
 package dev.zico;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+
 public class QueryMusic {
     public static void main(String[] args) {
-
+        Properties props = new Properties();
+        try{
+            props.load(Files.newInputStream(Path.of("music.properties"), StandardOpenOption.READ));
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        var datasource = new MysqlDataSource();
+        datasource.setServerName(props.getProperty("serverName"));
+        datasource.setPort(Integer.parseInt(props.getProperty("port")));
+        datasource.setDatabaseName(props.getProperty("databaseName"));
     }
 }
