9ab6bb9787187de59b10f9cf4054d0fe9937d551
isaac-miles <isaacnice247@gmail.com>
2025-10-22 22:31:52 +0100

    Database JDBC

--- MySQLMusic/src/dev/zico/QueryMusic.java
+++ MySQLMusic/src/dev/zico/QueryMusic.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class QueryMusic {
@@ -16,10 +19,23 @@ public class QueryMusic {
         }catch (IOException e){
             throw new RuntimeException(e);
         }
+        String query = "SELECT * FROM music.artists";
+
         var datasource = new MysqlDataSource();
         datasource.setServerName(props.getProperty("serverName"));
         datasource.setPort(Integer.parseInt(props.getProperty("port")));
         datasource.setDatabaseName(props.getProperty("databaseName"));
+
+        try(var connection = datasource.getConnection(props.getProperty("user"),System.getenv("MYSQL_PASS")
+        ); Statement statement = connection.createStatement()){
+            System.out.println("Connection Established successfully");
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                System.out.printf("%d %s %n",resultSet.getInt(1),resultSet.getString("artist_name"));
+            }
+        }catch (SQLException e){
+            throw  new RuntimeException(e);
+        }
 
     }
 }
