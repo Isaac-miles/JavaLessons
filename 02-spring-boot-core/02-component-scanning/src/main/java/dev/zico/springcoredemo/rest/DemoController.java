f220d58bf3f91357e400818fa65bc2ed587dc948
isaac-miles <isaacnice247@gmail.com>
2026-02-14 23:38:02 +0100

    spring boot core component scanning

--- 02-spring-boot-core/02-component-scanning/src/main/java/dev/zico/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-core/02-component-scanning/src/main/java/dev/zico/springcoredemo/rest/DemoController.java
@@ -1,6 +1,6 @@
 package dev.zico.springcoredemo.rest;
 
-import dev.zico.util.Coach;
+import dev.zico.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
