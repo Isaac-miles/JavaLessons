dea875d0eac8151d97e8e6607041624e00c1c210
isaac-miles <isaacnice247@gmail.com>
2026-02-16 16:28:29 +0100

    spring boot core: lazy loading

---
+++ 02-spring-boot-core/06-lazy-initialization/src/main/java/dev/zico/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,11 @@
+package dev.zico.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Run 100m daily";
+    }
+}
