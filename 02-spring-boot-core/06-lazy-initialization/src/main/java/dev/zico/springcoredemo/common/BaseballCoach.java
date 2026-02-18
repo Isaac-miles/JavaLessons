9a404d979bb6e9a5ea4cb97aae9da5308b6c15ae
isaac-miles <isaacnice247@gmail.com>
2026-02-18 11:37:20 +0100

    spring boot core: lazy loading

--- 02-spring-boot-core/06-lazy-initialization/src/main/java/dev/zico/springcoredemo/common/BaseballCoach.java
+++ 02-spring-boot-core/06-lazy-initialization/src/main/java/dev/zico/springcoredemo/common/BaseballCoach.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class BaseballCoach implements Coach {
 
+    public BaseballCoach() {
+        System.out.println("In constructor: "+ getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Spend 10 minutes in battling practice";
