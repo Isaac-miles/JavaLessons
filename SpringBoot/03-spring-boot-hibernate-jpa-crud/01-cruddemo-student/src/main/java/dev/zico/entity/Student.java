7d4f64298c3280408233742a31f4e91ea2beeb71
isaac-miles <isaacnice247@gmail.com>
2026-02-21 21:42:36 +0100

    japa hibernate crud

--- SpringBoot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/dev/zico/entity/Student.java
+++ SpringBoot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/dev/zico/entity/Student.java
@@ -1,9 +1,29 @@
 package dev.zico.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "student")
 public class Student {
+    //define fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    //define constructors
+
+    //define getters/setters
+
+    //define toString() method
 }
