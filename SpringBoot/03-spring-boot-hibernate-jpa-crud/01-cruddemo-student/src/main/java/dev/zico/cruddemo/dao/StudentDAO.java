d740fdc9ed295473af65cbb82c6d51daf3c608fa
isaac-miles <isaacnice247@gmail.com>
2026-02-22 16:17:28 +0100

    japa hibernate crud:data access object

---
+++ SpringBoot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/dev/zico/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package dev.zico.cruddemo.dao;
+
+import dev.zico.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+}
--- SpringBoot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/dev/zico/dao/StudentDAO.java
+++
@@ -1,7 +0,0 @@
-package dev.zico.dao;
-
-import dev.zico.entity.Student;
-
-public interface StudentDAO {
-    void save(Student student);
-}
