c3330c82bf7502617c0436d9ec29778d1e9f5033
isaac-miles <isaacnice247@gmail.com>
2026-02-24 15:21:46 +0100

    japa hibernate crud:data access object

--- SpringBoot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/dev/zico/cruddemo/dao/StudentDAO.java
+++ SpringBoot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/dev/zico/cruddemo/dao/StudentDAO.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public interface StudentDAO {
     void save(Student student);
+
     Student findById(Integer id);
+
     List<Student> findAll();
+
     List<Student> findByLastName(String lastName);
-    void update(Student student);
+
+    void update(Student theStudent);
+
     void delete(Student student);
 }
