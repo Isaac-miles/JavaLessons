e26a59de1f3ddbaa615258bc64df3fee33f92051
isaac-miles <isaacnice247@gmail.com>
2026-02-28 14:24:04 +0100

    rest controllers

--- SpringBoot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/dev/zico/restdemo/entity/Student.java
+++ SpringBoot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/dev/zico/restdemo/entity/Student.java
@@ -2,4 +2,29 @@ package dev.zico.restdemo.entity;
 
 public class Student {
 
+        private String FirstName;
+        private String LastName;
+
+        public Student() {}
+
+        public Student(String FirstName, String LastName) {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
 }
