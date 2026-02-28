ebbe5dc90c758747f48baa8a85542b8f53ee02ec
isaac-miles <isaacnice247@gmail.com>
2026-02-28 14:35:26 +0100

    rest controllers

--- SpringBoot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/dev/zico/restdemo/rest/StudentRestController.java
+++ SpringBoot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/dev/zico/restdemo/rest/StudentRestController.java
@@ -1,4 +1,23 @@
 package dev.zico.restdemo.rest;
 
+import dev.zico.restdemo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class StudentRestController {
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("John", "Doe"));
+        students.add(new Student("Jane", "Joe"));
+
+        return students;
+    }
 }
