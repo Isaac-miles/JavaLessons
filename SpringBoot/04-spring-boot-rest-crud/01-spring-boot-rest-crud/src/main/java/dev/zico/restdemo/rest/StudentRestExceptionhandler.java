61c7fd54dfa127dbc7b95ec0373159656b984814
isaac-miles <isaacnice247@gmail.com>
2026-03-13 13:19:27 +0100

    rest controllers: exception handling

--- SpringBoot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/dev/zico/restdemo/rest/StudentRestExceptionhandler.java
+++ SpringBoot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/dev/zico/restdemo/rest/StudentRestExceptionhandler.java
@@ -1,4 +1,32 @@
 package dev.zico.restdemo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class StudentRestExceptionhandler {
+
+    //Add an exception handler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStaus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    //catch any exception
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStaus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(e.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
