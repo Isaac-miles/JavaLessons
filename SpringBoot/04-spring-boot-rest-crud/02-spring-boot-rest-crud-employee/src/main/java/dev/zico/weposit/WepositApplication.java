21960870305ac167c6e0e72d5771f43d5874f753
isaac-miles <isaacnice247@gmail.com>
2026-03-13 15:59:57 +0100

    rest controllers: exception handling

---
+++ SpringBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/dev/zico/weposit/WepositApplication.java
@@ -0,0 +1,13 @@
+package dev.zico.weposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WepositApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/test/java/dev/zico/weposit/WepositApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zico.weposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WepositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
