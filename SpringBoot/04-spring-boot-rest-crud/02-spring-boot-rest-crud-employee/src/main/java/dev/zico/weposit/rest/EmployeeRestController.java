6e59f51b7a5349d1c378cb300f880293b920a4e8
isaac-miles <isaacnice247@gmail.com>
2026-03-30 13:11:49 +0100

    Employee repository CRUD OPERATIONS

--- SpringBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/dev/zico/weposit/rest/EmployeeRestController.java
+++ SpringBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/dev/zico/weposit/rest/EmployeeRestController.java
@@ -1,4 +1,26 @@
 package dev.zico.weposit.rest;
 
+import dev.zico.weposit.dao.EmployeeDAO;
+import dev.zico.weposit.entity.Employee;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees()
+    {
+        return employeeDAO.getEmployees();
+    }
 }
