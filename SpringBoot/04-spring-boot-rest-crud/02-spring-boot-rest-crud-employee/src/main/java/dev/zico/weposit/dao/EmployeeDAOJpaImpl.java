193324893d175161f50953ea68393ca5eae2ef47
isaac-miles <isaacnice247@gmail.com>
2026-05-01 22:01:23 +0100

    Employee repository CRUD OPERATIONS

--- SpringBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/dev/zico/weposit/dao/EmployeeDAOJpaImpl.java
+++ SpringBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/dev/zico/weposit/dao/EmployeeDAOJpaImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Autowired
     public  EmployeeDAOJpaImpl(EntityManager entityManager) {
