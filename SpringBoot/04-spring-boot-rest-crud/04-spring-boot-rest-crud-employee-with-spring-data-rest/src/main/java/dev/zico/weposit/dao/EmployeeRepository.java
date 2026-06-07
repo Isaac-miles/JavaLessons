68182faeabf2ff6af4c45fce98aee3abc2cbf7d0
isaac-miles <isaacnice247@gmail.com>
2026-06-07 22:12:12 +0100

    Employee repository CRUD Spring rest

--- SpringBoot/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/dev/zico/weposit/dao/EmployeeRepository.java
+++ SpringBoot/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/dev/zico/weposit/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package dev.zico.weposit.dao;
 
 import dev.zico.weposit.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource(path = "employee")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
