bacfee188d4180d84ce666ff99e01db2cd414574
isaac-miles <isaacnice247@gmail.com>
2026-06-12 21:02:15 +0100

    spring security

--- SpringBoot/04-spring-boot-rest-crud/05-spring-boot-rest-crud-employee-with-spring-data-jpa copy/src/main/java/dev/zico/weposit/dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.zico.weposit.dao;
-
-import dev.zico.weposit.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
