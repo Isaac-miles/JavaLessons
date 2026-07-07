5b425f7f4aeff3c0c5da4a2c15ce548431772f62
isaac-miles <isaacnice247@gmail.com>
2026-07-07 19:41:33 +0100

    spring mvc thymeleaf

---
+++ SpringBoot/06-spring-boot-mvc/01-thymeleaf-helloworld/src/main/java/dev/zico/thymeleaf/model/Student.java
@@ -0,0 +1,46 @@
+package dev.zico.thymeleaf.model;
+
+import java.util.UUID;
+
+public class Student {
+    private String id = UUID.randomUUID().toString();
+    private String firstName;
+    private String lastName;
+    public Student(String id, String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
