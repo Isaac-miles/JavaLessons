506627d5869fe5296d911d00582e187a4105ee9b
isaac-miles <isaacnice247@gmail.com>
2026-07-07 19:38:30 +0100

    spring mvc thymeleaf

--- SpringBoot/06-spring-boot-mvc/01-thymeleaf-helloworld/src/main/java/dev/zico/thymeleaf/controller/DemoController.java
+++ SpringBoot/06-spring-boot-mvc/01-thymeleaf-helloworld/src/main/java/dev/zico/thymeleaf/controller/DemoController.java
@@ -1,19 +1,39 @@
 package dev.zico.thymeleaf.controller;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DemoController {
 
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String requestForm() {
         return "helloworld-form";
     }
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @PostMapping("/updateForm")
+    public String letsShoutDude(HttpServletRequest req, Model model){
+        String theName = req.getParameter("studentName");
+
+        theName = theName.toUpperCase();
+        String result = "Yo!!, " + theName;
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping(path = "/updateFormV2", method = RequestMethod.POST)
+    public String letsShoutDudeV2(@RequestParam("studentName") String theName, Model model){
+        theName = theName.toUpperCase();
+        String result = "Yo!! whatsup, " + theName;
+        model.addAttribute("message", result);
         return "helloworld";
     }
 }
