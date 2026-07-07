1756b371dc5eecb58ab3618a11340636f8b35752
isaac-miles <isaacnice247@gmail.com>
2026-07-07 14:37:42 +0100

    spring mvc thymeleaf

--- SpringBoot/06-spring-boot-mvc/01-thymeleaf-helloworld/src/main/java/dev/zico/thymeleaf/controller/DemoController.java
+++ SpringBoot/06-spring-boot-mvc/01-thymeleaf-helloworld/src/main/java/dev/zico/thymeleaf/controller/DemoController.java
@@ -1,6 +1,19 @@
 package dev.zico.thymeleaf.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class DemoController {
 
+    @RequestMapping("/showForm")
+    public String requestForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
 }
