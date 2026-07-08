6e278f1dc0627d6723fd84a1fa421d648578b66a
isaac-miles <isaacnice247@gmail.com>
2026-07-08 22:36:45 +0100

    spring validation

--- SpringBoot/06-spring-boot-mvc/02-validation/src/main/java/devzico/Customer.java
+++ SpringBoot/06-spring-boot-mvc/02-validation/src/main/java/devzico/Customer.java
@@ -1,5 +1,7 @@
 package devzico;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -8,6 +10,14 @@ public class Customer {
     @NotNull(message = "field is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0,message = "must be greater than 0")
+    @Max(value = 10,message = "must be less than 10")
+    private int freePass;
+
+    public int getFreePass() {
+        return freePass;
+    }
 
     public String getFirstName() {
         return firstName;
