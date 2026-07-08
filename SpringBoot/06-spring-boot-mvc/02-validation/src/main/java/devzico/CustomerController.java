7a596eccd4880d4b130b49eaeceabd7e2e275658
isaac-miles <isaacnice247@gmail.com>
2026-07-08 22:36:53 +0100

    spring validation

--- SpringBoot/06-spring-boot-mvc/02-validation/src/main/java/devzico/CustomerController.java
+++ SpringBoot/06-spring-boot-mvc/02-validation/src/main/java/devzico/CustomerController.java
@@ -1,19 +1,26 @@
 package devzico;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
 
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimer = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringTrimer);
+    }
     @GetMapping("/")
     public String showform(Model model) {
-
         model.addAttribute("customerAtt", new Customer());
         return "customer-form";
     }
@@ -24,7 +31,7 @@ public class CustomerController {
         if (result.hasErrors()) {
             return "customer-form";
         }else {
-            return "customer-confirm";
+            return "customer-confirmation";
         }
 
     }
