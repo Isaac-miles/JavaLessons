8becab4513172e1c18eb4c19694ab00cca465db8
isaac-miles <isaacnice247@gmail.com>
2026-07-08 18:27:59 +0100

    spring validation

--- SpringBoot/06-spring-boot-mvc/02-validation/src/main/java/devzico/CustomerController.java
+++ SpringBoot/06-spring-boot-mvc/02-validation/src/main/java/devzico/CustomerController.java
@@ -1,8 +1,12 @@
 package devzico;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -12,5 +16,16 @@ public class CustomerController {
 
         model.addAttribute("customerAtt", new Customer());
         return "customer-form";
+    }
+
+    @PostMapping("processForm")
+    public String processForm(@Valid @ModelAttribute("customerAtt") Customer customerAtt,
+                              BindingResult result) {
+        if (result.hasErrors()) {
+            return "customer-form";
+        }else {
+            return "customer-confirm";
+        }
+
     }
 }
