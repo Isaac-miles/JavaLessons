63bc004ecf147110f071e1617fb627fbee660c01
isaac-miles <isaacnice247@gmail.com>
2026-07-09 14:58:17 +0100

    spring validation

---
+++ SpringBoot/06-spring-boot-mvc/02-validation/src/main/java/devzico/validation/CourseCode.java
@@ -0,0 +1,20 @@
+package devzico.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.FIELD,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+    public String message() default "must start with zico";
+    public String value() default "Zico";
+    public Class<?>[] groups() default {};
+
+    public Class<? extends Payload>[] payload() default {};
+}
