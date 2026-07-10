89800acca6925f8b434d78f86743c95ef778a26d
isaac-miles <isaacnice247@gmail.com>
2026-07-10 20:47:30 +0100

    mvc crud thymeleaf

--- SpringBoot/07-spring-boot-mvc-crud/00-spring-boot-spring-mvc-crud/src/main/java/dev/zico/weposit/controller/EmployeeController.java
+++ SpringBoot/07-spring-boot-mvc-crud/00-spring-boot-spring-mvc-crud/src/main/java/dev/zico/weposit/controller/EmployeeController.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,5 +41,18 @@ public class EmployeeController {
             employeeService.saveEmployee(employee);
             return "redirect:/employees/list";
         }
+    }
+
+    @GetMapping("/updateForm")
+    public String updateEmployeeForm(@RequestParam("employeeId") int id, Model model) {
+        Employee theEm =  employeeService.getEmployee(id);
+        model.addAttribute("employeeAtt", theEm);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/deleteEmp")
+    public String deleteEmployee(@RequestParam("employeeId") int id) {
+        employeeService.deleteEmployee(id);
+        return "redirect:/employees/list";
     }
 }
