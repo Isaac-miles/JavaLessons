58158aa07431ae8d356234e29dd763463ca4dd45
isaac-miles <isaacnice247@gmail.com>
2026-07-10 22:16:56 +0100

    spring mvc security

---
+++ SpringBoot/08-spring-boot-mvc-security/01-spring-boot-mvc-security/src/main/java/devzico/security/controller/DemoController.java
@@ -0,0 +1,13 @@
+package devzico.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+}
