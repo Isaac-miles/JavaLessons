7bbfb2c39a1fef84c859895406a205b6d9a780f4
isaac-miles <isaacnice247@gmail.com>
2026-07-10 14:35:17 +0100

    mvc crud thymeleaf

---
+++ SpringBoot/07-spring-boot-mvc-crud/00-spring-boot-spring-mvc-crud/src/main/java/dev/zico/weposit/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package dev.zico.weposit.controller;
+
+import dev.zico.weposit.entity.Employee;
+import dev.zico.weposit.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        List<Employee> employees = employeeService.getEmployees();
+        model.addAttribute("employees", employees);
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showForm")
+    public String showEmployeeForm(Model model) {
+        Employee newEmp = new Employee();
+        model.addAttribute("employeeAtt", newEmp);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@Validated @ModelAttribute("employeeAtt") Employee employee, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "employees/employee-form";
+        }else{
+            employeeService.saveEmployee(employee);
+            return "redirect:/employees/list";
+        }
+    }
+}
