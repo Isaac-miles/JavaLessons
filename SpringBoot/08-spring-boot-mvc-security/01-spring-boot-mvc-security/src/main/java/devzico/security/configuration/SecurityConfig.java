63d1a4a9f9a15e9f9637b208e64c7671211fa43d
isaac-miles <isaacnice247@gmail.com>
2026-07-12 21:39:59 +0100

    spring mvc security

--- SpringBoot/08-spring-boot-mvc-security/01-spring-boot-mvc-security/src/main/java/devzico/security/configuration/SecurityConfig.java
+++ SpringBoot/08-spring-boot-mvc-security/01-spring-boot-mvc-security/src/main/java/devzico/security/configuration/SecurityConfig.java
@@ -2,10 +2,12 @@ package devzico.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -32,4 +34,23 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(config->
+                config
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(form->
+                        form
+                                .loginPage("/loginPage")
+                                .loginProcessingUrl("/doLogin")
+                                .permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
 }
