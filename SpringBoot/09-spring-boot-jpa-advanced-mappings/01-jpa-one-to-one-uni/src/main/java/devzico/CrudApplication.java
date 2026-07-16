80fae45f8a866a65738f79b6c93e6a88abe923d5
isaac-miles <isaacnice247@gmail.com>
2026-07-16 16:36:50 +0100

    Spring jpa one to one relationships

--- SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/CrudApplication.java
+++ SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/CrudApplication.java
@@ -1,5 +1,8 @@
 package devzico;
 
+import devzico.dao.AppDAO;
+import devzico.entity.Instructor;
+import devzico.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,9 +17,17 @@ public class CrudApplication {
 	}
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
         return runer->{
-            System.out.println("Hello World!");
+            createInstructor(appDAO);
         };
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+        Instructor tempInstructor = new Instructor("Miles","Leanerd","miles@hayoo.com");
+
+        InstructorDetail tempInstructorDetail = new InstructorDetail("www.miles/home","learning");
+
+        appDAO.save(tempInstructor);
     }
 }
