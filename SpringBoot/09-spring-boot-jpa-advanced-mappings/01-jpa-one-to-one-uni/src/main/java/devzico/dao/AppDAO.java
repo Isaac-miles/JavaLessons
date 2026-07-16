9b8e0af5cf319a12023be7efb0a2bbb80311bc03
isaac-miles <isaacnice247@gmail.com>
2026-07-16 22:04:58 +0100

    Spring jpa one to one relationships

--- SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/dao/AppDAO.java
+++ SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/dao/AppDAO.java
@@ -5,4 +5,7 @@ import devzico.entity.Instructor;
 public interface AppDAO {
 
     void save(Instructor instructor);
+    Instructor findById(int id);
+    void update(Instructor instructor);
+    void delete(int id);
 }
