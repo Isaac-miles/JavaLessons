3747da455c29e36985744d1f9213482523c014b8
isaac-miles <isaacnice247@gmail.com>
2026-07-16 14:34:59 +0100

    Spring jpa one to one relationships

--- SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/CrudApplication.java
+++ SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/CrudApplication.java
@@ -1,7 +1,10 @@
 package devzico;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -10,4 +13,10 @@ public class CrudApplication {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runer->{
+            System.out.println("Hello World!");
+        };
+    }
 }
