014b2d956629e51b38bfe164584a0ccf51acb70e
isaac-miles <isaacnice247@gmail.com>
2026-07-16 14:43:23 +0100

    Spring jpa one to one relationships

--- SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/entity/InstructorDetail.java
+++ SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/entity/InstructorDetail.java
@@ -1,10 +1,20 @@
 package devzico.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
 
 @Entity
 @Table(name = "instructor_detail")
 public class InstructorDetail {
-    private String instructorName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
 }
