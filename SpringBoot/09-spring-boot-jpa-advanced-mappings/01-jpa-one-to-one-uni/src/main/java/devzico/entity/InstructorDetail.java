7cec871604bb0af1c012c296487c9349e775f9a4
isaac-miles <isaacnice247@gmail.com>
2026-07-16 14:38:12 +0100

    Spring jpa one to one relationships

--- SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/entity/InstructorDetail.java
+++ SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/entity/InstructorDetail.java
@@ -1,4 +1,10 @@
 package devzico.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "instructor_detail")
 public class InstructorDetail {
+    private String instructorName;
 }
