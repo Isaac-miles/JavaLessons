2bd1cc970fa4f97deca28d1ab5dce08e28cce57c
isaac-miles <isaacnice247@gmail.com>
2026-07-16 22:05:07 +0100

    Spring jpa one to one relationships

--- SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/CrudApplication.java
+++ SpringBoot/09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/devzico/CrudApplication.java
@@ -6,7 +6,6 @@ import devzico.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -19,15 +18,39 @@ public class CrudApplication {
     @Bean
     public CommandLineRunner commandLineRunner(AppDAO appDAO) {
         return runer->{
-            createInstructor(appDAO);
+//            createInstructor(appDAO);
+//            findInstructor(appDAO);
+            removeInstructor(appDAO);
         };
     }
 
+    private void removeInstructor(AppDAO appDAO) {
+        int id = 1;
+        System.out.println("Removing instructor " + id);
+        appDAO.delete(id);
+        System.out.println("Instructor Deleted " + id);
+    }
+
+    private void findInstructor(AppDAO appDAO) {
+        int id = 3;
+        System.out.println("Finding instructor with id " + id);
+        var instructor = appDAO.findById(id);
+        if (instructor == null) {
+            System.out.println("No instructor with id " + id);
+        }else{
+            System.out.println("tempInstructor "+ instructor);
+            System.out.println("associate details "+ instructor.getInstructorDetail());
+        }
+
+    }
+
     private void createInstructor(AppDAO appDAO) {
-        Instructor tempInstructor = new Instructor("Miles","Leanerd","miles@hayoo.com");
+//        Instructor tempInstructor = new Instructor("Miles","Omokafe","miles@hayoo.com");
+        Instructor tempInstructor = new Instructor("Zico","avies","zico@hayoo.com");
 
-        InstructorDetail tempInstructorDetail = new InstructorDetail("www.miles/home","learning");
-
+        InstructorDetail tempInstructorDetail = new InstructorDetail("www.miles/home","music");
+        tempInstructor.setInstructorDetail(tempInstructorDetail);
+        System.out.println("Instructor created "+ tempInstructor);
         appDAO.save(tempInstructor);
     }
 }
